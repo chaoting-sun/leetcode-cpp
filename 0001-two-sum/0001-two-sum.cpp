6d782e8b78786e52b4bba9dc9ced6aa2a7168fc0
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-07-27 17:46:02 +0800

    Time: 43 ms (28.81%), Space: 14.2 MB (63.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    // one-pass hash table
-    // time: O(n) -> traverse the vector containing n elements only once. each lookup in the table cost only O(1) time.
-    // space: O(n) -> use a map which stores at most n elements
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hash{};
+    // brute-force
+    // time: O(n**2): nested loop of the vector of n elements
+    // space: O(1)
 
+    vector<int> twoSum(vector<int>& nums, int target) {
         for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            if (hash.find(complement) != hash.end()) {
-                return { hash[complement], i };
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return { i, j };
+                }
             }
-            hash.insert({ nums[i], i });
         }
         return {};
     }
+
+    // one-pass hash table
+    // time: O(n) -> traverse the vector containing n elements only once. each lookup in the table cost only O(1) time.
+    // space: O(n) -> use a map which stores at most n elements
+
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     unordered_map<int, int> hash{};
+
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         int complement = target - nums[i];
+    //         if (hash.find(complement) != hash.end()) {
+    //             return { hash[complement], i };
+    //         }
+    //         hash.insert({ nums[i], i });
+    //     }
+    //     return {};
+    // }
+
+
 };
