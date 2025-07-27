698b8d21e8fc9a4e2c5ef92dfda4de8430fda0b4
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-07-28 00:45:28 +0800

    Time: 72 ms (9.07%), Space: 36.2 MB (6.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,15 +1,47 @@
 class Solution {
 public:
+    // Approach 1: Categorize by Sorted String
+    // time: O(n*(k*logk)) -> n is the length of strs and k is the maximum length of the strings in strs
+    // space: O(n*k)
+
+    // vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    //     unordered_map<string, vector<string>> records(0);
+
+    //     for (string& s: strs) {
+    //         string t = s;
+    //         sort(t.begin(), t.end());
+    //         if (records.find(t) == records.end()) {
+    //             records.insert({ t, vector<string>{} });
+    //         }
+    //         records[t].push_back(s);
+    //     }
+
+    //     vector<vector<string>> ans(0);
+    //     for (auto& rec: records) {
+    //         ans.push_back(rec.second);
+    //     }
+
+    //     return ans;
+    // }
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> records(0);
 
         for (string& s: strs) {
-            string t = s;
-            sort(t.begin(), t.end());
-            if (records.find(t) == records.end()) {
-                records.insert({ t, vector<string>{} });
+            vector<int> counts(26);
+            for (char ch: s) {
+                counts[ch - 'a'] += 1;
             }
-            records[t].push_back(s);
+            string k{};
+            for (int i = 0; i < 26; i++) {
+                k += to_string(counts[i]);
+                if (i < 25) k += "#";
+            }
+            
+            if (records.find(k) == records.end()) {
+                records.insert({ k, vector<string>() });
+            }
+            records[k].push_back(s);
         }
 
         vector<vector<string>> ans(0);
