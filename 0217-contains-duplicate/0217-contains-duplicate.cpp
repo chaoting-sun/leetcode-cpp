7b3f1ea9bbe0e8ec9eb2469c0ee6d6b2546244c7
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-07-27 20:07:29 +0800

    Time: 107 ms (9.55%), Space: 92.5 MB (23.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    // brute-force -> TLE
+    // time: O(n**2)
+    // space: O(1)
+
+    // bool containsDuplicate(vector<int>& nums) {
+    //     int n = nums.size();
+    //     for (int i = 0; i < n - 1; i++) {
+    //         for (int j = i + 1; j < n; j++) {
+    //             if (nums[i] == nums[j]) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // sort nums and compare adjacent numbers
+    // time: O(n*lg(n)) -> sorting cost n*lg(n) time
+    // space: O(n)
+
+    // bool containsDuplicate(vector<int>& nums) {
+    //     vector<int> nums_copy = nums;
+
+    //     sort(nums_copy.begin(), nums_copy.end());
+    //     for (int i = 0; i < nums.size() - 1; i++) {
+    //         if (nums_copy[i] == nums_copy[i+1]) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
     // store distinct values in set
     // time: O(n) -> iterate all elements in nums at most
     // space: O(n) -> store at most n distinct element in set
