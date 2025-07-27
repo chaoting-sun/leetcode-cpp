c138400f7e25f9724e9b932e2a688f54bdd9759a
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-07-27 22:07:19 +0800

    Time: 0 ms (100%), Space: 9.5 MB (90.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     // sort both strings
     // time: O(n*lg(n)+m*lg(m)), where n = len of s, m = len of t
-    // space: O(n+m)
+    // space: O(1) -> 26 elements at most
 
     // bool isAnagram(string s, string t) {
     //     if (s.size() != t.size()) return false;
@@ -22,43 +22,91 @@ public:
     //     return true;
     // }
 
-    // sort both strings
+    // Counter: counting the number of characters in s first,
+    // and then do a for loop on t, decrement the count of the same
+    // character appearing in t.
+    // time: O(n+m), where n = len of s, m = len of t
+    // space: O(1) -> 26 element at most
+
+    // bool isAnagram(string s, string t) {
+    //     if (s.size() != t.size()) return false;
+
+    //     unordered_map<char, int> counts;
+    //     int n_distincts = 0;
+    //     int length = s.size();
+
+    //     for (int i = 0; i < length; i++) {
+    //         if (counts.find(s[i]) == counts.end()) {
+    //             counts.insert({ s[i], 1 });
+    //             n_distincts++;
+    //         } else {
+    //             counts[s[i]]++;
+    //         }
+    //     }
+
+    //     for (int i = 0; i < length; i++) {
+    //         // the element in t does not exist in s
+    //         if (counts.find(t[i]) == counts.end()) {
+    //             return false;
+    //         }
+
+    //         counts[t[i]]--;
+            
+    //         // t has more t[i] than s
+    //         if (counts[t[i]] < 0) {
+    //             return false;
+    //         }
+    //         // has found the same number of t[i] in s
+    //         if (counts[t[i]] == 0) {
+    //             n_distincts--;
+    //         }
+    //     }
+
+    //     return n_distincts == 0;
+    // }
+
+    // Simplified Counter: we do not have to check if s contains
+    // extra characters not in "t" because an extra character in t
+    // comes at the expense of an extra character in s.
+    // time: O(n+m), where n = len of s, m = len of t
+    // space: O(distinct number of characters in s)
+
+    // bool isAnagram(string s, string t) {
+    //     if (s.size() != t.size()) return false;
+    //     unordered_map<char, int> counts;
+    //     int length = s.size();
+    //     for (int i = 0; i < length; i++) {
+    //         counts[s[i]]++;
+    //     }
+    //     for (int i = 0; i < length; i++) {
+    //         counts[t[i]]--;
+    //         if (counts[t[i]] < 0) {
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+    // Save counts in array
     // time: O(n*lg(n)+m*lg(m)), where n = len of s, m = len of t
-    // space: O(n+m)
+    // space: O(1) -> 26 elements at most
 
     bool isAnagram(string s, string t) {
         if (s.size() != t.size()) return false;
+        
+        int counts[26] = {0};
+        int n = s.size();
 
-        unordered_map<char, int> map;
-        int n_distincts = 0;
-        int length = s.size();
-
-        for (int i = 0; i < length; i++) {
-            if (map.find(s[i]) == map.end()) {
-                map.insert({ s[i], 1 });
-                n_distincts++;
-            } else {
-                map[s[i]]++;
-            }
+        for (int i = 0; i < n; i++) {
+            counts[s[i]-'a']++;
         }
-
-        for (int i = 0; i < length; i++) {
-            // the element in t does not exist in s
-            if (map.find(t[i]) == map.end()) {
+        for (int i = 0; i < n; i++) {
+            counts[t[i]-'a']--;
+            if (counts[t[i]-'a'] < 0) {
                 return false;
             }
-
-            map[t[i]]--;
-            
-            // t has more t[i] than s
-            if (map[t[i]] < 0) {
-                return false;
-            }
-            if (map[t[i]] == 0) {
-                n_distincts--;
-            }
         }
-
-        return n_distincts == 0;
+        return true;
     }
 };
