429b3d6eef604179a5dbb93f0d725ec7091cebb9
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-07-28 20:23:55 +0800

    Time: 0 ms (100%), Space: 18.1 MB (33.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    // Approach1.1: count the elements and sort by priority queue
+    // time: O(n+n*log(n)+k)=O(n*log(n)), where n is the length of nums
+    // space: O(n)
+
+    // vector<int> topKFrequent(vector<int>& nums, int k) {
+    //     unordered_map<int, int> count;
+    //     for (int n: nums) {
+    //         if (count.find(n) == count.end()) {
+    //             count.insert({ n, 1 });
+    //         } else {
+    //             count[n]++;
+    //         }
+    //     }
+
+    //     priority_queue<pair<int, int>> pq;
+    //     for (auto c: count) {
+    //         pq.push(make_pair(c.second, c.first));
+    //     }
+
+    //     vector<int> ans;
+    //     for (int i = 0; i < k; i++) {
+    //         ans.push_back(pq.top().second);
+    //         pq.pop();
+    //     }
+
+    //     return ans;
+    // }
+
+    // Approach1.2: count the elements and sort by array
+    // time: O(n*log(n))
+    // space: O(n)
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> count;
         for (int n: nums) {
@@ -10,15 +42,18 @@ public:
             }
         }
 
-        priority_queue<pair<int, int>> pq;
+        vector<pair<int, int>> count_arr;
         for (auto c: count) {
-            pq.push(make_pair(c.second, c.first));
+            count_arr.push_back(make_pair(c.first, c.second));
         }
+
+        sort(count_arr.begin(), count_arr.end(), [&](pair<int, int>& c1, pair<int, int>& c2){
+            return c1.second > c2.second;
+        });
 
         vector<int> ans;
         for (int i = 0; i < k; i++) {
-            ans.push_back(pq.top().second);
-            pq.pop();
+            ans.push_back(count_arr[i].first);
         }
 
         return ans;
