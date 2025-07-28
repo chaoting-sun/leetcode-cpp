319715261e82b43ff4fe9000c3ae3757f15894f7
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-07-28 22:01:13 +0800

    Time: 0 ms (100%), Space: 18.2 MB (27.43%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -31,7 +31,7 @@ public:
     //     return ans;
     // }
 
-    // Approach2.1: count the elements and sort by priority queue
+    // Approach2.1: count the elements and sort by max heap
     // time: O(n+n*log(n)+k)=O(n*log(n)), where n is the length of nums
     // space: O(n)
 
@@ -59,59 +59,70 @@ public:
     //     return ans;
     // }
 
-    // Approach2.2: count the elements and sort by priority queue
-    // time: O(n+n*log(n)+k)=O(n*log(n)), where n is the length of nums
+    // Approach2.2: count the elements and sort by min heap, when the
+    // number of elements > k, then pop one to keep them k elements.
+    // time:
+    //  1. store in hash map: O(n)
+    //  2. store in min heap:
+    //      - push first k elements: O(log1+log2+...+logk) = O(log(k!)) = O(k*logk)
+    //      - push and pop remaining n-k elements: O(2*((n-k)*logk)) = O((n-k)*logk)
+    //      -> total: O(n*logk)
+    // 3. total: O(n) + O(n*logk) = O(n*logk)
+    // space: O(n)
+
+    // vector<int> topKFrequent(vector<int>& nums, int k) {
+    //     unordered_map<int, int> count;
+    //     for (int n: nums) {
+    //         if (count.find(n) == count.end()) {
+    //             count.insert({ n, 1 });
+    //         } else {
+    //             count[n]++;
+    //         }
+    //     }
+
+    //     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    //     for (auto c: count) {
+    //         pq.push(make_pair(c.second, c.first));
+    //         if (pq.size() > k) pq.pop();
+    //     }
+
+    //     vector<int> ans;
+    //     for (int i = 0; i < k; i++) {
+    //         ans.push_back(pq.top().second);
+    //         pq.pop();
+    //     }
+
+    //     return ans;
+    // }
+
+    // Approach 3. count the elements and use an array to store counts of elements
+    // time: suppose the max count is MC. in worst case it's n. so it's
+    //  O(n+n+MC+n) <= O(n+n+n+n) = O(n)
     // space: O(n)
 
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> count;
+        int max_count = 0;
         for (int n: nums) {
-            if (count.find(n) == count.end()) {
-                count.insert({ n, 1 });
-            } else {
-                count[n]++;
-            }
+            count[n]++;
+            max_count = max(max_count, count[n]);
         }
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        for (auto c: count) {
-            pq.push(make_pair(c.second, c.first));
-            if (pq.size() > k) pq.pop();
+        vector<vector<int>>count_arr(max_count+1);
+        for (auto& c: count) {
+            count_arr[c.second].push_back(c.first);
         }
 
         vector<int> ans;
-        for (int i = 0; i < k; i++) {
-            ans.push_back(pq.top().second);
-            pq.pop();
+        int n_left = k;
+        for (int i = max_count; i >= 0; i--) {
+            for (int j = 0; j < count_arr[i].size(); j++) {
+                ans.push_back(count_arr[i][j]);
+                n_left--;
+                if (n_left == 0) break;
+            }
+            if (n_left == 0) break;
         }
-
         return ans;
     }
-
-    // vector<int> topKFrequent(vector<int>& nums, int k) {
-    //     unordered_map<int, int> count;
-    //     int max_count = 0;
-    //     for (int n: nums) {
-    //         count[n]++;
-    //         max_count = max(max_count, count[n]);
-    //     }
-
-    //     vector<vector<int>>count_arr(max_count+1);
-    //     for (auto& c: count) {
-    //         count_arr[c.second].push_back(c.first);
-    //     }
-
-    //     vector<int> ans;
-    //     int n_left = k;
-    //     for (int i = max_count; i >= 0; i--) {
-    //         int j = 0;
-    //         for (int j = 0; j < count_arr[i].size(); j++) {
-    //             ans.push_back(count_arr[i][j]);
-    //             n_left--;
-    //             if (n_left == 0) break;
-    //         }
-    //         if (n_left == 0) break;
-    //     }
-    //     return ans;
-    // }
 };
