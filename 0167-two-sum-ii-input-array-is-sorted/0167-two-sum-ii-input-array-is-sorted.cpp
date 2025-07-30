47c3bd92330faa21df28bb459af7557ed04f5ed2
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-07-30 22:11:04 +0800

    Time: 7 ms (2.44%), Space: 21.6 MB (4.93%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
+    // Approach1: Two Pointers. Maintain two pointer from left-most
+    // index and right-most index, and move them toward the center
+    // Time: O(n), where n is the length of numbers
+    // Space: O(1)
+
+    // vector<int> twoSum(vector<int>& numbers, int target) {
+    //     int n = numbers.size();
+    //     int left = 0, right = n - 1;
+
+    //     while (left < right) {
+    //         if (numbers[left] + numbers[right] < target) {
+    //             left++;
+    //         } else if (numbers[left] + numbers[right] > target) {
+    //             right--;
+    //         } else {
+    //             return { left + 1, right + 1 };
+    //         }
+    //     }
+
+    //     return {};
+    // }
+
+    // Approach2: Set. linearly pass through the array, recording the
+    // nums[i] in the set, and check if target - nums[i] is in the set
+    // Time: O(n)
+    // Space: O(n)
+
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int n = numbers.size();
-        int left = 0, right = n - 1;
+        unordered_map<int, int> map;
 
-        while (left < right) {
-            if (numbers[left] + numbers[right] < target) {
-                left++;
-            } else if (numbers[left] + numbers[right] > target) {
-                right--;
-            } else {
-                return { left + 1, right + 1 };
+        for (int i = 0; i < numbers.size(); i++) {
+            if (map.find(target - numbers[i]) != map.end()) {
+                return { map[target - numbers[i]] + 1, i + 1 };
             }
+            map.insert(make_pair(numbers[i], i));
         }
-
         return {};
     }
+
+    
 };
