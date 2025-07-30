334d25cc25ab745dbdf73d7618fae1fbeaa38b63
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-07-30 21:18:49 +0800

    Time: 0 ms (100%), Space: 9.9 MB (58.64%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,16 +1,52 @@
 class Solution {
 public:
+    // Approach: Two pointers. Maintain two pointers from left-most and right-most
+    // index of s, comparing them and moving them toward the center
+    // Time: O(n), where n is the length of s
+    // Space: O(1)
+
+    // Technical Details
+    // - isalnum and tolower are both from <cctype> library
+    // - as s consists only of printable ASCII characters, so it is safe to write
+    //   isalnum(ch) or tolower(ch). If the constraint is not given, a safer method
+    //   would be isalnum(static_cast<unsigned char>(ch), and ...(ignored)
+
+    // bool isPalindrome(string s) {
+    //     int n = s.size();
+    //     int left = 0, right = n - 1;
+
+    //     while (left < right) {
+    //         while (left < n && !isalnum(s[left]))
+    //             left++;
+    //         while (right >= 0 && !isalnum(s[right]))
+    //             right--;
+
+    //         if (left < right && tolower(s[left]) != tolower(s[right])) {
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+    //     return true;
+    // }
+
+    // simpler code
+
     bool isPalindrome(string s) {
         int n = s.size();
         int left = 0, right = n - 1;
 
         while (left < right) {
-            while (left < n && !isalnum(static_cast<unsigned char>(s[left])))
+            if (!isalnum(s[left])) {
                 left++;
-            while (right >= 0 && !isalnum(static_cast<unsigned char>(s[right])))
+                continue;
+            }
+            if (!isalnum(s[right])) {
                 right--;
+                continue;
+            }
 
-            if (left < right && tolower(static_cast<unsigned char>(s[left])) != tolower(static_cast<unsigned char>(s[right]))) {
+            if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
             left++;
