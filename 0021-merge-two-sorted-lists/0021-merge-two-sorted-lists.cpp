c2e3248250bbc19f3c757ae69922d4a73321a018
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-02 17:40:44 +0800

    Time: 0 ms (100%), Space: 19.5 MB (62.81%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,47 +10,68 @@
  */
 class Solution {
 public:
+    // Approach1: Iteration
+    // Time: O(n+m), where n, m are the length of list1 and list2
+    // Space: O(1)
+
+    // ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    //     if (!list1) return list2;
+    //     if (!list2) return list1;
+        
+    //     ListNode *curr1 = list1, *curr2 = list2;
+    //     ListNode *head, *tail;
+
+    //     if (curr1->val < curr2->val) {
+    //         head = curr1;
+    //         tail = curr1;
+    //         curr1 = curr1->next;
+    //     } else {
+    //         head = curr2;
+    //         tail = curr2;
+    //         curr2 = curr2->next;
+    //     }
+
+    //     while (curr1 || curr2) {
+    //         if (!curr1) {
+    //             tail->next = curr2;
+    //             break;
+    //         }
+    //         if (!curr2) {
+    //             tail->next = curr1;
+    //             break;
+    //         }
+            
+    //         if (curr1->val < curr2->val) {
+    //             ListNode* tmp = curr1->next;
+    //             tail->next = curr1;
+    //             curr1 = tmp;
+    //         } else {
+    //             ListNode* tmp = curr2->next;
+    //             tail->next = curr2;
+    //             curr2 = tmp;
+    //         }
+
+    //         tail = tail->next;
+    //         tail->next = nullptr;
+    //     }
+
+    //     return head;
+    // }
+
+    // Approach2: Recursion
+    // Time: O(n+m)
+    // Space: O(n+m)
+
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if (!list1) return list2;
         if (!list2) return list1;
         
-        ListNode *curr1 = list1, *curr2 = list2;
-        ListNode *head, *tail;
-
-        if (curr1->val < curr2->val) {
-            head = curr1;
-            tail = curr1;
-            curr1 = curr1->next;
+        if (list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
         } else {
-            head = curr2;
-            tail = curr2;
-            curr2 = curr2->next;
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
-
-        while (curr1 || curr2) {
-            if (!curr1) {
-                tail->next = curr2;
-                break;
-            }
-            if (!curr2) {
-                tail->next = curr1;
-                break;
-            }
-            
-            if (curr1->val < curr2->val) {
-                ListNode* tmp = curr1->next;
-                tail->next = curr1;
-                curr1 = tmp;
-            } else {
-                ListNode* tmp = curr2->next;
-                tail->next = curr2;
-                curr2 = tmp;
-            }
-
-            tail = tail->next;
-            tail->next = nullptr;
-        }
-
-        return head;
     }
 };
