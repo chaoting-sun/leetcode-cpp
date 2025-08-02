b55d4cf2650d23e7bf54b348546228ce044c1c16
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-02 21:50:19 +0800

    Time: 4 ms (44.97%), Space: 23.6 MB (31.56%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,27 +1,65 @@
+// Approach1: One Stack. For each pushed element, store { value, minimum value }
+// Time: O(1) for push, pop, top, and getMin
+// Space: O(n)
+
+// class MinStack {
+// public:
+//     stack<pair<int,int>> st;
+//     MinStack() {}
+    
+//     void push(int val) {
+//         if (!st.empty()) {
+//             int min_val = st.top().second;
+//             st.push(make_pair(val, min(val, min_val)));
+//         } else {
+//             st.push(make_pair(val, val));
+//         }
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top().first;
+//     }
+    
+//     int getMin() {
+//         return st.top().second;
+//     }
+// };
+
+// Approach2: Two Stack. One stores the element and another stores the minimum value
+// Time: O(1) for push, pop, top, and getMin
+// Space: O(n)
 class MinStack {
 public:
-    stack<pair<int,int>> st;
+    stack<int> st;
+    stack<int> min_st;
     MinStack() {}
     
     void push(int val) {
-        if (!st.empty()) {
-            int min_val = st.top().second;
-            st.push(make_pair(val, min(val, min_val)));
-        } else {
-            st.push(make_pair(val, val));
+        st.push(val);
+        // if the value has been the minimum value, we still need to store it in min_st
+        // because if not, once it get removed, we are not sure if the value exists before it.
+        if (min_st.empty() || val <= min_st.top()) {
+            min_st.push(val);
         }
     }
     
     void pop() {
+        int top = st.top();
         st.pop();
+        if (top == min_st.top())
+            min_st.pop();
     }
     
     int top() {
-        return st.top().first;
+        return st.top();
     }
     
     int getMin() {
-        return st.top().second;
+        return min_st.top();
     }
 };
 
