ff063b6cfce333bff385645220954f2310334e01
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-04 23:53:22 +0800

    Time: 3 ms (15.24%), Space: 22.7 MB (80.02%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,37 +10,88 @@
  */
 class Solution {
 public:
-    // Approach: store the node references in the array, and then link the nodes by their array index
+    // Approach1: store the node references in the array, and then link the nodes by their array index
     // Time: O(n)
     // Space: O(n)
 
+    // void reorderList(ListNode* head) {
+    //     vector<ListNode*> nodes;
+    //     ListNode* curr = head;
+    //     while (curr) {
+    //         nodes.push_back(curr);
+    //         curr = curr->next;
+    //     }
+
+    //     int start = 0, end = nodes.size() - 1;
+    //     while (start <= end) {
+    //         // handle cases of odd number of nodes
+    //         if (start == end) {
+    //             nodes[start]->next = nullptr;
+    //             break;
+    //         }
+    //         // handle cases of even number of nodes
+    //         if (start + 1 == end) {
+    //             nodes[start]->next = nodes[end];
+    //             nodes[end]->next = nullptr;
+    //             break;
+    //         }
+            
+    //         nodes[start]->next = nodes[end];
+    //         nodes[end]->next = nodes[start + 1];
+
+    //         start++;
+    //         end--;
+    //     }
+    // }
+
+    // Approach2:
+    // Time: O(n)
+    // Space: O(1)
+
     void reorderList(ListNode* head) {
-        vector<ListNode*> nodes;
-        ListNode* curr = head;
-        while (curr) {
-            nodes.push_back(curr);
-            curr = curr->next;
+        if (!head) return;
+
+        // find the center node that splits two equally long linked list, or with length difference of 1
+        ListNode *fast = head, *slow = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        int start = 0, end = nodes.size() - 1;
-        while (start <= end) {
-            // handle cases of odd number of nodes
-            if (start == end) {
-                nodes[start]->next = nullptr;
-                break;
-            }
-            // handle cases of even number of nodes
-            if (start + 1 == end) {
-                nodes[start]->next = nodes[end];
-                nodes[end]->next = nullptr;
-                break;
-            }
-            
-            nodes[start]->next = nodes[end];
-            nodes[end]->next = nodes[start + 1];
+        // reverse the second linked list
+        ListNode *prev = nullptr, *curr = slow->next;
+        slow->next = nullptr;
 
-            start++;
-            end--;
+        while (curr) {
+            ListNode *tmp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        // merge two linked list
+        ListNode *curr1 = head, *curr2 = prev;
+        curr = new ListNode(0);
+
+        while (curr1 || curr2) {
+            if (!curr1) {
+                curr->next = curr2;
+                break;
+            }
+            if (!curr2) {
+                curr->next = curr1;
+                break;
+            }
+
+            ListNode *tmp1 = curr1->next, *tmp2 = curr2->next;
+
+            curr->next = curr1;
+            curr = curr->next;
+            curr->next = curr2;
+            curr = curr->next;
+
+            curr1 = tmp1;
+            curr2 = tmp2;
         }
     }
 };
