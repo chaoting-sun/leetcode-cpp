0d0255d58bb15407585bf6d66e55fa3aed56a71f
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-04 22:02:17 +0800

    Time: 54 ms (42.77%), Space: 82.6 MB (62.54%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -6,12 +6,12 @@ public:
     // cars ahead, and compute the time they'll take. If a car A start from a larger position then
     // B, but B takes less time then A, then A and B will form a car fleet.
     
-    // Approach: compute the time a car will arrive at the target assuming that each car can pass
+    // Approach: Array/Stack & Sort. compute the time a car will arrive at the target assuming that each car can pass
     // all the others, storing in the time array "time". Then sort "time" in reverse order of the
-    // position. We loop through time from 0 to n - 1, grouping the non-increasing groups. Each group
-    // is the car fleet. For example, [3,2,1] have one non-increasing group.
+    // position. We loop through time from 0 to n - 1. For each new max. value, the corresponding
+    // car form a car fleet.
 
-    // Time: O(n*log(n))
+    // Time: O(n*log(n)) -> sorting
     // Space: O(n)
 
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
@@ -32,15 +32,25 @@ public:
         vector<double> sorted_time(n);
         for (int i = 0; i < n; i++) sorted_time[i] = time[time_ids[i]];
 
-        int n_car_fleets = 1;
-        double max_val = sorted_time[0];
-        for (int i = 1; i < n; i++) {
-            if (sorted_time[i] > max_val) {
-                max_val = sorted_time[i];
-                n_car_fleets++;
+
+        // 1. find the number of larger values in the array
+        // int n_car_fleets = 1;
+        // double max_val = sorted_time[0];
+        // for (int i = 1; i < n; i++) {
+        //     if (sorted_time[i] > max_val) {
+        //         max_val = sorted_time[i];
+        //         n_car_fleets++;
+        //     }
+        // }
+        // return n_car_fleets;
+
+        // 2. put values in the stack
+        stack<double> st;
+        for (int i = 0; i < n; i++) {
+            if (st.empty() || st.top() < sorted_time[i]) {
+                st.push(sorted_time[i]);
             }
         }
-        
-        return n_car_fleets;
+        return st.size();
     }
 };
