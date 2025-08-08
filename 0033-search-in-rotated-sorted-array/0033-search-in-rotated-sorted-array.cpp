1b809f9e8d4609bd51bd2bd7c7c2e90f6708692e
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-08 11:16:08 +0800

    Time: 0 ms (100%), Space: 15 MB (94.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,75 +4,75 @@ public:
     // interval in left or right hand side
     // Find the min in sorted array: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
 
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int left = 0, right = n - 1;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-
-        int pivot = right;
-        if (nums[pivot] <= target && target <= nums[n - 1]) {
-            left = pivot;
-            right = n;
-        } else {
-            left = 0;
-            right = pivot;
-        }
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > target) {
-                right = mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-
-        return -1;
-    }
-
-    // Approach: One pass binary search
-
     // int search(vector<int>& nums, int target) {
     //     int n = nums.size();
     //     int left = 0, right = n - 1;
 
-    //     while (left <= right) {
+    //     while (left < right) {
     //         int mid = left + (right - left) / 2;
-    //         if (nums[mid] == target) return mid;
-    //         // When the condition satisfies, the interval [left, mid] is not rotated.
-    //         // so if the target is between the nums[left] and nums[mid], it must be in the interval if it exists.
-    //         // if it is not between the two, then search in the right hand side
-            
-    //         // Edge case: mid may be equal to left, so we should use "<=" rather than "<" to handle the case that target == nums[mid]
-    //         if (nums[left] <= nums[mid]) {
-    //             if (nums[left] <= target && target < nums[mid]) {
-    //                 right = mid - 1;
-    //             } else {
-    //                 left = mid + 1;
-    //             }
+    //         if (nums[mid] > nums[right]) {
+    //             left = mid + 1;
+    //         } else {
+    //             right = mid;
     //         }
-    //         // In this case, [left, mid] is a rotated array.
-    //         // Because the right hand side is not rotated, so we check if target is between nums[mid] and nums[right].
-    //         // If it is true, then search in the right, or else the left.
-    //         else {
-    //             if (nums[mid] < target && target <= nums[right]) {
-    //                 left = mid + 1;
-    //             } else {
-    //                 right = mid - 1;
-    //             }
+    //     }
+
+    //     int pivot = right;
+    //     if (nums[pivot] <= target && target <= nums[n - 1]) {
+    //         left = pivot;
+    //         right = n;
+    //     } else {
+    //         left = 0;
+    //         right = pivot;
+    //     }
+
+    //     while (left < right) {
+    //         int mid = left + (right - left) / 2;
+    //         if (nums[mid] > target) {
+    //             right = mid;
+    //         } else if (nums[mid] < target) {
+    //             left = mid + 1;
+    //         } else {
+    //             return mid;
     //         }
-    //     }       
+    //     }
 
     //     return -1;
     // }
+
+    // Approach: One pass binary search
+
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            // When the condition satisfies, the interval [left, mid] is not rotated.
+            // so if the target is between the nums[left] and nums[mid], it must be in the interval if it exists.
+            // if it is not between the two, then search in the right hand side
+            
+            // Edge case: mid may be equal to left, so we should use "<=" rather than "<" to handle the case that target == nums[mid]
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // In this case, [left, mid] is a rotated array.
+            // Because the right hand side is not rotated, so we check if target is between nums[mid] and nums[right].
+            // If it is true, then search in the right, or else the left.
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }       
+
+        return -1;
+    }
 };
