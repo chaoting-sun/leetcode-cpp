c86b8d8fd086fb734c6c96616c678cf7fec646a2
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-09 00:42:16 +0800

    Time: 2 ms (72.58%), Space: 65 MB (70.08%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
+    // Approach1: Marking on an array of slots
+    // Time: O(n)
+    // Space: O(n)
+
+    // int findDuplicate(vector<int>& nums) {
+    //     int sz = nums.size();
+    //     vector<int> slot(sz);
+
+    //     for (int i = 0; i < sz; i++) {
+    //         slot[nums[i]]++;
+    //     }
+
+    //     for (int i = 1; i < sz; i++) {
+    //         if (slot[i] > 1) return i;
+    //     }
+    //     return -1;
+    // }
+
+    // Approach2: fast and slow + Floyd's cycle detection
+    // Time: O(n)
+    // Space: O(1)
+
     int findDuplicate(vector<int>& nums) {
-        int sz = nums.size();
-        vector<int> slot(sz);
-
-        for (int i = 0; i < sz; i++) {
-            slot[nums[i]]++;
+        int fast = nums[0], slow = nums[0];
+        while (true) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            if (fast == slow) break;
         }
 
-        for (int i = 1; i < sz; i++) {
-            if (slot[i] > 1) return i;
+        int curr = nums[0];
+        while (curr != slow) {
+            curr = nums[curr];
+            slow = nums[slow];
         }
-        return -1;
+
+        return curr;
     }
 };
