1e89f2e290474a9510d218b2e3e0700adba1e3d5
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-08 16:28:34 +0800

    Time: 0 ms (100%), Space: 15.1 MB (24.75%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,24 +10,55 @@
  */
 class Solution {
 public:
+    // Approach: Two Pass
+    // Time: O(n)
+    // Space: O(1)
+
+    // ListNode* removeNthFromEnd(ListNode* head, int n) {
+    //     ListNode* curr = head;
+    //     int sz = 0;
+    //     while (curr) {
+    //         sz++;
+    //         curr = curr->next;
+    //     }
+
+    //     ListNode* dummy = new ListNode(0, head);
+    //     curr = dummy;
+    //     int n_move = sz - n;
+    //     while (n_move--) curr = curr->next;
+
+    //     if (curr->next->next) {
+    //         curr->next = curr->next->next;
+    //     } else {
+    //         curr->next = nullptr;
+    //     }
+    //     return dummy->next;
+    // }
+
+
+    // Approach: Two Pass. Two Pointers + Fast and Slow
+    // Time: O(n)
+    // Space: O(1)
+
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* curr = head;
-        int sz = 0;
-        while (curr) {
-            sz++;
-            curr = curr->next;
-        }
-
         ListNode* dummy = new ListNode(0, head);
-        curr = dummy;
-        int n_move = sz - n;
-        while (n_move--) curr = curr->next;
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
 
-        if (curr->next->next) {
-            curr->next = curr->next->next;
-        } else {
-            curr->next = nullptr;
+        int step = n;
+        while (step--) fast = fast->next;
+
+        while (fast->next) {
+            slow = slow->next;
+            fast = fast->next;
         }
+
+        if (slow->next->next) {
+            slow->next = slow->next->next;
+        } else {
+            slow->next = nullptr;
+        }
+
         return dummy->next;
     }
 };
