c20c3db446ae17c9df917d008025ec547778291b
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-08 10:39:59 +0800

    Time: 0 ms (100%), Space: 15 MB (99.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // Approach: find the index of the smallest value, and based on the target, selecting the
+    // interval in left or right hand side
+    // Find the min in sorted array: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
     // int search(vector<int>& nums, int target) {
     //     int n = nums.size();
     //     int left = 0, right = n - 1;
@@ -43,6 +47,8 @@ public:
     //     return -1;
     // }
 
+    // Approach: One pass binary search
+
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int left = 0, right = n - 1;
@@ -50,7 +56,11 @@ public:
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) return mid;
-            // left hand side
+            // When the condition satisfies, the interval [left, mid] is not rotated.
+            // so if the target is between the nums[left] and nums[mid], it must be in the interval if it exists.
+            // if it is not between the two, then search in the right hand side
+            
+            // Edge case: mid may be equal to left, so we should use "<=" rather than "<" to handle the case that target == nums[mid]
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
@@ -58,7 +68,9 @@ public:
                     left = mid + 1;
                 }
             }
-            // right hand side 
+            // In this case, [left, mid] is a rotated array.
+            // Because the right hand side is not rotated, so we check if target is between nums[mid] and nums[right].
+            // If it is true, then search in the right, or else the left.
             else {
                 if (nums[mid] < target && target <= nums[right]) {
                     left = mid + 1;
