9623871de73aa4d74025ab8c8f67c771fb2da58d
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-08 19:29:08 +0800

    Time: 0 ms (100%), Space: 15.3 MB (8.27%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -14,50 +14,126 @@ public:
 };
 */
 
+// Approach1: Array + Hash Table
+// Time: O(n)
+// Space: O(n)
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         if (!head) return nullptr;
+
+//         int n = 0;
+//         vector<Node*> nodes;
+
+//         // count the nodes and maintain array (index -> reference)
+//         Node* curr = head;
+//         while (curr) {
+//             nodes.push_back(curr);
+//             n++;
+//             curr = curr->next;
+//         }
+        
+//         // maintain node reference -> index
+//         unordered_map<Node*, int> node_ids;
+//         for (int i = 0; i < n; i++) {
+//             node_ids[nodes[i]] = i;
+//         }
+
+//         // construct the copy
+//         // 1. handle val
+//         vector<Node*> new_nodes;
+//         for (int i = 0; i < n; i++) {
+//             Node* node_copy = new Node(nodes[i]->val);
+//             new_nodes.push_back(node_copy);
+//         }
+//         // 2. handle next & random
+//         for (int i = 0; i < n; i++) {
+//             if (i < n - 1) {
+//                 new_nodes[i]->next = new_nodes[i + 1];
+//             }
+//             int random_id = nodes[i]->random ? node_ids[nodes[i]->random] : -1;
+//             if (random_id != -1) {
+//                 new_nodes[i]->random = new_nodes[random_id];
+//             }
+//         }
+
+//         return new_nodes[0];
+//     }
+// };
+
+// Approach2: Recursion
+// we recursively do:
+//  1. copy current node (curr) with a value
+//  2. copy curr->next
+//  3. copy curr->random
+
+// Time: O(n)
+// Space: O(n)
+
+// class Solution {
+// private:
+//     unordered_map<Node*, Node*> node_map;
+
+// public:
+//     Node* copyRandomList(Node* head) {
+//         if (!head) return nullptr;
+
+//         if (node_map.find(head) != node_map.end()) {
+//             return node_map[head];
+//         }
+        
+//         // copy current node with a value
+//         Node* copy = new Node(head->val);
+
+//         // a node may be pointed to by node->random many times
+//         // record the node -> copy such that when the node appears twice we can directly use the corresponding copy
+//         node_map[head] = copy;
+        
+//         // copy curr->next
+//         copy->next = copyRandomList(head->next);
+//         // copy curr->next
+//         copy->random = copyRandomList(head->random);
+//         return copy;
+//     }
+
+// };
+
+// Approach2: Iteration
+
+// Time: O(n)
+// Space: O(n)
+
 class Solution {
 public:
-    // Approach1: Array + Hash Table
-    // Time: O(n)
-    // Space: O(n)
-
     Node* copyRandomList(Node* head) {
         if (!head) return nullptr;
-
-        int n = 0;
-        vector<Node*> nodes;
-
-        // count the nodes and maintain array (index -> reference)
+        
+        unordered_map<Node*, Node*> node_map;        
+        Node* dummy = new Node(0);
+        
         Node* curr = head;
+        Node* curr_copy = dummy;
+
         while (curr) {
-            nodes.push_back(curr);
-            n++;
+            Node *new_copy = new Node(curr->val);
+            curr_copy->next = new_copy;
+            node_map[curr] = new_copy;
+
+            curr_copy = curr_copy->next;
             curr = curr->next;
         }
         
-        // maintain node reference -> index
-        unordered_map<Node*, int> node_ids;
-        for (int i = 0; i < n; i++) {
-            node_ids[nodes[i]] = i;
+        curr = head;
+        curr_copy = dummy->next;
+
+        while (curr) {
+            curr_copy->random = node_map[curr->random];
+            curr = curr->next;
+            curr_copy = curr_copy->next;
         }
 
-        // construct the copy
-        // 1. handle val
-        vector<Node*> new_nodes;
-        for (int i = 0; i < n; i++) {
-            Node* node_copy = new Node(nodes[i]->val);
-            new_nodes.push_back(node_copy);
-        }
-        // 1. handle next & random
-        for (int i = 0; i < n; i++) {
-            if (i < n - 1) {
-                new_nodes[i]->next = new_nodes[i + 1];
-            }
-            int random_id = nodes[i]->random ? node_ids[nodes[i]->random] : -1;
-            if (random_id != -1) {
-                new_nodes[i]->random = new_nodes[random_id];
-            }
-        }
-
-        return new_nodes[0];
+        return dummy->next;
     }
+
 };
