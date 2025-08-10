18b9db6beb8516c3f3846b5bff5e024f126089ce
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-10 12:02:55 +0800

    Time: 0 ms (100%), Space: 12.5 MB (23.93%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -11,15 +11,44 @@
  */
 class Solution {
 public:
+    // Approach1: recursion
+    // Time: O(n). n is the number of nodes. we have to traverse all nodes and flip their children in the tree
+    // Space: O(h) = O(n). h is the height of the tree, which, in worst case, is O(n)
+
+    // TreeNode* invertTree(TreeNode* root) {
+    //     if (!root) return nullptr;
+        
+    //     TreeNode* tmp = root->left;
+    //     root->left = root->right;
+    //     root->right = tmp;
+
+    //     invertTree(root->left);
+    //     invertTree(root->right);
+        
+    //     return root;
+    // }
+
+    // Approach2: iteration
+    // Time: O(n). Each node on the tree will be pushed and popped from queue one time
+    // Space: O(n)
+
     TreeNode* invertTree(TreeNode* root) {
         if (!root) return nullptr;
-        
-        TreeNode* tmp = root->left;
-        root->left = root->right;
-        root->right = tmp;
 
-        invertTree(root->left);
-        invertTree(root->right);
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            TreeNode* tmp = node->left;
+            node->left = node->right;
+            node->right = tmp;
+
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
         
         return root;
     }
