aeae96f9a5175b198f0ae19ad0e701907d87653d
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-10 12:27:06 +0800

    Time: 0 ms (100%), Space: 19.2 MB (13.83%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    // Approach: recursion. recursively traverse the current node, and its left and right node.
+    // Approach1: recursion. recursively traverse the current node, and its left and right node.
     // In each node, compute the left and right height respectively and return the maximum one.
     // Time: O(n). we need to traverse all nodes in the tree
     // Space: O(n). the tree in worst case has height = n
@@ -29,13 +29,37 @@ public:
     //     return recursion(root, 0);
     // }
 
+
     // simplify the code
 
+    // int maxDepth(TreeNode* root) {
+    //     if (!root) return 0;
+
+    //     int left_h = 1 + maxDepth(root->left);
+    //     int right_h = 1 + maxDepth(root->right);
+    //     return max(left_h, right_h);
+    // }
+
+    // Approach2: Iteration by BFS
+    // Time: O(n). we need to traverse all nodes
+    // Space: O(n). in worst case, the tree is a complete binary tree. There will be n/2 nodes at the last level.
     int maxDepth(TreeNode* root) {
         if (!root) return 0;
+        
+        int h = 0;
+        queue<TreeNode*> q;
+        q.push(root);
 
-        int left_h = 1 + maxDepth(root->left);
-        int right_h = 1 + maxDepth(root->right);
-        return max(left_h, right_h);
-    }
+        while (!q.empty()) {
+            h++;
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+                if (curr->left) q.push(curr->left);
+                if (curr->right) q.push(curr->right);
+            }
+        }
+        return h;
+    }  
 };
