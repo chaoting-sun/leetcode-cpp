8dbb6ff3b4d6cc16e5b2fb7d50b2de867f249192
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-12 07:41:52 +0800

    Time: 0 ms (100%), Space: 15.1 MB (40.03%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,23 +11,51 @@
  */
 class Solution {
 public:
-    void recursion(TreeNode* root, int level, vector<int> &ans) {
-        // handle base case
-        if (!root) return;
-        
-        // handle this node
-        if (ans.size() < level) {
-            ans.push_back(root->val);
-        }
+    // Approach: Recursion
+    // Time: O(n)
+    // Space: O(n). skewwed tree -> O(n), balanced tree -> O(logn)
 
-        // go to other nodes
-        recursion(root->right, level+1, ans);
-        recursion(root->left, level+1, ans);
-    }
+    // void recursion(TreeNode* root, int level, vector<int> &ans) {
+    //     // handle base case
+    //     if (!root) return;
+        
+    //     // handle this node
+    //     if (ans.size() < level) {
+    //         ans.push_back(root->val);
+    //     }
+
+    //     // go to other nodes
+    //     recursion(root->right, level+1, ans);
+    //     recursion(root->left, level+1, ans);
+    // }
+
+    // vector<int> rightSideView(TreeNode* root) {
+    //     vector<int> ans;
+    //     recursion(root, 1, ans);
+    //     return ans;
+    // }
+
+    // Approach: Iteration
+    // Time: O(n)
+    // Space: O(n). skewwed -> O(1), balanced tree -> O(n)
 
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        recursion(root, 1, ans);
+        queue<TreeNode*> q;
+
+        if (!root) return ans;
+        
+        q.push(root);
+        while (!q.empty()) {
+            int n = q.size();
+            ans.push_back(q.front()->val);
+            for (int i = 0; i < n; i++) {
+                if (q.front()->right) q.push(q.front()->right);
+                if (q.front()->left) q.push(q.front()->left);
+                q.pop();
+            }
+        }
+
         return ans;
     }
 };
