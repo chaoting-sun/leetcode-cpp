a4eccfebe98201b391530b761896e005a7ec8845
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-12 07:21:05 +0800

    Time: 0 ms (100%), Space: 16.7 MB (97.57%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -15,19 +15,46 @@ public:
     // Time: O(n)
     // Space: O(tree height + # nodes) <= O(2n) = O(n)
 
-    void recursion(TreeNode* root, int level, vector<vector<int>> &ans) {
-        if (!root) return;
-        if (ans.size() < level + 1) {
-            ans.push_back({});
-        }
-        ans[level].push_back(root->val);
-        recursion(root->left, level + 1, ans);
-        recursion(root->right, level + 1, ans);
-    }
+    // void recursion(TreeNode* root, int level, vector<vector<int>> &ans) {
+    //     if (!root) return;
+    //     if (ans.size() < level + 1) {
+    //         ans.push_back({});
+    //     }
+    //     ans[level].push_back(root->val);
+    //     recursion(root->left, level + 1, ans);
+    //     recursion(root->right, level + 1, ans);
+    // }
+
+    // vector<vector<int>> levelOrder(TreeNode* root) {
+    //     vector<vector<int>> ans;
+    //     recursion(root, 0, ans);
+    //     return ans;
+    // }
+
+    // Iteration
 
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        recursion(root, 0, ans);
+        queue<TreeNode*> q;
+        int level = 0;
+
+        if (!root) return ans;        
+
+        q.push(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+            ans.push_back(vector<int>());
+
+            for (int i = 0; i < n; i++) {
+                ans[level].push_back(q.front()->val);
+                if (q.front()->left) q.push(q.front()->left);
+                if (q.front()->right) q.push(q.front()->right);
+                q.pop();
+            }
+            level++;
+        }
+
         return ans;
     }
 };
