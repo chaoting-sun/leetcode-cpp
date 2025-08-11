7ace91d07b0f796238f37f2c79a6312569c723e5
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-11 23:50:20 +0800

    Time: 22 ms (61.09%), Space: 23.5 MB (64.37%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,26 +15,26 @@ public:
     // Time: O(n). the tree height
     // Space: O(n). the tree height
 
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root->val > p->val && root->val > q->val) {
-            return lowestCommonAncestor(root->left, p, q);
-        } else if (root->val < p->val && root->val < q->val) {
-            return lowestCommonAncestor(root->right, p, q);
-        } else {
-            return root;
-        }
-    }
+    // TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    //     if (root->val > p->val && root->val > q->val) {
+    //         return lowestCommonAncestor(root->left, p, q);
+    //     } else if (root->val < p->val && root->val < q->val) {
+    //         return lowestCommonAncestor(root->right, p, q);
+    //     } else {
+    //         return root;
+    //     }
+    // }
 
     // Approach: Iteration
     // Time: O(n)
-    // Space: O(n)
+    // Space: O(1)
 
-    // TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    //     while (root) {
-    //         if (root->val > p->val && root->val > q->val) root = root->left;
-    //         if (root->val < p->val && root->val < q->val) root = root->right;
-    //         return root;
-    //     }
-    //     return nullptr;
-    // }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root) {
+            if (root->val > p->val && root->val > q->val) root = root->left;
+            else if (root->val < p->val && root->val < q->val) root = root->right;
+            else return root;
+        }
+        return nullptr;
+    }
 };
