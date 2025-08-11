6897b1e37e532d872d16214f1eee1449b616d791
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-11 23:44:46 +0800

    Time: 25 ms (36.24%), Space: 23.5 MB (64.37%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    // Intuition: for the BST, if a node k is not the ancestor of the two distinct nodes p and q, then p and q should be either in the left subtree or right subtree
+    // Approach: recursion
+    // Time: O(n). the tree height
+    // Space: O(n). the tree height
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root->val > p->val && root->val > q->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else if (root->val < p->val && root->val < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else {
+            return root;
+        }
+    }
+
+    // Approach: Iteration
+    // Time: O(n)
+    // Space: O(n)
+
+    // TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    //     while (root) {
+    //         if (root->val > p->val && root->val > q->val) root = root->left;
+    //         if (root->val < p->val && root->val < q->val) root = root->right;
+    //         return root;
+    //     }
+    //     return nullptr;
+    // }
+};
