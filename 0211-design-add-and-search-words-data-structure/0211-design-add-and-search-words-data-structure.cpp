bffe9de07abae139ebd94e5f5dbf5ec6859971b3
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-13 23:10:10 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,3 +1,13 @@
+// addWord
+// Time: O(L). L is the length of word. at each step, we either examine or create a node in the trie
+// Space: O(L). In worst case, we create L nodes in the trie
+
+// search
+// Time: O(L*26^m). in worst case, in every level <= L, all characters (26) exists and so do the
+// characters in the next level. Also, suppose that there are m ".", each expand 26 possibilities,
+// so there are 26^m paths. each path has length L, so the time is L*26^m.
+// Space: O(L)
+
 class CharNode {
 public:
     unordered_map<char, CharNode*> children;
@@ -32,23 +42,47 @@ public:
         return searchInNode(word, 0, root);
     }
 
-    bool searchInNode(string &word, int idx, CharNode* root) {
-        if (idx >= word.size()) return root->is_word;
+    // bool searchInNode(string &word, int idx, CharNode* root) {
+    //     if (idx >= word.size()) return root->is_word;
 
-        if (word[idx] == '.') {
-            for (auto node: root->children) {
-                if (searchInNode(word, idx + 1, node.second)) {
-                    return true;
+    //     if (word[idx] == '.') {
+    //         for (auto node: root->children) {
+    //             if (searchInNode(word, idx + 1, node.second)) {
+    //                 return true;
+    //             }
+    //         }
+    //     } else {
+    //         if (root->children.count(word[idx]) && searchInNode(word, idx + 1, root->children[word[idx]])) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+
+    bool searchInNode(string &word, int start_idx, CharNode* root) {
+        for (int i = start_idx; i < word.size(); i++) {
+            char ch = word[i];
+
+            if (ch == '.') {
+                for (auto node: root->children) {
+                    if (searchInNode(word, i + 1, node.second)) {
+                        return true;
+                    }
                 }
-            }
-        } else {
-            if (root->children.count(word[idx]) && searchInNode(word, idx + 1, root->children[word[idx]])) {
-                return true;
+                return false;
+            } else {
+                if (!root->children.count(ch) || !searchInNode(word, i + 1, root->children[ch])) {
+                    return false;
+                }
+                root = root->children[ch];
             }
         }
-        return false;
+
+        return root->is_word;
     }
 };
+
 
 /**
  * Your WordDictionary object will be instantiated and called as such:
