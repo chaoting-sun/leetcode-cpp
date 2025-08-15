012dd984e5c23e4ec04f3f11d1633078fac9c82f
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-15 23:14:59 +0800

    Time: 162 ms (6.71%), Space: 167.2 MB (5.87%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    // Approach: backtracking
+    // This is a classic "generate all valid partitions" problem -> backtracking
+    // explore the string left to right. at each start index, I try every possible end index;
+    // if s[start..i] is a palindrome, we choose it, recurse from i+1, and then un-choose (pop)
+    // when we reach the end, I've built one valid partition and addit to the 
+    
+    // Time: O(n * 2^n)
+    // - the number of substrings: in worst case all characters are the same, such as aa..aa. for
+    // a string aa..aa with length n, there are n - 1 gaps that we can choose to cut off. so
+    // there are 2^(n-1) combinations. so there are O(2^n) substring
+    // - the execution time: it takes O(n) to generate each substring
+    
+    // Space: O(n)
+    // The max. depth of the recursive call stack will be O(n)
+
     vector<vector<string>> ans;
 
     bool isPalindrome(string &s, int start, int end) {
