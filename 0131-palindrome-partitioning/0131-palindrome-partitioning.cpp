f4bd03c5f4c6f6e7cc7d79cff0d9df8e03166c5b
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-15 23:16:54 +0800

    Time: 157 ms (7.29%), Space: 167.4 MB (5.39%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -15,16 +15,47 @@ public:
     // Space: O(n)
     // The max. depth of the recursive call stack will be O(n)
 
-    vector<vector<string>> ans;
+    // vector<vector<string>> ans;
 
-    bool isPalindrome(string &s, int start, int end) {
-        while (start < end) {
-            if (s[start] != s[end]) return false;
-            start++;
-            end--;
-        }
-        return true;
-    }
+    // bool isPalindrome(string &s, int start, int end) {
+    //     while (start < end) {
+    //         if (s[start] != s[end]) return false;
+    //         start++;
+    //         end--;
+    //     }
+    //     return true;
+    // }
+
+    // void searchPalindrome(string &s, int start, vector<string> substrings) {
+    //     if (start == s.size()) {
+    //         ans.push_back(substrings);
+    //         return;
+    //     }
+
+    //     for (int i = start; i < s.size(); i++) {
+    //         if (isPalindrome(s, start, i)) {
+    //             substrings.push_back(s.substr(start, i - start + 1));
+    //             searchPalindrome(s, i + 1, substrings);
+    //             substrings.pop_back();
+    //         }
+    //     }
+    // }
+
+    // vector<vector<string>> partition(string s) {
+    //     searchPalindrome(s, 0, vector<string>());
+    //     return ans;
+    // }
+
+    // Intuition: to check if a substring [start...end] is a palindrome, we can check:
+    // if start == end: is palindrome
+    // elif start + 1 == end & s[start + 1] == s[end]: is a palindrome
+    // elif start + 1 < end & s[start+1..end-1] is a palindrom: is a palindrome
+    // so there are overlapping subproblems    
+
+    // Approach: Backtracking + dp[][]
+
+    vector<vector<string>> ans;
+    vector<vector<bool>> dp;
 
     void searchPalindrome(string &s, int start, vector<string> substrings) {
         if (start == s.size()) {
@@ -32,16 +63,20 @@ public:
             return;
         }
 
-        for (int i = start; i < s.size(); i++) {
-            if (isPalindrome(s, start, i)) {
-                substrings.push_back(s.substr(start, i - start + 1));
-                searchPalindrome(s, i + 1, substrings);
+        for (int end = start; end < s.size(); end++) {
+            if (start == end || s[start] == s[end] && (start + 1 == end || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                substrings.push_back(s.substr(start, end - start + 1));
+                searchPalindrome(s, end + 1, substrings);
                 substrings.pop_back();
             }
         }
     }
 
     vector<vector<string>> partition(string s) {
+        int n = s.size();
+        dp.resize(n, vector<bool>(n, false));
+
         searchPalindrome(s, 0, vector<string>());
         return ans;
     }
