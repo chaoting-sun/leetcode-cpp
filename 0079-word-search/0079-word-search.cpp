1542313adf2f5c81c077cf0c9405b30929164a47
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-16 11:31:45 +0800

    Time: 183 ms (94.65%), Space: 10.5 MB (74.07%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,14 +4,27 @@ private:
     int n;
 
 public:
+    // Approach: backtracking
+    // Treat the board as a grid graph. from every cell that matches the first character, start a DFS
+    // Then at each step i, temporarily mark the cell as visited so we don't reuse it in the current path
+
+    // Time: O(m*n*3^L)
+    // there are m*n grids so we'll try m*n cell as a start. from a start the branching factor
+    // is at most 4 on the first move, but then <=3 thereafter (you cannot go back to the visited cell)
+    // Space: O(L)
+    // O(L) recursion stack depth
+
     bool backtrack(vector<vector<char>>& board, string& word, int i, int x, int y) {
         if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] != word[i]) return false;
 
         if (i == word.size() - 1) return true;
         
         char tmp = board[x][y];
+
+        // mark as visited
         board[x][y] = '#';
 
+        // search in the selection list
         if (backtrack(board, word, i + 1, x + 1, y) ||
             backtrack(board, word, i + 1, x - 1, y) ||
             backtrack(board, word, i + 1, x, y + 1) ||
@@ -20,6 +33,7 @@ public:
             return true;
         }
         
+        // mark as unvisited to not affect the other backtracking path
         board[x][y] = tmp;
         return false;
     }
