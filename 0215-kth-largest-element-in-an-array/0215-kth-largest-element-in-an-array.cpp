5dae5cdabc81adb4219a227ab0218060040a417e
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-17 16:42:17 +0800

    Time: 42 ms (20.93%), Space: 61.5 MB (45.69%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -18,4 +18,44 @@ public:
         }
         return pq.top();
     }
+
+    // quick select
+    // Time: O(n) in average case. O(n^2) in worst case (array is sorted)
+    // Space: O(1)
+
+    // int findKthLargest(vector<int>& nums, int k) {
+    //     return quickSelect(nums, k, 0, nums.size() - 1);
+    // }
+
+    // int quickSelect(vector<int>& nums, int k, int left, int right) {
+    //     if (left == right) {
+    //         return nums[left];
+    //     }
+
+    //     int pivot_index = partition(nums, left, right);
+    //     int rank = pivot_index - left + 1;
+
+    //     if (rank == k) {
+    //         return nums[pivot_index];
+    //     } else if (rank > k) {
+    //         return quickSelect(nums, k, left, pivot_index - 1);
+    //     } else {
+    //         return quickSelect(nums, k - rank, pivot_index + 1, right);
+    //     }
+    // }
+
+    // int partition(vector<int>& nums, int left, int right) {
+    //     int pivot = nums[right];
+
+    //     int i = left;
+    //     for (int j = left; j < right; j++) {
+    //         if (nums[j] >= pivot) {
+    //             swap(nums[i], nums[j]);
+    //             i++;
+    //         }
+    //     }
+
+    //     swap(nums[i], nums[right]);
+    //     return i;
+    // }
 };
