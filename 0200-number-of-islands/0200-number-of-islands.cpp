c0d9821589affc4b7362243c3e35b3f064abd4aa
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-18 22:18:38 +0800

    Time: 22 ms (88.52%), Space: 16.3 MB (79.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,35 +1,72 @@
 class Solution {
 public:
-    int m;
-    int n;
+    // Approach: DFS
 
-    bool findIsland(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j) {
-        if (i < 0 || i >= m || j < 0 || j >= n) return false;
-        if (visited[i][j]) return false;
-        if (grid[i][j] == '0') return false;
+    // int m;
+    // int n;
 
-        visited[i][j] = true;
+    // bool findIsland(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j) {
+    //     if (i < 0 || i >= m || j < 0 || j >= n) return false;
+    //     if (visited[i][j]) return false;
+    //     if (grid[i][j] == '0') return false;
 
-        findIsland(grid, visited, i + 1, j);
-        findIsland(grid, visited, i - 1, j);
-        findIsland(grid, visited, i, j + 1);
-        findIsland(grid, visited, i, j - 1);
+    //     visited[i][j] = true;
 
-        return true;
+    //     findIsland(grid, visited, i + 1, j);
+    //     findIsland(grid, visited, i - 1, j);
+    //     findIsland(grid, visited, i, j + 1);
+    //     findIsland(grid, visited, i, j - 1);
+
+    //     return true;
+    // }
+
+    // int numIslands(vector<vector<char>>& grid) {
+    //     m = grid.size();
+    //     n = grid[0].size();
+        
+    //     vector<vector<bool>> visited(m, vector<bool>(n, false));
+    //     int count = 0;
+
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (!visited[i][j] && grid[i][j] == '1') {
+    //                 bool found = findIsland(grid, visited, i, j);
+    //                 if (found) count++;
+    //             }
+    //         }
+    //     }
+
+    //     return count;
+    // }
+
+    // Approach: DFS (inplace chang)
+
+    void findIsland(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if (i < 0 || i >= m || j < 0 || j >= n) return;
+        if (grid[i][j] != '1') return;
+
+        grid[i][j] = '0';
+
+        findIsland(grid, i + 1, j);
+        findIsland(grid, i - 1, j);
+        findIsland(grid, i, j + 1);
+        findIsland(grid, i, j - 1);
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         int count = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    bool found = findIsland(grid, visited, i, j);
-                    if (found) count++;
+                if (grid[i][j] == '1') {
+                    findIsland(grid, i, j);
+                    count++;
                 }
             }
         }
@@ -37,3 +74,4 @@ public:
         return count;
     }
 };
+
