8af4ae9732b822811f221e1b01c5b2bf21a87433
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-18 22:44:56 +0800

    Time: 4 ms (42.83%), Space: 27.3 MB (97.72%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,19 +1,59 @@
 class Solution {
 public:
-    void dfsCountArea(vector<vector<int>>& grid, int i, int j, int& count) {
+    // Approach: DFS
+    // Time: O(m*n)
+    // Space: O(m*n)
+
+    // void dfsCountArea(vector<vector<int>>& grid, int i, int j, int& count) {
+    //     int m = grid.size();
+    //     int n = grid[0].size();
+
+    //     if (i < 0 || i > m - 1 || j < 0 || j > n - 1) return;
+    //     if (grid[i][j] != 1) return;
+
+    //     count++;
+    //     grid[i][j] = 2;
+
+    //     dfsCountArea(grid, i - 1, j, count);
+    //     dfsCountArea(grid, i + 1, j, count);
+    //     dfsCountArea(grid, i, j - 1, count);
+    //     dfsCountArea(grid, i, j + 1, count);
+    // }
+
+    // int maxAreaOfIsland(vector<vector<int>>& grid) {
+    //     int m = grid.size();
+    //     int n = grid[0].size();
+    //     int max_count = 0;
+
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (grid[i][j] == 1) {
+    //                 int count = 0;
+    //                 dfsCountArea(grid, i, j, count);
+    //                 max_count = max(max_count, count);
+    //             }
+    //         }
+    //     }
+
+    //     return max_count;
+    // }
+
+
+    // Another Style of DFS
+
+    int dfsCountArea(vector<vector<int>>& grid, int i, int j) {
         int m = grid.size();
         int n = grid[0].size();
 
-        if (i < 0 || i > m - 1 || j < 0 || j > n - 1) return;
-        if (grid[i][j] != 1) return;
+        if (i < 0 || i >= m || j < 0 || j >= n) return 0;
+        if (grid[i][j] != 1) return 0;
 
-        count++;
         grid[i][j] = 2;
 
-        dfsCountArea(grid, i - 1, j, count);
-        dfsCountArea(grid, i + 1, j, count);
-        dfsCountArea(grid, i, j - 1, count);
-        dfsCountArea(grid, i, j + 1, count);
+        return 1 + dfsCountArea(grid, i - 1, j)
+                 + dfsCountArea(grid, i + 1, j)
+                 + dfsCountArea(grid, i, j - 1)
+                 + dfsCountArea(grid, i, j + 1);
     }
 
     int maxAreaOfIsland(vector<vector<int>>& grid) {
@@ -24,9 +64,9 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    int count = 0;
-                    dfsCountArea(grid, i, j, count);
+                    int count = dfsCountArea(grid, i, j);
                     max_count = max(max_count, count);
+                    cout << count << " ";
                 }
             }
         }
