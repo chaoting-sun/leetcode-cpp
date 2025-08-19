5179c198b4f272d3f58753bb9d6696914d2a14b2
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-19 08:13:59 +0800

    Time: 3 ms (76.91%), Space: 19 MB (69.71%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,36 +1,38 @@
+const int GATE = 0;
+const int EMPTY = 2147483647;
+
 class Solution {
 public:
-    void dfs(vector<vector<int>>& rooms, int i, int j, int step) {
-        int m = rooms.size();
-        int n = rooms[0].size();
-
-        if (i < 0 || i >= m || j < 0 || j >= n) return;
-        if (rooms[i][j] == -1 || rooms[i][j] == 0) return;
-
-        step++;
-        int tmp = min(rooms[i][j], step);
-        rooms[i][j] = -1;
-
-        dfs(rooms, i + 1, j, step);
-        dfs(rooms, i - 1, j, step);
-        dfs(rooms, i, j + 1, step);
-        dfs(rooms, i, j - i, step);
-
-        rooms[i][j] = tmp;
-    }
-
     void wallsAndGates(vector<vector<int>>& rooms) {
         int m = rooms.size();
         int n = rooms[0].size();
 
+        queue<pair<int, int>> pos;
+        vector<int> di { -1, 1, 0, 0 };
+        vector<int> dj { 0, 0, -1, 1 };
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (rooms[i][j] == 0) {
-                    dfs(rooms, i + 1, j, 0);
-                    dfs(rooms, i - 1, j, 0);
-                    dfs(rooms, i, j + 1, 0);
-                    dfs(rooms, i, j - 1, 0);
+                if (rooms[i][j] == GATE) {
+                    pos.push({ i, j });
                 }
+            }
+        }
+
+        while (!pos.empty()) {
+            int ci = pos.front().first;
+            int cj = pos.front().second;
+            pos.pop();
+
+            for (int k = 0; k < 4; k++) {
+                int ni = ci + di[k];
+                int nj = cj + dj[k];
+
+                if (ni < 0 || ni >= m || nj < 0 || nj >= n || rooms[ni][nj] != EMPTY)
+                    continue;
+                
+                rooms[ni][nj] = rooms[ci][cj] + 1;
+                pos.push({ ni, nj });
             }
         }
     }
