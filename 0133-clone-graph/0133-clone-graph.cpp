009e7cf4a611a5d4e81764e875359a8998f65c48
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-19 21:11:06 +0800

    Time: 3 ms (68.03%), Space: 12.1 MB (65.23%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,25 +21,60 @@ public:
 
 class Solution {
 public:
-    Node* dfs(Node* node, unordered_map<Node*, Node*>& graph2clone) {
-        if (!node) return nullptr;
-        if (graph2clone.count(node)) {
-            return graph2clone[node];
-        }
+    // Node* dfs(Node* node, unordered_map<Node*, Node*>& graph2clone) {
+    //     if (!node) return nullptr;
+    //     if (graph2clone.count(node)) {
+    //         return graph2clone[node];
+    //     }
 
-        Node* curr = new Node(node->val);
-        graph2clone[node] = curr;
+    //     Node* curr = new Node(node->val);
+    //     graph2clone[node] = curr;
 
-        for (Node* nei: node->neighbors) {
-            Node* nei_clone = dfs(nei, graph2clone);
-            curr->neighbors.push_back(nei_clone);
-        }
+    //     for (Node* nei: node->neighbors) {
+    //         Node* nei_clone = dfs(nei, graph2clone);
+    //         curr->neighbors.push_back(nei_clone);
+    //     }
 
-        return curr;
-    }
+    //     return curr;
+    // }
+
+    // Node* cloneGraph(Node* node) {
+    //     unordered_map<Node*, Node*> graph2clone;
+    //     return dfs(node, graph2clone);
+    // }
+
+    // BFS
+    // Time: O(V + E)
+    //  - V (# nodes): each node is copied once
+    //  - E (# edges): if we pop a node, we loop all of its neighbors. so each edge is processed twice.
+    // Space: O(V). including hash map and queue
 
     Node* cloneGraph(Node* node) {
-        unordered_map<Node*, Node*> graph2clone;
-        return dfs(node, graph2clone);
+        if (!node) return nullptr;
+        
+        unordered_map<Node*, Node*> clones;
+        queue<Node*> q;
+
+        clones[node] = new Node(node->val);
+        q.push(node);
+
+        while (!q.empty()) {
+            int n = q.size();
+
+            for (int i = 0; i < n; i++) {
+                Node* curr = q.front();
+                q.pop();
+
+                for (Node* nei: curr->neighbors) {
+                    if (!clones.count(nei)) {
+                        clones[nei] = new Node(nei->val);
+                        q.push(nei);
+                    }
+                    clones[curr]->neighbors.push_back(clones[nei]);
+                }
+            }
+        }
+
+        return clones[node];
     }
 };
