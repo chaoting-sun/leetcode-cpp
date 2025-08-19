05bd005826d2782b402d061b56b6f38f16aa60b4
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-19 08:14:33 +0800

    Time: 7 ms (46.83%), Space: 18.9 MB (96.35%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -3,6 +3,18 @@ const int EMPTY = 2147483647;
 
 class Solution {
 public:
+    // Intuition: we need to find the shortest path of each empty room to gate,
+    // and BFS is a convenient algorithm to do this. in BFS, we push all gates
+    // to the queue first, and then update the neigboring empty rooms. if an
+    // empty room has been updated from a gate (A) ealier than the other gate (B),
+    // it has bee updated to the smallest distance. otherwise it will be updated
+    // later then B. Therefore, we don't have to update the empty room twice.
+    
+    // Approach: BFS
+
+    // Time: O(m*n). BFS takes at most m*n steps to reach all rooms
+    // Space: O(m*n). in worst case all are gates
+
     void wallsAndGates(vector<vector<int>>& rooms) {
         int m = rooms.size();
         int n = rooms[0].size();
