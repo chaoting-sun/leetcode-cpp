44939a82eb961af650fec5016f9f4969964f84f5
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-21 22:15:11 +0800

    Time: 0 ms (100%), Space: 16.9 MB (47.31%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -39,7 +39,50 @@ public:
     //     return true;
     // }
 
+    // Approach: DFS (iterative)
+
+    // bool validTree(int n, vector<vector<int>>& edges) {
+    //     vector<vector<int>> adj(n);
+    //     // Time: O(E)
+    //     // Space: O(E + V)
+    //     for (vector<int> edge: edges) {
+    //         adj[edge[0]].push_back(edge[1]);
+    //         adj[edge[1]].push_back(edge[0]);
+    //     }
+
+    //     unordered_map<int, int> parent; // child -> parent
+        
+    //     stack<int> stk;
+    //     stk.push(0);
+    //     parent[0] = -1;
+    //     int count = 0;
+
+    //     while (!stk.empty()) {
+    //         int curr = stk.top();
+    //         stk.pop();
+    //         for (int neighbor: adj[curr]) {
+    //             // it is its parent
+    //             if (parent.count(curr) && parent[curr] == neighbor) {
+    //                 continue;
+    //             }
+    //             // neighbor has been visited -> has cycle
+    //             if (parent.count(neighbor)) {
+    //                 return false;
+    //             }
+    //             stk.push(neighbor);
+    //             parent[neighbor] = curr;
+    //         }
+    //     }
+
+    //     return parent.size() == n;
+    // }
+
+    // Optimization
+    // to check if it is a tree -> fully connected & there are n - 1 edges
+
     bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() + 1 != n) return false;
+        
         vector<vector<int>> adj(n);
         // Time: O(E)
         // Space: O(E + V)
@@ -48,30 +91,23 @@ public:
             adj[edge[1]].push_back(edge[0]);
         }
 
-        unordered_map<int, int> parent; // child -> parent
+        unordered_set<int> visited;
         
         stack<int> stk;
         stk.push(0);
-        parent[0] = -1;
-        int count = 0;
+        visited.insert(0);
 
         while (!stk.empty()) {
             int curr = stk.top();
             stk.pop();
             for (int neighbor: adj[curr]) {
-                // it is its parent
-                if (parent.count(curr) && parent[curr] == neighbor) {
-                    continue;
-                }
                 // neighbor has been visited -> has cycle
-                if (parent.count(neighbor)) {
-                    return false;
-                }
+                if (visited.count(neighbor)) continue;
                 stk.push(neighbor);
-                parent[neighbor] = curr;
+                visited.insert(neighbor);
             }
         }
 
-        return parent.size() == n;
+        return visited.size() == n;
     }
 };
