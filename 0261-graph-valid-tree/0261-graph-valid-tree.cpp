fdc6664d3f93aaf92264ce876e12af8e8c365b46
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-21 22:04:53 +0800

    Time: 8 ms (20.13%), Space: 18.9 MB (14.03%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,34 +1,77 @@
 class Solution {
-public: 
-    bool hasCycle(vector<vector<int>>& adj, vector<bool>& visited, int prev, int curr, int& count) {
-        if (visited[curr]) return true;
+public:
+    // Approach: DFS (recursive)
+    // Time: O(E + V)
+    // Space: O(E + V)
 
-        visited[curr] = true;
-        count++;
+    // bool hasCycle(vector<vector<int>>& adj, vector<bool>& visited, int prev, int curr, int& count) {
+    //     if (visited[curr]) return true;
 
-        for (int next: adj[curr]) {
-            if (next == prev) continue;
-            if (hasCycle(adj, visited, curr, next, count)) {
-                return true;
-            }
-        }
+    //     visited[curr] = true;
+    //     count++;
 
-        return false;
-    }
+    //     for (int next: adj[curr]) {
+    //         if (next == prev) continue;
+    //         if (hasCycle(adj, visited, curr, next, count)) {
+    //             return true;
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
+    // bool validTree(int n, vector<vector<int>>& edges) {
+    //     vector<vector<int>> adj(n);
+    //     // Time: O(E)
+    //     // Space: O(E + V)
+    //     for (vector<int> edge: edges) {
+    //         adj[edge[0]].push_back(edge[1]);
+    //         adj[edge[1]].push_back(edge[0]);
+    //     }
+
+    //     vector<bool> visited(n, 0);
+        
+    //     int count = 0;
+    //     // Time: O(E + V)
+    //     bool exist_cycle = hasCycle(adj, visited, -1, 0, count);
+        
+    //     if (exist_cycle || count != n) return false;
+    //     return true;
+    // }
 
     bool validTree(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
+        // Time: O(E)
+        // Space: O(E + V)
         for (vector<int> edge: edges) {
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
 
-        vector<bool> visited(n, 0);
+        unordered_map<int, int> parent; // child -> parent
         
+        stack<int> stk;
+        stk.push(0);
+        parent[0] = -1;
         int count = 0;
-        bool exist_cycle = hasCycle(adj, visited, -1, 0, count);
-        
-        if (exist_cycle || count != n) return false;
-        return true;
+
+        while (!stk.empty()) {
+            int curr = stk.top();
+            stk.pop();
+            for (int neighbor: adj[curr]) {
+                // it is its parent
+                if (parent.count(curr) && parent[curr] == neighbor) {
+                    continue;
+                }
+                // neighbor has been visited -> has cycle
+                if (parent.count(neighbor)) {
+                    return false;
+                }
+                stk.push(neighbor);
+                parent[neighbor] = curr;
+            }
+        }
+
+        return parent.size() == n;
     }
 };
