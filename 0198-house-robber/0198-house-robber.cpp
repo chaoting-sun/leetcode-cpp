6ba3b09745352c7ab08efefc507c047ca6d76023
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-23 18:51:44 +0800

    Time: 0 ms (100%), Space: 10 MB (97.32%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     // Approach: DP
-    // state transition function: dp[n] = max(dp[n-2] + nums[n], dp[n-1])
+    // state transition function: dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+    // - dp[i-2] + nums[i]: rob i
+    // - dp[i-1]: not rob i
+    // Time: O(n)
+    // Space: O(n)
+
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if (n == 1) return nums[0];
+
+    //     vector<int> dp(n + 1);
+    //     dp[1] = nums[0];
+
+    //     for (int i = 1; i < n; i++) {
+    //         dp[i + 1] = max(dp[i - 1] + nums[i], dp[i]);
+    //     }
+
+    //     return dp[n];
+    // }
+
+    // Approach: Two variables maintaining the prev max and curr max money
+    // Time: O(n)
+    // Space: O(1)
 
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
-        if (n == 2) return max(nums[0], nums[1]);
 
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int prev_max = 0;
+        int curr_max = nums[0];
 
-        for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        for (int i = 1; i < n; i++) {
+            int next_max = max(prev_max + nums[i], curr_max);
+            prev_max = curr_max;
+            curr_max = next_max;
         }
 
-        return dp[n - 1];
+        return curr_max;
     }
 };
