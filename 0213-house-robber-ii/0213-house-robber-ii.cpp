cda9adb684fcfcc7cd0017ec104f1ea912d66229
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-23 19:01:16 +0800

    Time: 0 ms (100%), Space: 10.7 MB (55.98%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        vector<int> dp0(n + 1), dp1(n + 1);
+        dp0[1] = nums[0];
+        for (int i = 2; i < n; i++) {
+            dp0[i] = max(dp0[i - 2] + nums[i - 1], dp0[i - 1]);
+        }
+        for (int i = 2; i <= n; i++) {
+            dp1[i] = max(dp1[i - 2] + nums[i - 1], dp1[i - 1]);
+        }
+        return max(dp0[n - 1], dp1[n]);
+    }
+};
