fef23211b0682f57b77cf5a581144f94e2e03145
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-23 21:20:14 +0800

    Time: 3 ms (97.9%), Space: 9.4 MB (81.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // Approach: DP
+    // Approach: 2D DP
     // meaning of dp[i][j]: if the substring [i,j] in s is a palindromic substring
     // initialization: set dp[i][i] to true
     // state transition function dp[i][j] = dp[i + 1][j - 1] && s[i] == s[j]
@@ -8,24 +8,64 @@ public:
     // Time: O(n^2)
     // Space: O(n^2)
 
+    // string longestPalindrome(string s) {
+    //     int n = s.size();
+    //     vector<vector<bool>> dp(n, vector<bool>(n, false));
+    //     int lmax = 0, rmax = 0;
+
+    //     for (int i = 0; i < n; i++) dp[i][i] = true;
+
+    //     for (int l = n - 1; l >= 0; l--) {
+    //         for (int r = l + 1; r < n; r++) {
+    //             if (l + 1 == r) {
+    //                 dp[l][r] = s[l] == s[r];
+    //             } else {
+    //                 dp[l][r] = dp[l + 1][r - 1] && s[l] == s[r];
+    //             }
+    //             if (dp[l][r] && r - l > rmax - lmax) {
+    //                 lmax = l;
+    //                 rmax = r;
+    //             }
+    //         }
+    //     }
+
+    //     return s.substr(lmax, rmax - lmax + 1);
+    // }
+
+    // Approach: Two Pointers
+    // Time: O(n)
+    // Space: O(1)
+
+    pair<int, int> findMaxPalindrome(string& s, int l, int r) {
+        if (s[l] != s[r]) return { -1, -1 };
+
+        int n = s.size();
+        while (0 < l && r < n - 1 && s[l - 1] == s[r + 1]) {
+            l--;
+            r++;
+        }
+        return { l, r };
+    }
+
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
         int lmax = 0, rmax = 0;
 
-        for (int i = 0; i < n; i++) dp[i][i] = true;
+        // odd
+        for (int i = 0; i < n; i++) {
+            auto [l, r] = findMaxPalindrome(s, i, i);
+            if (r - l > rmax - lmax) {
+                rmax = r;
+                lmax = l;
+            }
+        }
 
-        for (int l = n - 1; l >= 0; l--) {
-            for (int r = l + 1; r < n; r++) {
-                if (l + 1 == r) {
-                    dp[l][r] = s[l] == s[r];
-                } else {
-                    dp[l][r] = dp[l + 1][r - 1] && s[l] == s[r];
-                }
-                if (dp[l][r] && r - l > rmax - lmax) {
-                    lmax = l;
-                    rmax = r;
-                }
+        // even
+        for (int i = 0; i < n - 1; i++) {
+            auto [l, r] = findMaxPalindrome(s, i, i + 1);
+            if (r - l > rmax - lmax) {
+                rmax = r;
+                lmax = l;
             }
         }
 
