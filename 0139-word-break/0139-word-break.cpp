94b8a7c8adb2be60867400a12d52ea955536c0f9
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-24 23:13:13 +0800

    Time: 0 ms (100%), Space: 10.2 MB (99.05%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,38 @@
+// Approach: DP
+// dp[i] means if the substring between 0 to i is valid (True/False)
+// dp[i] = (s[(i - l_w1 + 1) ~ i] in wordset && dp[i - l_w1]) ||
+//         (s[(i - l_w2 + 1) ~ i] in wordset && dp[i - l_w2]) ...
+// Initialization: dp(n + 1); dp[0] = True
+
+// Time:
+// Space: O(n)
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        if (n == 0) return true;
+
+        vector<bool> dp(n + 1, false); // dp table is 1-indexed
+        dp[0] = true;
+
+        for (int i = 0; i < n; i++) { // index for s
+            for (string& w: wordDict) {
+                int n_letters = w.size();
+                if (n_letters > i + 1) continue;
+                bool is_valid_suffix = s.substr(i - n_letters + 1, n_letters) == w;
+                bool is_valid = dp[i - n_letters + 1] && is_valid_suffix;
+                dp[i + 1] = dp[i + 1] || is_valid;
+                if (is_valid) break;
+            }
+        }
+        return dp[n];
+    }
+};
+
+// l e e t c o d e
+// 0 1 2 3 4 5 6 7
+
+// dp = t f f f t f f f f
+//      0 1 2 3 4 5 6 7 8
+
