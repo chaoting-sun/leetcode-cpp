89baaff0ac5dbf6c5d299051206d6fec43dddf61
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-24 15:05:01 +0800

    Time: 0 ms (100%), Space: 9 MB (44.95%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -3,41 +3,30 @@ public:
     // Approach: DP
     // def. of dp[i]: the number of decode ways from 0 to i of s
     // initialization: dp[0] = 0, dp[1] = if s[0] is valid
+    // state transition function: dp[i] =
+    //      dp[i - 1] if si is valid else 0
+    //      +
+    //      dp[i - 2] if s(i-1)~si is valid else 0
 
-    bool isValid(string& s) {
-        int n = s.size();
-        if (n == 1) {
-            return s[0] != '0';
-        } else {
-            if (s[0] == '0') return false;
-            int num = stoi(s);
-            return num >= 1 && num <= 26;
-        }
-    }
+    // Time: O(n)
+    // Space: O(n)
 
     int numDecodings(string s) {
         int n = s.size();
         if (n == 0) return 0;
 
         vector<int> dp(n + 1, 0);
-        string s1, s2;
-        
-        s1 = s.substr(0, 1);
-        dp[1] = isValid(s1) ? 1 : 0;
+        dp[0] = 1;
+        dp[1] = s[0] != '0' ? 1 : 0;
 
         for (int i = 2; i <= n; i++) {
-            s1 = s.substr(i - 1, 1);
-            s2 = s.substr(i - 2, 2);
-
-            if (isValid(s1) && dp[i - 1] != 0) {
+            if (s[i - 1] != '0') {
                 dp[i] += dp[i - 1];
             }
-            if (isValid(s2)) {
-                if (i == 2) {
-                    dp[i] += 1;
-                } else {
-                    dp[i] += dp[i - 2];
-                }
+
+            int two_digits = stoi(s.substr(i - 2, 2));
+            if (two_digits >= 10 && two_digits <= 26) {
+                dp[i] += dp[i - 2];
             }
         }
     
