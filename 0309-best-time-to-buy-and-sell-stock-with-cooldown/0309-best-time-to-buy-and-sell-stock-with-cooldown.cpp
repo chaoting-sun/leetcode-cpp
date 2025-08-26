10911c88ee48e79f98e8472112da333575ebd3d3
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-26 21:06:17 +0800

    Time: 0 ms (100%), Space: 14.8 MB (99.9%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -7,21 +7,46 @@ public:
     //  - held[i] = max(held[i-1], rest[i-1] - prices[i])
     //  - rest[i] = max(rest[i-1], sold[i-1])
 
+    // Time: O(n)
+    // Space: O(n)
+
+    // int maxProfit(vector<int>& prices) {
+    //     int n = prices.size();
+    //     vector<int> sold(n, 0), held(n, 0), rest(n, 0);
+
+    //     sold[0] = INT_MIN; // impossible condition
+    //     held[0] = -prices[0];
+    //     rest[0] = 0;
+
+    //     for (int i = 1; i < n; i++) {
+    //         sold[i] = held[i - 1] + prices[i];
+    //         held[i] = max(held[i - 1], rest[i - 1] - prices[i]);
+    //         rest[i] = max(rest[i - 1], sold[i - 1]);
+    //     }
+
+    //     return max(sold[n - 1], rest[n - 1]);
+    // }
+
+    // Time: O(n)
+    // Space: O(1)
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<int> sold(n, 0), held(n, 0), rest(n, 0);
+        if (n == 0) return 0;
 
-        sold[0] = INT_MIN; // impossible condition
-        held[0] = -prices[0];
-        rest[0] = 0;
+        int sold = INT_MIN;
+        int held = -prices[0];
+        int rest = 0;
 
         for (int i = 1; i < n; i++) {
-            sold[i] = held[i - 1] + prices[i];
-            held[i] = max(held[i - 1], rest[i - 1] - prices[i]);
-            rest[i] = max(rest[i - 1], sold[i - 1]);
+            int next_sold = held + prices[i];
+            held = max(held, rest - prices[i]);
+            rest = max(rest, sold);
+
+            sold = next_sold;
         }
 
-        return max(sold[n - 1], rest[n - 1]);
+        return max(sold, rest);
     }
 };
 
