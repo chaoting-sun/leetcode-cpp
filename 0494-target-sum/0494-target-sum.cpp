72d2178cff9641a1dfcfddb1c9a67a55d63110f7
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-27 21:47:32 +0800

    Time: 159 ms (35.28%), Space: 60.2 MB (10.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
-    int backtrack(vector<int>& nums, int target, int i, int curr_sum) {
+    // int backtrack(vector<int>& nums, int target, int i, int curr_sum) {
+    //     if (i == nums.size()) {
+    //         return curr_sum == target ? 1 : 0;
+    //     }
+
+    //     return backtrack(nums, target, i + 1, curr_sum + nums[i]) +
+    //            backtrack(nums, target, i + 1, curr_sum - nums[i]);
+    // }
+
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     return backtrack(nums, target, 0, 0);
+    // }
+
+    int backtrack(vector<int>& nums, int target, vector<unordered_map<int, int>>& memo, int i, int curr_sum) {
         if (i == nums.size()) {
             return curr_sum == target ? 1 : 0;
         }
 
-        return backtrack(nums, target, i + 1, curr_sum + nums[i]) +
-               backtrack(nums, target, i + 1, curr_sum - nums[i]);
+        if (memo[i].count(curr_sum)) return memo[i][curr_sum];
+        
+        memo[i][curr_sum] = backtrack(nums, target, memo, i + 1, curr_sum + nums[i]) +
+                            backtrack(nums, target, memo, i + 1, curr_sum - nums[i]);
+        return memo[i][curr_sum];
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        return backtrack(nums, target, 0, 0);
+        int n = nums.size();
+        vector<unordered_map<int, int>> memo(nums.size());
+        return backtrack(nums, target, memo, 0, 0);
     }
-
 
     // definition of dp[i][j]: the number of expressions we can build to target i from nums[0] to nums[j]
     // state transition function: dp[i][j] = dp[i][]
