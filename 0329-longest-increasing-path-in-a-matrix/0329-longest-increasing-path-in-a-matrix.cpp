bd86b792ab0d34bd1eee08e4a2235a690e534c77
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-28 07:47:18 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> x_dirs = { 0, 0, 1, -1 };
+    vector<int> y_dirs = { 1, -1, 0, 0 };
+    
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        if (m == 0 || n == 0) return 0;
+
+        int max_step = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                max_step = max(max_step, dfs(matrix, i, j));
+            }
+        }
+
+        return max_step;
+    }
+
+    int dfs(vector<vector<int>>& matrix, int i, int j) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int max_step = 0;
+        for (int d = 0; d < 4; d++) {
+            int newi = i + x_dirs[d];
+            int newj = j + y_dirs[d];
+            if (newi >= 0 && newi < m && newj >= 0 && newj < n && matrix[newi][newj] > matrix[i][j]) {
+                max_step = max(max_step, dfs(matrix, newi, newj));
+            }
+        }
+        max_step++;
+
+        return max_step;
+    }
+};
