28b8871017db65129d78a725b26f78bf7a809fd0
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-30 17:03:59 +0800

    Time: 0 ms (100%), Space: 21.6 MB (74.04%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -5,6 +5,9 @@ public:
     // - overlapping intervals with newInterval
     // - non-overlapping intervals at the right of newInterval
 
+    // Time: O(n)
+    // Space: O(1)
+
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n = intervals.size();
         int i = 0;
@@ -14,26 +17,18 @@ public:
         vector<vector<int>> ans;
 
         // 1st case
-        while (i < n) {
-            if (intervals[i][1] < newInterval[0]) {
-                ans.push_back(intervals[i]);
-                i++;
-            } else {
-                break;
-            }
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            ans.push_back(intervals[i]);
+            i++;
         }
 
         // 2nd case
         int min_left = newInterval[0];
         int max_right = newInterval[1];
-        while (i < n) {
-            if (intervals[i][0] <= newInterval[1]) {
-                min_left = min(min_left, intervals[i][0]);
-                max_right = max(max_right, intervals[i][1]);
-                i++;
-            } else {
-                break;
-            }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            min_left = min(min_left, intervals[i][0]);
+            max_right = max(max_right, intervals[i][1]);
+            i++;
         }
         ans.push_back({ min_left, max_right });
 
