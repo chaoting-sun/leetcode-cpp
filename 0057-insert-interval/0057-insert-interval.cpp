da89e1a04d5fae66168ad61f45ef152ac8d9dacc
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-08-30 16:42:57 +0800

    Time: 0 ms (100%), Space: 21.7 MB (44.74%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    // Approach: handle 3 cases
+    // - non-overlapping intervals at the left of newInterval
+    // - overlapping intervals with newInterval
+    // - non-overlapping intervals at the right of newInterval
+
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size();
+        int i = 0;
+
+        if (n == 0) return { newInterval };
+
+        vector<vector<int>> ans;
+
+        // 1st case
+        while (i < n) {
+            if (intervals[i][1] < newInterval[0]) {
+                ans.push_back(intervals[i]);
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        // 2nd case
+        int min_left = newInterval[0];
+        int max_right = newInterval[1];
+        while (i < n) {
+            if (intervals[i][0] <= newInterval[1]) {
+                min_left = min(min_left, intervals[i][0]);
+                max_right = max(max_right, intervals[i][1]);
+                i++;
+            } else {
+                break;
+            }
+        }
+        ans.push_back({ min_left, max_right });
+
+        // 3rd case
+        while (i < n) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        return ans;
+    }
+};
