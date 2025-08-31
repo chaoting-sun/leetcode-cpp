30b9771f4d671d97cdbaa6fee4a1b6ab47705006
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-01 07:37:32 +0800

    Time: 0 ms (100%), Space: 20.5 MB (63.18%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,28 +1,54 @@
 class Solution {
 public:
-    int backtrack(vector<int>& nums, int start, vector<int> &memo) {
-        int n = nums.size();
+    // Approach: Backtracking + Memoization
+    // Time: O(n^2)
+    // Space: O(n)
 
-        // base case
-        if (start == n - 1) return 0;
-        if (nums[start] == 0) return INT_MAX; // unreachable
-    
-        if (memo[start] != -1) return memo[start];
+    // int backtrack(vector<int>& nums, int start, vector<int> &memo) {
+    //     int n = nums.size();
 
-        int min_length = INT_MAX;
-        int furthest_jump = min(n - 1, start + nums[start]);
-        for (int end = start + 1; end <= furthest_jump; end++) {
-            int curr_length = backtrack(nums, end, memo);
-            if (curr_length != INT_MAX) {
-                min_length = min(min_length, 1 + curr_length);
-            }
-        }
-        return memo[start] = min_length;
-    }
+    //     // base case
+    //     if (start == n - 1) return 0;
+    //     if (nums[start] == 0) return INT_MAX; // unreachable
     
+    //     if (memo[start] != -1) return memo[start];
+
+    //     int min_length = INT_MAX; // unreachable
+    //     int furthest_jump = min(n - 1, start + nums[start]);
+    //     for (int end = start + 1; end <= furthest_jump; end++) {
+    //         int curr_length = backtrack(nums, end, memo);
+    //         if (curr_length != INT_MAX) {
+    //             min_length = min(min_length, 1 + curr_length);
+    //         }
+    //     }
+    //     return memo[start] = min_length;
+    // }
+    
+    // int jump(vector<int>& nums) {
+    //     vector<int> memo(nums.size(), -1); // -1 -> not computed, INT_MAX -> unreachable
+    //     int min_length = backtrack(nums, 0, memo);
+    //     return min_length == INT_MAX ? 0 : min_length;
+    // }
+
     int jump(vector<int>& nums) {
-        vector<int> memo(nums.size(), -1); // -1 -> not computed
-        int min_length = backtrack(nums, 0, memo);
-        return min_length == INT_MAX ? 0 : min_length;
+        int n = nums.size();
+        int l = 0, r = 0;
+        int ans = 0;
+
+        while (r < n - 1) {
+            int tmpr = INT_MIN;
+            for (int i = l; i <= r; i++) {
+                int furthest_jump = min(i + nums[i], n - 1);
+                tmpr = max(tmpr, furthest_jump);
+            }
+            
+            if (tmpr == r) return 0;
+            
+            l = r + 1;
+            r = tmpr;
+            ans++;
+        }
+        return ans;
     }
+
 };
