bbed086f9427544e03fe21f3e9af891a07a39a65
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-02 20:48:02 +0800

    Time: 0 ms (100%), Space: 9 MB (45.57%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
+    // Approach: Greedy
+    // any same characters should be in the same group, so if there is a string like a....a...,
+    // the a....a should be in the same group. if there is a character c in the center and another
+    // c at the right, like a..c..a..c.., then a..c..a..c should be in the same group.
+    // in other word, if a substring form a group, for each character appears more than once, their
+    // maximum position should be bounded in the group.
+    // So, the method is to store the maximum position of the characters in a hash map max_ids.
+    // we start from index = 0, and set end as the max_ids[start]. then we keep looping from start
+    // to the end and keeps updating the end. when start == end. then all existing characters should
+    // be bounded in this group. then we set start = end + 1 for the next round.
+    
+    // Time: O(n)
+    // Space: O(k). k is the number of distinct characters in s
+
     vector<int> partitionLabels(string s) {
         int n = s.size();
         if (n == 0) return {};
         
-        unordered_map<int, int> min_ids, max_ids;
+        unordered_map<int, int> max_ids;
 
         for (int i = 0; i < n; i++) {
-            char c = s[i];
-            if (!min_ids.count(c)) min_ids[c] = i;
-            max_ids[c] = i;
+            max_ids[s[i]] = i;
         }
 
         vector<int> ans;
