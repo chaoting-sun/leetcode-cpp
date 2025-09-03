e15bc9a87aea392140adcd4a9339732401343638
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-04 07:51:18 +0800

    Time: 83 ms (63.45%), Space: 44.2 MB (83.11%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,31 +1,69 @@
 class Solution {
 public:
+    // Bellman-Ford Algorithm
+    // Time: O(EV)
+    // Space: O(V)
+
+    // int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+    //     vector<int> distance(n + 1, INT_MAX);
+
+    //     distance[k] = 0;
+
+    //     for (int i = 0; i < n - 1; i++) {
+    //         for (vector<int>& time: times) {
+    //             int u = time[0];
+    //             int v = time[1];
+    //             int w = time[2];
+
+    //             if (distance[u] != INT_MAX && distance[v] > distance[u] + w) {
+    //                 distance[v] = distance[u] + w;
+    //             }
+    //         }
+    //     }
+
+    //     int min_time = INT_MIN;
+    //     for (int i = 1; i <= n; i++) {
+    //         // unreachable
+    //         if (distance[i] == INT_MAX) {
+    //             return -1;
+    //         }
+    //         min_time = max(min_time, distance[i]);
+    //     }
+
+    //     return min_time;
+    // }
+
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<long long> distance(n + 1, INT_MAX);
+        vector<vector<pair<int,int>>> adj(n + 1);
+        for (vector<int> &time: times) {
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
+            adj[u].push_back({ v, w });
+        }
+
+        priority_queue<pair<int,int>> pq;
+        vector<int> distance(n + 1, INT_MAX);
 
         distance[k] = 0;
+        pq.push({ k, 0 });
 
-        for (int i = 0; i < n - 1; i++) {
-            for (vector<int>& time: times) {
-                int u = time[0];
-                int v = time[1];
-                int w = time[2];
-
-                if (distance[u] != INT_MAX && distance[v] > distance[u] + w) {
-                    distance[v] = distance[u] + w;
+        while (!pq.empty()) {
+            auto [u, d] = pq.top();
+            pq.pop();
+            for (auto& [v, w]: adj[u]) {
+                if (d + w < distance[v]) {
+                    distance[v] = d + w;
+                    pq.push({ v, d + w });
                 }
             }
         }
 
-        int min_time = INT_MIN;
+        int max_time = 0;
         for (int i = 1; i <= n; i++) {
-            // unreachable
-            if (distance[i] == INT_MAX) {
-                return -1;
-            }
-            min_time = max(min_time, (int)distance[i]);
+            if (distance[i] == INT_MAX) return -1;
+            max_time = max(max_time, distance[i]);
         }
-
-        return min_time;
+        return max_time;
     }
 };
