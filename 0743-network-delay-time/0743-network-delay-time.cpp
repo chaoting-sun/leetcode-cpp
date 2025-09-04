76f6376dd9c5f7f9a31e9e2da9486dbc1373c618
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-04 08:13:14 +0800

    Time: 74 ms (89.91%), Space: 44.4 MB (56.75%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -42,7 +42,7 @@ public:
             adj[u].push_back({ v, w });
         }
 
-        priority_queue<pair<int,int>> pq;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         vector<int> distance(n + 1, INT_MAX);
 
         distance[k] = 0;
