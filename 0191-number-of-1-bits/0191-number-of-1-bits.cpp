6f5602f6d87e2c89d3ae4a130af6994aeac5e3f7
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-06 17:09:22 +0800

    Time: 0 ms (100%), Space: 8.2 MB (47.83%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,11 +3,23 @@ public:
     // Approach: Bit Manipulation
     // AND: a & 1 = the bit of LSB
 
+    // int hammingWeight(int n) {
+    //     int count = 0;
+    //     while (n > 0) {
+    //         count += n & 1;
+    //         n = n >> 1;
+    //     }
+    //     return count;
+    // }
+
+    // Approach: Bit Manipulation
+    // n ^ (n - 1) removes the lowest set bit
+    
     int hammingWeight(int n) {
         int count = 0;
         while (n > 0) {
-            count += n & 1;
-            n = n >> 1;
+            n &= n - 1;
+            count++;
         }
         return count;
     }
