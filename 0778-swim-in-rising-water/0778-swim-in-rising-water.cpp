eb2d6a27741c2fd5eec583b32bd7cb3f0d3807b4
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-06 15:05:27 +0800

    Time: 14 ms (29.47%), Space: 14.8 MB (29.51%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -20,45 +20,93 @@ public:
     // Once we pop the destination cell (m-1,n-1), the current `time` is the answer.
     //
     // Complexity:
-    //   Time:  O(m*n * log(m*n)) due to heap operations
+    //   Time:  O(m*n * log(m*n)) due to heap operations. if m = n then O(n^2*logn)
     //   Space: O(m*n) for visited states and heap storage
+
+    // int swimInWater(vector<vector<int>>& grid) {
+    //     int m = grid.size();
+    //     int n = grid[0].size();
+
+    //     using unit = pair<int,int>;
+    //     priority_queue<unit, vector<unit>, greater<unit>> pq;
+    //     vector<vector<bool>> visited(m, vector<bool>(n, false));
+        
+    //     vector<int> i_diff { -1, 1, 0, 0 }, j_diff { 0, 0, -1, 1 };
+
+    //     int time = 0;
+    //     pq.push({ grid[0][0], 0 });
+
+    //     while (!pq.empty()) {
+    //         auto [h, pos] = pq.top();
+    //         pq.pop();
+    //         int ci = pos / m, cj = pos % n;
+    //         visited[ci][cj] = true;
+            
+    //         if (h > time) {
+    //             time = h;           
+    //         }
+
+    //         // termination condition
+    //         if (ci == m - 1 && cj == n - 1) return time;
+
+    //         for (int p = 0; p < 4; p++) {
+    //             int ni = ci + i_diff[p];
+    //             int nj = cj + j_diff[p];
+    //             if (ni >= 0 && ni < m && nj >= 0 && nj < n && !visited[ni][nj]) {
+    //                 pq.push({ grid[ni][nj], { ni * m + nj } });
+    //                 visited[ni][nj] = true;
+    //             }
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+    vector<int> i_diff { -1, 1, 0, 0 }, j_diff { 0, 0, -1, 1 };
 
     int swimInWater(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        using unit = pair<int,int>;
-        priority_queue<unit, vector<unit>, greater<unit>> pq;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        
-        vector<int> i_diff { -1, 1, 0, 0 }, j_diff { 0, 0, -1, 1 };
+        int left = 0, right = m * n - 1;
 
-        int time = 0;
-        pq.push({ grid[0][0], 0 });
-
-        while (!pq.empty()) {
-            auto [h, pos] = pq.top();
-            pq.pop();
-            int ci = pos / m, cj = pos % n;
-            visited[ci][cj] = true;
-            
-            if (h > time) {
-                time = h;           
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isReachable(grid, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
+        }
+        return right;
+    }
 
-            // termination condition
-            if (ci == m - 1 && cj == n - 1) return time;
+    bool isReachable(vector<vector<int>>& grid, int time) {
+        if (grid[0][0] > time) return false;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        stack<pair<int,int>> stk;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        stk.push({ 0, 0 });
+
+        while (!stk.empty()) {
+            auto [ci, cj] = stk.top();
+            stk.pop();
+            
+            if (ci == m - 1 && cj == n - 1) return true;
+
+            visited[ci][cj] = true;
 
             for (int p = 0; p < 4; p++) {
                 int ni = ci + i_diff[p];
                 int nj = cj + j_diff[p];
-                if (ni >= 0 && ni < m && nj >= 0 && nj < n && !visited[ni][nj]) {
-                    pq.push({ grid[ni][nj], { ni * m + nj } });
-                    visited[ni][nj] = true;
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && !visited[ni][nj] && grid[ni][nj] <= time) {
+                    stk.push({ ni, nj });
                 }
             }
         }
 
-        return -1;
+        return false;
     }
 };
