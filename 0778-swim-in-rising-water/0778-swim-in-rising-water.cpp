3aa7eacfb922ae09e41e6dea1a1adf8a222b4ef1
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-06 15:24:32 +0800

    Time: 4 ms (78.28%), Space: 13.2 MB (63.89%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -62,51 +62,118 @@ public:
     //     return -1;
     // }
 
+    // Approach: Binary Search
+    // Intuition: 這題的 grid[i][j] 有給明確的範圍，0 <= grid[i][j] < n^2，所以可以利用 binary search 嘗試
+    // 最小但可以從起點走到終點的時間，這與常見的 binary search 題目中常見的問題 "尋找第一個不小於給定的數" 基本上一樣。
+    // 要辨別能不能在給定的時間內走到，可以用 DFS
+    // Time: O(mn*log(mn)) = O(n^2*logn).
+    // Space: O(mn) = O(n^2)
+
+    // vector<int> i_diff { -1, 1, 0, 0 }, j_diff { 0, 0, -1, 1 };
+
+    // int swimInWater(vector<vector<int>>& grid) {
+    //     int m = grid.size();
+    //     int n = grid[0].size();
+
+    //     int left = 0, right = m * n - 1;
+
+    //     while (left < right) {
+    //         int mid = left + (right - left) / 2;
+    //         if (isReachable(grid, mid)) {
+    //             right = mid;
+    //         } else {
+    //             left = mid + 1;
+    //         }
+    //     }
+    //     return right;
+    // }
+
+    // bool isReachable(vector<vector<int>>& grid, int time) {
+    //     if (grid[0][0] > time) return false;
+    //     int m = grid.size();
+    //     int n = grid[0].size();
+
+    //     stack<pair<int,int>> stk;
+    //     vector<vector<bool>> visited(m, vector<bool>(n, false));
+    //     stk.push({ 0, 0 });
+
+    //     while (!stk.empty()) {
+    //         auto [ci, cj] = stk.top();
+    //         stk.pop();
+            
+    //         if (ci == m - 1 && cj == n - 1) return true;
+
+    //         visited[ci][cj] = true;
+
+    //         for (int p = 0; p < 4; p++) {
+    //             int ni = ci + i_diff[p];
+    //             int nj = cj + j_diff[p];
+    //             if (ni >= 0 && ni < m && nj >= 0 && nj < n && !visited[ni][nj] && grid[ni][nj] <= time) {
+    //                 stk.push({ ni, nj });
+    //             }
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
+    // Kruskal's Algorithm
+
+    vector<int> parent;
+    vector<int> size;
     vector<int> i_diff { -1, 1, 0, 0 }, j_diff { 0, 0, -1, 1 };
+
+    int find_set(int v) {
+        if (parent[v] == v) return v;
+        return parent[v] = find_set(parent[v]);
+    }
+
+    void union_sets(int u, int v) {
+        u = find_set(u);
+        v = find_set(v);
+        if (u != v) {
+            if (size[u] > size[v]) swap(u, v);
+            parent[u] = v;
+            size[v] += size[u];
+        }
+    }
 
     int swimInWater(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        int n_cells = m * n;
 
-        int left = 0, right = m * n - 1;
+        parent.resize(n_cells, 0);
+        size.resize(n_cells, 1);
+        for (int i = 0; i < n_cells; i++) parent[i] = i;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (isReachable(grid, mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return right;
-    }
+        vector<pair<int, int>> nodes(n_cells);
 
-    bool isReachable(vector<vector<int>>& grid, int time) {
-        if (grid[0][0] > time) return false;
-        int m = grid.size();
-        int n = grid[0].size();
+        for (int i = 0; i < n_cells; i++) nodes[i] = { grid[i / n][i % n], i };
+        sort(nodes.begin(), nodes.end(), [&](pair<int,int> &a, pair<int,int> &b) {
+            return a.first < b.first;
+        });
 
-        stack<pair<int,int>> stk;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        stk.push({ 0, 0 });
+        vector<vector<bool>> opened(m, vector<bool>(n, false));
 
-        while (!stk.empty()) {
-            auto [ci, cj] = stk.top();
-            stk.pop();
+        for (int i = 0; i < n_cells; i++) {
+            auto [h, pos] = nodes[i];
+            int ci = pos / n;
+            int cj = pos % n;
             
-            if (ci == m - 1 && cj == n - 1) return true;
-
-            visited[ci][cj] = true;
-
+            opened[ci][cj] = true;
+            
             for (int p = 0; p < 4; p++) {
                 int ni = ci + i_diff[p];
                 int nj = cj + j_diff[p];
-                if (ni >= 0 && ni < m && nj >= 0 && nj < n && !visited[ni][nj] && grid[ni][nj] <= time) {
-                    stk.push({ ni, nj });
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && opened[ni][nj]) {
+                    union_sets(ci * n + cj, ni * n + nj);
                 }
             }
+
+            if (find_set(0) == find_set(n_cells - 1)) return h;
         }
 
-        return false;
+        return -1;
     }
 };
