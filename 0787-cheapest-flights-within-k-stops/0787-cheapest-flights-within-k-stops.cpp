f42ba417c68601d4a1791041049c32f4a07af920
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-07 21:42:38 +0800

    Time: 9 ms (15.62%), Space: 19.9 MB (10.34%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,24 +1,73 @@
 class Solution {
 public:
+    // Approach: Bellman-Ford Algorithm
+    
+    // “At most k stops” ⇒ at most k+1 edges (flights).
+    // Bellman–Ford guarantees: after the i-th round, distances use ≤ i edges.
+    // So we run k+1 rounds (instead of V-1) and, in each round, we must read from the previous layer and write into a new layer.
+    // This prevents chaining multiple new edges within the same round.
+    // Implement this by keeping dist (read-only for the round) and next (the updated distances for this round). Initialize next = dist each round.
+
+    // Time: O(V + E)
+    // Space: O(V)
+
+    // int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    //     vector<int> dist(n, INT_MAX);
+
+    //     dist[src] = 0;
+
+    //     for (int i = 0; i < k + 1; i++) {
+    //         // Time: O(V)
+    //         vector<int> updated_dist(dist);
+            
+    //         // Time: O(E)
+    //         for (vector<int>& flight: flights) {
+    //             int u = flight[0], v = flight[1], w = flight[2];
+    //             if (dist[u] != INT_MAX && dist[u] + w < updated_dist[v]) {
+    //                 updated_dist[v] = dist[u] + w;
+    //             }
+    //         }
+
+    //         dist = updated_dist;
+    //     }
+
+    //     return dist[dst] == INT_MAX ? -1 : dist[dst];
+    // }
+
+    // Approach: Dijkstra's Algorithm
+
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist(n, INT_MAX);
-
-        dist[src] = 0;
-
-        for (int i = 0; i < k + 1; i++) {
-            vector<int> updated_dist(dist);
-
-            for (vector<int>& flight: flights) {
-                int u = flight[0], v = flight[1], w = flight[2];
-                if (dist[u] != INT_MAX && dist[u] + w < updated_dist[v]) {
-                    updated_dist[v] = dist[u] + w;
-                }
-            }
-
-            dist = updated_dist;
+        vector<vector<pair<int,int>>> adj(n);
+        for (auto flight: flights) {
+            int u = flight[0], v = flight[1], w = flight[2];
+            adj[u].push_back({ v, w });
         }
 
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+        using State = array<int, 3>; // 3 elements: cost, vertex, times
+        priority_queue<State, vector<State>, greater<State>> pq;
+        vector<vector<int>> best_cost(n, vector<int>(k + 2, INT_MAX));
+
+        best_cost[src][0] = 0;
+        pq.push({ 0, src, 0 });
+
+        while (!pq.empty()) {
+            auto [dist, u, time] = pq.top();
+            pq.pop();
+            
+            if (u == dst) return dist;
+            if (dist != best_cost[u][time]) continue;
+            if (time == k + 1) continue;
+
+            int next_time = time + 1;
+            for (auto [v, w]: adj[u]) {
+                if (dist + w < best_cost[v][next_time]) {
+                    best_cost[v][next_time] = dist + w;
+                    pq.push({ best_cost[v][next_time], v, next_time });
+                }
+            }
+        }
+
+        return -1;
     }
 };
 
