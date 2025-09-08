a954d2f6c221b4b6c74e9f6ef63269c76141caa3
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-08 22:09:11 +0800

    Time: 0 ms (100%), Space: 19.4 MB (86.75%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -14,64 +14,49 @@ public:
     // Time: O(n+m), where n, m are the length of list1 and list2
     // Space: O(1)
 
-    // ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    //     if (!list1) return list2;
-    //     if (!list2) return list1;
-        
-    //     ListNode *curr1 = list1, *curr2 = list2;
-    //     ListNode *head, *tail;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *ptr1 = list1, *ptr2 = list2;
+        ListNode *dummy = new ListNode();
+        ListNode *curr = dummy;
 
-    //     if (curr1->val < curr2->val) {
-    //         head = curr1;
-    //         tail = curr1;
-    //         curr1 = curr1->next;
-    //     } else {
-    //         head = curr2;
-    //         tail = curr2;
-    //         curr2 = curr2->next;
-    //     }
+        while (ptr1 || ptr2) {
+            if (!ptr1) {
+                curr->next = ptr2;
+                break;
+            } else if (!ptr2) {
+                curr->next = ptr1;
+                break;
+            } else {
+                if (ptr1->val < ptr2->val) {
+                    curr->next = ptr1;
+                    ptr1 = ptr1->next;
+                } else {
+                    curr->next = ptr2;
+                    ptr2 = ptr2->next;
+                }
+                curr = curr->next;
+            }
+        }
 
-    //     while (curr1 || curr2) {
-    //         if (!curr1) {
-    //             tail->next = curr2;
-    //             break;
-    //         }
-    //         if (!curr2) {
-    //             tail->next = curr1;
-    //             break;
-    //         }
-            
-    //         if (curr1->val < curr2->val) {
-    //             ListNode* tmp = curr1->next;
-    //             tail->next = curr1;
-    //             curr1 = tmp;
-    //         } else {
-    //             ListNode* tmp = curr2->next;
-    //             tail->next = curr2;
-    //             curr2 = tmp;
-    //         }
-
-    //         tail = tail->next;
-    //         tail->next = nullptr;
-    //     }
-
-    //     return head;
-    // }
+        return dummy->next;
+    }
 
     // Approach2: Recursion
     // Time: O(n+m)
     // Space: O(n+m)
 
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (!list1) return list2;
-        if (!list2) return list1;
-        
-        if (list1->val < list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        } else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
-        }
-    }
+    // ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    //     if (!list1)
+    //         return list2;
+    //     if (!list2)
+    //         return list1;
+
+    //     if (list1->val < list2->val) {
+    //         list1->next = mergeTwoLists(list1->next, list2);
+    //         return list1;
+    //     } else {
+    //         list2->next = mergeTwoLists(list1, list2->next);
+    //         return list2;
+    //     }
+    // }
 };
