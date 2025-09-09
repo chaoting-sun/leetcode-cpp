60465e4686404c90c725b802e5b16754f5a6c317
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-09 20:45:49 +0800

    Time: 197 ms (8.77%), Space: 159.6 MB (28.88%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -6,18 +6,15 @@ public:
 
     long long interchangeableRectangles(vector<vector<int>>& rectangles) {
         unordered_map<string, int> value_counts;// width/gcd,height/gcd: counts
+        long long count = 0;
         for (auto& rec: rectangles) {
             int _gcd = gcd(rec[0], rec[1]);
             int new_height = rec[0] / _gcd;
             int new_width = rec[1] / _gcd;
 
             string key = to_string(new_height) + "," + to_string(new_width);
+            if (value_counts.count(key)) count += value_counts[key];
             value_counts[key]++;
-        }
-
-        long long count = 0;
-        for (auto [_, current_count]: value_counts) {
-            count += (long long)current_count * (current_count - 1) / 2;
         }
         return count;
     }
