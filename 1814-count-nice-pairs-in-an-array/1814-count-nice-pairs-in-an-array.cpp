f915970aaaadd42914a9bd1e7e61cb2f30db5ccd
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-09 21:57:13 +0800

    Time: 27 ms (32.53%), Space: 60.7 MB (94.76%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -5,31 +5,28 @@ public:
     // find the same computed result of nums[i] + rev(nums[j])
 
     int reverse(int num) {
-        if (num == 0) return 0;
-        while (num % 10 == 0) num /= 10;
-        int digits = 0;
-        while (pow(10, digits) <= num) digits++;
         int rev = 0;
         while (num > 0) {
-            rev += (num % 10) * (int)pow(10, digits - 1);
+            rev = rev * 10 + num % 10;
             num /= 10;
-            digits--;
         }
         return rev;
     }
 
     int countNicePairs(vector<int>& nums) {
         unordered_map<int, int> value_count; // value: count
+        const int MOD = 1e9 + 7;
+        int count = 0;        
         for (int i = 0; i < (int)nums.size(); i++) {
             int result = nums[i] - reverse(nums[i]);
+            if (value_count.count(result)) {
+                count += value_count[result];
+                count %= MOD;
+            }
             value_count[result]++;
         }
-        long long count = 0;
-        for (auto [_, current_count]: value_count) {
-            count += (long long)current_count * (current_count - 1) / 2;
-            count %= (long long)pow(10, 9) + 7;
-        }
-        return (int)count;
+
+        return count;
     }
 };
 
