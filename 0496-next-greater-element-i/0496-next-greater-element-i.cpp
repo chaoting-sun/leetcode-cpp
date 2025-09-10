9c99eeac80cc71382dbe65f17ea16dbdb778d908
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-10 21:49:24 +0800

    Time: 4 ms (24.61%), Space: 12.7 MB (74.15%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    // Approach: Monotonic Stack
+    // Time: O(n1 + n2)
+    // Space: O(n2)
+
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+
+        stack<int> value_stack;
+        vector<int> nge(n1, -1);
+        unordered_map<int,int> nge_map;
+
+        for (int i = 0; i < n2; i++) {
+            while (!value_stack.empty() && value_stack.top() < nums2[i]) {
+                nge_map[value_stack.top()] = nums2[i];
+                value_stack.pop();
+            }
+            value_stack.push(nums2[i]);
+        }
+
+        for (int i = 0; i < n1; i++) {
+            if (nge_map.count(nums1[i])) {
+                nge[i] = nge_map[nums1[i]];
+            }
+        }
+
+        return nge;
+    }
+};
