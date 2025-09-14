64fbe14e0bff54f8576a80fb2cd5a7639de4aba8
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-14 22:04:30 +0800

    Time: 13 ms (25%), Space: 158.2 MB (16.67%) - LeetHub

--- 3686-number-of-stable-subsequences/3686-number-of-stable-subsequences.cpp
+++ 3686-number-of-stable-subsequences/3686-number-of-stable-subsequences.cpp
@@ -1,33 +1,33 @@
 int M = 1e9 + 7;
+using LL = long long;
 
 class Solution {
 public:
     int countStableSubsequences(vector<int>& nums) {
         int n = nums.size();
-        int e1 = 0;
-        int e2 = 0;
-        int o1 = 0;
-        int o2 = 0;
+        LL e1 = 0; // # sequences that ends with 1 even number
+        LL e2 = 0; // # sequences that ends with 2 even numbers
+        LL o1 = 0; // # sequences that ends with 1 odd number
+        LL o2 = 0; // # sequences that ends with 2 odd numbers
 
         for (int i = 0; i < n; i++) {
             // odd
             if (nums[i] % 2 == 1) {
-                // choose
-                o2 += o1;
-                o1 += (e1 + e2 + 1) % M;
-                o1 %= M;
-                o2 %= M;
+                LL next_o1 = (o1 + (e1 + e2 + 1)) % M;
+                LL next_o2 = (o1 + o2) % M;
+                o1 = next_o1;
+                o2 = next_o2;
             }
             // even
             else {
-                e2 += e1;
-                e1 += (o1 + o2 + 1) % M;
-                e1 %= M;
-                e2 %= M;
+                LL next_e1 = (e1 + (o1 + o2 + 1)) % M;
+                LL next_e2 = (e1 + e2) % M;
+                e1 = next_e1;
+                e2 = next_e2;
             }
         }
 
-        return ((o1 + o2) % M + (e1 + e2) % M) % M;
+        return (int)((o1 + o2 + e1 + e2) % M);
     }
 };
 
