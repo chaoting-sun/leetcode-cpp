fa1256942f703bf4b495763c7e236391f7e86ad1
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-16 20:19:42 +0800

    Time: 0 ms (100%), Space: 9.4 MB (97.14%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -4,25 +4,27 @@ public:
         int n = s.size();
         if (n == 0 || k == 0) return 0;
         
-        unordered_map<char, int> charCount;
+        vector<int> charCount(128, 0);
         int maxLength = 1;
         int start = 0, end = 0;
+        int distinctCharacters = 0;
         
         while (end < n) {
+            if (charCount[s[end]] == 0) distinctCharacters++;
             charCount[s[end]]++;
-            if (charCount.size() > k) {
-                maxLength = max(maxLength, end - start);
-                while (charCount.size() > k) {
-                    charCount[s[start]]--;
-                    if (charCount[s[start]] == 0) {
-                        charCount.erase(s[start]);
-                    }
-                    start++;
+
+            while (distinctCharacters > k) {
+                charCount[s[start]]--;
+                if (charCount[s[start]] == 0) {
+                    distinctCharacters--;
                 }
+                start++;
             }
+
+            maxLength = max(maxLength, end - start + 1);
             end++;
         }
     
-        return max(maxLength, end - start);
+        return maxLength;
     }
 };
