17fbe9b05b18a0548b3dc2b7a327d594bb80b91d
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-16 23:08:10 +0800

    Time: 40 ms (60.17%), Space: 22.3 MB (85.38%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+    // Approach: Sliding Window
+    // Time: O((N-L)*L)
+    // Space: O((N-L)*L)
+
     vector<string> findRepeatedDnaSequences(string s) {
+        int L = 10;
         unordered_set<string> seen, repeatedPattern;
-        for (int i = 0; i < (int)s.size() - 9; i++) {
-            string pattern = s.substr(i, 10);
+
+        for (int i = 0; i < (int)s.size() - L + 1; i++) { // N - L
+            string pattern = s.substr(i, 10); // L
             if (seen.count(pattern)) {
                 repeatedPattern.insert(pattern);
             } else {
