79071017f5f7b2fc91482bc1655a3c4b3cad8f89
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-17 07:43:08 +0800

    Time: 27 ms (97.78%), Space: 18.3 MB (95.49%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,69 +1,63 @@
-// class Solution {
-// public:
-//     // Approach: Sliding Window
-//     // Time: O((N-L)*L)
-//     // Space: O((N-L)*L)
-
-//     vector<string> findRepeatedDnaSequences(string s) {
-//         int L = 10;
-//         unordered_set<string> seen, repeatedPattern;
-
-//         for (int i = 0; i < (int)s.size() - L + 1; i++) { // N - L
-//             string pattern = s.substr(i, 10); // L
-//             if (seen.count(pattern)) {
-//                 repeatedPattern.insert(pattern);
-//             } else {
-//                 seen.insert(pattern);
-//             }
-//         }
-
-//         vector<string> ans;
-//         for (string pattern: repeatedPattern) ans.push_back(pattern);
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
+    // Approach: Sliding Window
+    // Time: O((N-L)*L)
+    // Space: O((N-L)*L)
+
+    // vector<string> findRepeatedDnaSequences(string s) {
+    //     int L = 10;
+    //     unordered_set<string> seen, repeatedPattern;
+
+    //     for (int i = 0; i < (int)s.size() - L + 1; i++) { // N - L
+    //         string pattern = s.substr(i, 10); // L
+    //         if (seen.count(pattern)) {
+    //             repeatedPattern.insert(pattern);
+    //         } else {
+    //             seen.insert(pattern);
+    //         }
+    //     }
+
+    //     vector<string> ans;
+    //     for (string pattern: repeatedPattern) ans.push_back(pattern);
+    //     return ans;
+    // }
+
+    // Approach: Sliding Window with Bit Manipulation
+    // Time: O(L)
+    // Space: O(L)
+
     vector<string> findRepeatedDnaSequences(string s) {
-        const int L = 10;
-        const int BITS = 2 * L;          // 20 bits for a 10-char window
-        const int MASK = (1 << BITS) - 1;
-
-        vector<string> ans;
-        if ((int)s.size() < L) return ans;
-
-        // Map nucleotide to 2-bit code.
-        auto code2 = [](char ch) -> int {
-            switch (ch) {
-                case 'A': return 0; // 00
-                case 'C': return 1; // 01
-                case 'G': return 2; // 10
-                case 'T': return 3; // 11
-                default:  return 0; // assume valid input
-            }
+        int L = 10;
+        int bit = 2 * L;
+        int mask = (1 << bit) - 1;
+        unordered_map<char,int> char2bit = {
+            { 'A', 0 }, // 00
+            { 'T', 1 }, // 01
+            { 'C', 2 }, // 10
+            { 'G', 3 }, // 11
         };
 
-        int rolling = 0;                 // rolling 20-bit code of current window
-        unordered_set<int> seen;         // codes seen at least once
-        unordered_set<int> added;        // codes already added to ans
+        vector<string> ans;
+        int n = s.size();
+        if (n < L) return {};
 
-        // Build the first (up to) 9 chars
-        for (int i = 0; i < L - 1; ++i) {
-            rolling = ((rolling << 2) | code2(s[i])) & MASK;
+        int pattern = 0;
+        unordered_set<int> added; // pattern added before
+        unordered_set<int> seen; // used to avoid repetitive addition to the ans
+        
+        for (int i = 0; i < L - 1; i++) {
+            pattern = (pattern << 2 | char2bit[s[i]]) & mask;
         }
 
-        // Slide the window
-        for (int i = L - 1; i < (int)s.size(); ++i) {
-            rolling = ((rolling << 2) | code2(s[i])) & MASK;
-
-            if (seen.count(rolling)) {
-                if (!added.count(rolling)) {
+        for (int i = L - 1; i < n; i++) { // L
+            pattern = ((pattern << 2) | char2bit[s[i]]) & mask;
+            if (added.count(pattern)) {
+                if (!seen.count(pattern)) {
+                    seen.insert(pattern);
                     ans.push_back(s.substr(i - L + 1, L));
-                    added.insert(rolling);
                 }
             } else {
-                seen.insert(rolling);
+                added.insert(pattern);
             }
         }
 
