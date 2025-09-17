407a7f194ba2f4df00b3eca8cce548e6e564806a
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-17 23:45:28 +0800

    Time: 0 ms (100%), Space: 69.6 MB (53.45%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // Approach1: Sliding Window
+    // Time: O(n)
+    // Space: O(1)
+
     int longestOnes(vector<int>& nums, int k) {
         int n = nums.size();
         if (n == 0) return 0;
@@ -21,34 +25,3 @@ public:
         return maxLength;
     }
 };
-
-// class Solution {
-// public:
-//     // Approach1: Sliding Window
-//     // Time: O(n)
-//     // Space: O(1)
-
-//     int longestOnes(vector<int>& nums, int k) {
-//         int left = 0, right = 0;
-//         int max_cnt = 0;
-//         int n = nums.size();
-//         int one_cnt = 0; // number of 1s
-
-//         while (right < n) {
-//             // count the number of 1s
-//             if (nums[right] == 1) one_cnt++;
-            
-//             // ensure that the number of 0s is not larger then k
-//             while ((right - left + 1) - one_cnt > k) {
-//                 if (nums[left] == 1) one_cnt--;
-//                 left++;
-//             }
-            
-//             max_cnt = max(max_cnt, right - left + 1);
-            
-//             right++;
-//         }
-
-//         return max_cnt;
-//     }
-// };
