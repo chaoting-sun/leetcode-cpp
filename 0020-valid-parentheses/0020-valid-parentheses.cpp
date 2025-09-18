5f493b73a5b3282a5244a36038c3802d1b62348c
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-19 07:30:51 +0800

    Time: 0 ms (100%), Space: 10.2 MB (6.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
+    // Approach: Stack. use Stack to store the "(", "[", and "{" and pop them
+    // if get their pair
+    // Time: O(n)
+    // Space: O(n)
+
     bool isValid(string s) {
-        stack<char> stk;
-        for (int i = 0; i < (int)s.size(); i++) {
-            if (s[i] == '(' || s[i] == '[' || s[i] == '{' || stk.empty()) {
-                stk.push(s[i]);
+        stack<int> st;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push(s[i]);
             } else {
-                if (stk.top() == '(' && s[i] == ')') stk.pop();
-                else if (stk.top() == '[' && s[i] == ']') stk.pop();
-                else if (stk.top() == '{' && s[i] == '}') stk.pop();
-                else stk.push(s[i]);
+                if (st.size() == 0) return false;
+                if (!(st.top() == '(' && s[i] == ')' || st.top() == '[' && s[i] == ']' || st.top() == '{' && s[i] == '}'))
+                    return false;
+                st.pop();
             }
         }
-        return stk.empty(); 
+
+        return st.size() == 0;
     }
 };
-
-
-
-
-
-// class Solution {
-// public:
-//     // Approach: Stack. use Stack to store the "(", "[", and "{" and pop them
-//     // if get their pair
-//     // Time: O(n)
-//     // Space: O(n)
-
-//     bool isValid(string s) {
-//         stack<int> st;
-
-//         for (int i = 0; i < s.size(); i++) {
-//             if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-//                 st.push(s[i]);
-//             } else {
-//                 if (st.size() == 0) return false;
-//                 if (!(st.top() == '(' && s[i] == ')' || st.top() == '[' && s[i] == ']' || st.top() == '{' && s[i] == '}'))
-//                     return false;
-//                 st.pop();
-//             }
-//         }
-
-//         return st.size() == 0;
-//     }
-// };
