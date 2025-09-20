e7b816f0db9c368627db83fef68bac859aab0e4b
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-20 17:27:37 +0800

    Time: 5 ms (10.84%), Space: 17.3 MB (14.71%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -3,45 +3,31 @@ public:
     // Approach: Stack
     // Time: O(n), the time to traverse all tokens
     // Space: O(n)
-    // int evalRPN(vector<string>& tokens) {
-    //     stack<int> st;
-    //     unordered_set<string> ops { "+", "-", "*", "/" };
-
-    //     for (int i = 0; i < tokens.size(); i++) {
-    //         if (ops.count(tokens[i])) {
-    //             int digit_second = st.top();
-    //             st.pop();
-    //             int digit_first = st.top();
-    //             st.pop();
-
-    //             if (tokens[i] == "+") st.push(digit_first + digit_second);
-    //             if (tokens[i] == "-") st.push(digit_first - digit_second);
-    //             if (tokens[i] == "/") st.push(digit_first / digit_second);
-    //             if (tokens[i] == "*") st.push(digit_first * digit_second);
-    //         } else {
-    //             st.push(stoi(tokens[i]));
-    //         }
-    //     }
-    //     return st.top();
-    // }
 
     int evalRPN(vector<string>& tokens) {
-        stack<int> stk;
-        for (string token: tokens) {
-            if (isdigit(token.back())) {
-                stk.push(stoi(token));
-            } else {
-                int latter = stk.top();
-                stk.pop();
-                int former = stk.top();
-                stk.pop();
+        stack<int> st;
+        unordered_set<string> ops { "+", "-", "*", "/" };
 
-                if (token == "+") stk.push(former + latter);
-                if (token == "-") stk.push(former - latter);
-                if (token == "*") stk.push(former * latter);
-                if (token == "/") stk.push(former / latter);
+        for (int i = 0; i < tokens.size(); i++) {
+            if (ops.count(tokens[i])) {
+                int digit_second = st.top();
+                st.pop();
+                int digit_first = st.top();
+                st.pop();
+
+                if (tokens[i] == "+") st.push(digit_first + digit_second);
+                if (tokens[i] == "-") st.push(digit_first - digit_second);
+                if (tokens[i] == "/") st.push(digit_first / digit_second);
+                if (tokens[i] == "*") st.push(digit_first * digit_second);
+            } else {
+                st.push(stoi(tokens[i]));
             }
         }
-        return stk.top();
+        return st.top();
     }
+
+    // For next practice
+
+    // int evalRPN(vector<string>& tokens) {
+    // }
 };
