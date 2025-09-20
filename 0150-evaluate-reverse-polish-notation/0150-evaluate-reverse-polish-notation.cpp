d5a4bcd80ad67ee56ee15f2ee2f196f92b019f63
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-20 17:24:52 +0800

    Time: 7 ms (7.76%), Space: 19.3 MB (6.85%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
     // Approach: Stack
+    // Time: O(n), the time to traverse all tokens
+    // Space: O(n)
+    // int evalRPN(vector<string>& tokens) {
+    //     stack<int> st;
+    //     unordered_set<string> ops { "+", "-", "*", "/" };
+
+    //     for (int i = 0; i < tokens.size(); i++) {
+    //         if (ops.count(tokens[i])) {
+    //             int digit_second = st.top();
+    //             st.pop();
+    //             int digit_first = st.top();
+    //             st.pop();
+
+    //             if (tokens[i] == "+") st.push(digit_first + digit_second);
+    //             if (tokens[i] == "-") st.push(digit_first - digit_second);
+    //             if (tokens[i] == "/") st.push(digit_first / digit_second);
+    //             if (tokens[i] == "*") st.push(digit_first * digit_second);
+    //         } else {
+    //             st.push(stoi(tokens[i]));
+    //         }
+    //     }
+    //     return st.top();
+    // }
+
     int evalRPN(vector<string>& tokens) {
-        stack<int> st;
-        unordered_set<string> ops { "+", "-", "*", "/" };
-
-        for (int i = 0; i < tokens.size(); i++) {
-            if (ops.count(tokens[i])) {
-                int digit_second = st.top();
-                st.pop();
-                int digit_first = st.top();
-                st.pop();
-
-                if (tokens[i] == "+") st.push(digit_first + digit_second);
-                if (tokens[i] == "-") st.push(digit_first - digit_second);
-                if (tokens[i] == "/") st.push(digit_first / digit_second);
-                if (tokens[i] == "*") st.push(digit_first * digit_second);
+        stack<string> stk;
+        for (string token: tokens) {
+            if (isdigit(token.back())) {
+                stk.push(token);
             } else {
-                st.push(stoi(tokens[i]));
+                int latter = stoi(stk.top());
+                stk.pop();
+                int former = stoi(stk.top());
+                stk.pop();
+
+                if (token == "+") stk.push(to_string(former + latter));
+                if (token == "-") stk.push(to_string(former - latter));
+                if (token == "*") stk.push(to_string(former * latter));
+                if (token == "/") stk.push(to_string(former / latter));
             }
         }
-        return st.top();
+        return stoi(stk.top());
     }
 };
