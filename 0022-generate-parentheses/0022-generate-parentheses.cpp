25c8b96ce6bdd368f103ec8a431fcc1cd53e6553
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-20 17:17:15 +0800

    Time: 3 ms (71.18%), Space: 12.9 MB (88.64%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
-    // Approach 1.1: Recursive generation using a stack to track parentheses validity.
-    // For each step, we can add '(' if we have open parentheses remaining, or ')' if there is a matching '(' on the stack.
-    // When adding ')', we pop the matching '(' from the stack. Only when both open and right counts are zero and the stack is empty do we add a valid result.
-    // This ensures all added strings are valid and well-formed.    // Time: O(2^n). We recursively generate all combinations by putting '(' or ')' for each run.
-    
+    // Intuition: we need to enumerate all possibilities, so we use standard
+    // Approach: Backtracking
+
     // Time: O(Cn * n)
     // Let's start from the case that all possibilities are valid.
     // The final string will be of length 2n.
@@ -19,102 +17,36 @@ public:
     // Space: O(n^2) used by recursion; O(Cn * 2n) used by ans
     // O(n^2) - recursion depth * max size of each recursion = O(n) * O(n) = O(n^2)
 
-    // void putParenthesis(int n_open, int n_close, stack<char>& st, string& s, vector<string>& ans) {
-    //     // termination
-    //     if (n_open == 0 && n_close == 0) {
-    //         if (st.empty()) {
-    //             ans.push_back(s);
-    //         }
-    //         return;
-    //     }
-
-    //     // put open parenthesis
-    //     if (n_open) {
-    //         string s_ = s;
-    //         stack<char> st_ = st;
-    //         s_ += '(';
-    //         st_.push('(');
-    //         putParenthesis(n_open - 1, n_close, st_, s_, ans);
-    //     }
-        
-    //     // put right parenthesis
-    //     if (n_close && !st.empty() && st.top() == '(') {
-    //         string s_ = s;
-    //         stack<char> st_ = st;
-    //         s_ += ')';
-    //         st_.pop();
-    //         putParenthesis(n_open, n_close - 1, st_, s_, ans);
-    //     }
-    // }
-
-    // vector<string> generateParenthesis(int n) {
-    //     stack<char> st;
-    //     string s = "";
-    //     vector<string> ans;
-
-    //     putParenthesis(n, n, st, s, ans);
-    //     return ans;
-    // }
-
-    // Approach1.2: Optimized -> for a valid parantheses, the number of open parentheses is always more than that of close parenthesis
-    // Time: O(Cn * n)
-    // Space: O(n^2)
-
-    // void putParenthesis(int n_open, int n_close, string s, vector<string>& ans) {
-    //     // termination
-    //     if (n_open == 0 && n_close == 0) {
-    //         ans.push_back(s);
-    //         return;
-    //     }
-
-    //     // put open parenthesis
-    //     if (n_open) {
-    //         putParenthesis(n_open - 1, n_close, s + '(', ans);
-    //     }
-        
-    //     // put close parenthesis
-    //     if (n_close > n_open) {
-    //         putParenthesis(n_open, n_close - 1, s + ')', ans);
-    //     }
-    // }
-
-    // vector<string> generateParenthesis(int n) {
-    //     string s = "";
-    //     vector<string> ans;
-
-    //     putParenthesis(n, n, s, ans);
-    //     return ans;
-    // }
-
-
-
-    
-    vector<string> ans;
-    
-    void backtrack(int nLeft, int nRight, int n, string& currentForm) {
-        // invalid condition
-        if (nLeft > n || nRight > n) return;
+    void backtrack(int nOpen, int nClose, int n, string& currentForm, vector<string>& ans) {
         // base case
-        if (nLeft == n && nRight == n) {
+        if (nOpen == n && nClose == n) {
             ans.push_back(currentForm);
             return;
         }
 
-        if (nLeft < n) {
+        if (nOpen < n) {
             currentForm.push_back('(');
-            backtrack(nLeft + 1, nRight, n, currentForm);
+            backtrack(nOpen + 1, nClose, n, currentForm, ans);
             currentForm.pop_back();
         }
-        if (nLeft > nRight) {
+        if (nOpen > nClose) {
             currentForm.push_back(')');
-            backtrack(nLeft, nRight + 1, n, currentForm);
+            backtrack(nOpen, nClose + 1, n, currentForm, ans);
             currentForm.pop_back();
         }
     }
 
     vector<string> generateParenthesis(int n) {
         string currentForm;
-        backtrack(0, 0, n, currentForm);
+        vector<string> ans;
+    
+        backtrack(0, 0, n, currentForm, ans);
         return ans;
     }
+
+    // For the exercise next time
+
+    // vector<string> generateParenthesis(int n) {
+
+    // }
 };
