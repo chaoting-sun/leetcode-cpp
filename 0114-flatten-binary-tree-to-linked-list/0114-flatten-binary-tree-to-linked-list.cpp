f53b42dcc2a4cd4b1fd675be7c310d0f7f2dab3e
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-20 15:49:46 +0800

    Time: 4 ms (1.74%), Space: 17.9 MB (9.32%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -34,27 +34,58 @@ public:
     //     }
     // }
 
+    // Intuition: To flatten the tree, we can firstly flatten the left subtree and right subtree,
+    // and then flatten the current subtree. to flatten from the current node, move the left subtree
+    // to the right subtree and connect the right subtree to the new right subtree's right node.
+    // To do this, we have to get the last node of the flattened left/right subtree, so in base case,
+    // if a node has no left and right node, return itself, if one of which exists, then flatten, and
+    // return the last node.
+    // Approach: Divide and Conquer
+
     // Time: O(n)
     // Space: O(n)
 
-    TreeNode* flattenNode(TreeNode* root) {
-        if (!root) return nullptr;
-        if (!root->left && !root->right) return root;
+    // TreeNode* flattenTree(TreeNode* root) {
+    //     if (!root) return nullptr;
+    //     if (!root->left && !root->right) return root;
 
-        TreeNode* leftLast = flattenNode(root->left);
-        TreeNode* rightLast = flattenNode(root->right);
+    //     TreeNode* leftLast = flattenTree(root->left);
+    //     TreeNode* rightLast = flattenTree(root->right);
         
-        if (leftLast) {
-            TreeNode* tempRightRoot = root->right;
-            root->right = root->left;
-            root->left = nullptr;
-            leftLast->right = tempRightRoot;
-        }
+    //     if (leftLast) {
+    //         TreeNode* tempRightRoot = root->right;
+    //         root->right = root->left;
+    //         root->left = nullptr;
+    //         leftLast->right = tempRightRoot;
+    //     }
 
-        return rightLast ? rightLast : leftLast;
-    }
+    //     return rightLast ? rightLast : leftLast;
+    // }
+
+    // void flatten(TreeNode* root) {
+    //     flattenTree(root);
+    // }
+
+    // Intuition: start from root and traverse downward. push the right node and then right node, and then
+    // connect the current node's right node to the stack pop and set its left node to null. so that for
+    // each node we move the head of its left subtree to its right.
+
+    // Approach: Stack
+    // Time: O(n)
+    // Space: O(n)
 
     void flatten(TreeNode* root) {
-        flattenNode(root);
+        if (!root) return;
+
+        stack<TreeNode*> stk;
+        stk.push(root);
+        while (!stk.empty()) {
+            TreeNode* node = stk.top();
+            stk.pop();
+            if (node->right) stk.push(node->right);
+            if (node->left) stk.push(node->left);
+            if (!stk.empty()) node->right = stk.top();
+            node->left = nullptr;
+        }
     }
 };
