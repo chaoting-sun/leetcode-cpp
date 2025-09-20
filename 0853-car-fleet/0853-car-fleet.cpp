ea5fde24bd7493f24ace6449fbe004d7911af4c8
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-20 21:32:40 +0800

    Time: 36 ms (75.29%), Space: 104.1 MB (32.83%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -20,45 +20,6 @@ public:
     // Time: O(n*log(n)) -> sorting
     // Space: O(n)
 
-    // int carFleet(int target, vector<int>& position, vector<int>& speed) {
-    //     int n = position.size();
-    //     vector<double> time(n);
-
-    //     for (int i = 0; i < n; i++) {
-    //         time[i] = (target - position[i]) * 1.0 / speed[i];
-    //     }
-        
-    //     vector<int> time_ids(n);
-    //     for (int i = 0; i < n; i++) time_ids[i] = i;
-        
-    //     sort(time_ids.begin(), time_ids.end(), [&](int i, int j){
-    //         return position[i] > position[j];
-    //     });
-
-    //     vector<double> sorted_time(n);
-    //     for (int i = 0; i < n; i++) sorted_time[i] = time[time_ids[i]];
-
-    //     // 1. find the number of larger values in the array
-    //     // int n_car_fleets = 1;
-    //     // double max_val = sorted_time[0];
-    //     // for (int i = 1; i < n; i++) {
-    //     //     if (sorted_time[i] > max_val) {
-    //     //         max_val = sorted_time[i];
-    //     //         n_car_fleets++;
-    //     //     }
-    //     // }
-    //     // return n_car_fleets;
-
-    //     // 2. put values in the stack
-    //     stack<double> st;
-    //     for (int i = 0; i < n; i++) {
-    //         if (st.empty() || st.top() < sorted_time[i]) {
-    //             st.push(sorted_time[i]);
-    //         }
-    //     }
-    //     return st.size();
-    // }
-
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         int n = position.size();
         if (n == 0) return 0;
