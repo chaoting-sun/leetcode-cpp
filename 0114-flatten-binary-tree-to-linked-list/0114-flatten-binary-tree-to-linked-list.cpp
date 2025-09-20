52dcbc82b119300745d33910bc97d6459730ef03
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-20 15:49:39 +0800

    Time: 0 ms (100%), Space: 17.6 MB (25.42%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,18 +11,50 @@
  */
 class Solution {
 public:
-    void flatten(TreeNode* root) {
-        if (!root) return;
-        if (!root->left) {
-            flatten(root->right);
-        } else {
-            TreeNode *rightSubtree = root->right;
+    // Intuition: recursively traverse to the right subtree. for each current node, check if
+    // it has left subtree. if the left subtree exists, move it to the right subtree, and connect
+    // the original right subtree to the very right of the new right subtree.
+    // Approach: Recursion
+
+    // Time: O(n^2)
+    // Space: O(n)
+
+    // void flatten(TreeNode* root) {
+    //     if (!root) return;
+    //     if (!root->left) {
+    //         flatten(root->right);
+    //     } else {
+    //         TreeNode *rightSubtree = root->right;
+    //         root->right = root->left;
+    //         root->left = nullptr;
+    //         TreeNode *curr = root;
+    //         while (curr->right) curr = curr->right;
+    //         curr->right = rightSubtree;
+    //         flatten(root->right);
+    //     }
+    // }
+
+    // Time: O(n)
+    // Space: O(n)
+
+    TreeNode* flattenNode(TreeNode* root) {
+        if (!root) return nullptr;
+        if (!root->left && !root->right) return root;
+
+        TreeNode* leftLast = flattenNode(root->left);
+        TreeNode* rightLast = flattenNode(root->right);
+        
+        if (leftLast) {
+            TreeNode* tempRightRoot = root->right;
             root->right = root->left;
             root->left = nullptr;
-            TreeNode *curr = root;
-            while (curr->right) curr = curr->right;
-            curr->right = rightSubtree;
-            flatten(root->right);
+            leftLast->right = tempRightRoot;
         }
+
+        return rightLast ? rightLast : leftLast;
+    }
+
+    void flatten(TreeNode* root) {
+        flattenNode(root);
     }
 };
