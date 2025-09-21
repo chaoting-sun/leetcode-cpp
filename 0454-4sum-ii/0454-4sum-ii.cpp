126f5610890ea6d49a3adb76225556310017aed5
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-22 07:37:56 +0800

    Time: 137 ms (16.07%), Space: 39.6 MB (5.04%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.cpp
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -1,23 +1,61 @@
+// Approach: Hash map
+// nums1[i] + nums2[j] == -(nums3[k] + nums4[l])
+// We record all combination of nums1[i] + nums2[j] in hash map, and then try all combination of
+// nums3[k] + nums4[l] to find if -(nums3[k] + nums4[l]) is in the hash map.
+
+// Time: O(n^2)
+// Space: O(n^2)
+
 class Solution {
 public:
+    // int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+    //     unordered_map<int,int> twoSum;
+    //     for (int v1: nums1) {
+    //         for (int v2: nums2) {
+    //             twoSum[v1 + v2]++;
+    //         }
+    //     }
+
+    //     int ans = 0;
+    //     for (int v3: nums3) {
+    //         for (int v4: nums4) {
+    //             int currentTwoSum = v3 + v4;
+    //             if (twoSum.count(-currentTwoSum)) {
+    //                 ans += twoSum[-currentTwoSum];
+    //             }
+    //         }
+    //     }
+
+    //     return ans;
+    // }
+
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        unordered_map<int,int> twoSum;
-        for (int v1: nums1) {
-            for (int v2: nums2) {
-                twoSum[v1 + v2]++;
+        vector<vector<int>> lists = { nums1, nums2, nums3, nums4 };
+        
+        auto countSum = [&](int start, int end) {
+            unordered_map<int,int> count({ { 0, 1 } });
+            for (int i = start; i < end; i++) {
+                unordered_map<int,int> temp;
+                for (auto c: count) {
+                    for (int val: lists[i]) {
+                        temp[c.first + val] += c.second;
+                    }
+                }
+                count = temp;
             }
-        }
+            return count;
+        };
+
+        int k = lists.size();
+        unordered_map<int,int> count1 = countSum(0, k/2);
+        unordered_map<int,int> count2 = countSum(k/2, k);
 
         int ans = 0;
-        for (int v3: nums3) {
-            for (int v4: nums4) {
-                int currentTwoSum = v3 + v4;
-                if (twoSum.count(-currentTwoSum)) {
-                    ans += twoSum[-currentTwoSum];
-                }
+        for (auto [c1, v1]: count1) {
+            for (auto [c2, v2]: count2) {
+                if (c1 + c2 == 0) ans += v1 * v2;
             }
         }
-
         return ans;
     }
 };
