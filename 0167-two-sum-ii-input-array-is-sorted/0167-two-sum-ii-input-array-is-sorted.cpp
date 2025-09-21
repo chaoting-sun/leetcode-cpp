b6c0f721351ef6886091183e8009c6198a5483a2
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-21 11:53:37 +0800

    Time: 0 ms (100%), Space: 19.4 MB (63.91%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -44,27 +44,40 @@ public:
     // Time: O(n*log(n)). O(log(n-1)+log(n-2)+...log(1)) <= O(n*log(n-1)) <= O(n*log(n))
     // Space: O(1)
 
+    // vector<int> twoSum(vector<int>& numbers, int target) {
+    //     int n = numbers.size();
+
+    //     for (int i = 0; i < n - 1; i++) {
+    //         int to_find = target - numbers[i];
+            
+    //         int left = i + 1;
+    //         int right = n - 1;
+            
+    //         while (left <= right) {
+    //             int mid = (left + right) / 2;
+    //             if (numbers[mid] == to_find) {
+    //                 return { i + 1, mid + 1 };
+    //             } else if (numbers[mid] > to_find) {
+    //                 right = mid - 1;
+    //             } else {
+    //                 left = mid + 1;
+    //             }
+    //         }
+    //     }
+
+    //     return { -1, -1 };
+    // }
+
+
     vector<int> twoSum(vector<int>& numbers, int target) {
         int n = numbers.size();
-
-        for (int i = 0; i < n - 1; i++) {
-            int to_find = target - numbers[i];
-            
-            int left = i + 1;
-            int right = n - 1;
-            
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                if (numbers[mid] == to_find) {
-                    return { i + 1, mid + 1 };
-                } else if (numbers[mid] > to_find) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int added = numbers[left] + numbers[right];
+            if (added < target) left++;
+            else if (added > target) right--;
+            else return { left + 1, right + 1};
         }
-
         return { -1, -1 };
     }
 };
