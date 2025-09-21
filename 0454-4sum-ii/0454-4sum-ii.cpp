48bb8cb4b88653bf786d9e27c3c384d16d0fa05a
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-22 07:38:07 +0800

    Time: 131 ms (17.11%), Space: 39.6 MB (5.04%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.cpp
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -1,13 +1,13 @@
-// Approach: Hash map
-// nums1[i] + nums2[j] == -(nums3[k] + nums4[l])
-// We record all combination of nums1[i] + nums2[j] in hash map, and then try all combination of
-// nums3[k] + nums4[l] to find if -(nums3[k] + nums4[l]) is in the hash map.
-
-// Time: O(n^2)
-// Space: O(n^2)
-
 class Solution {
 public:
+    // Approach: Hash map
+    // nums1[i] + nums2[j] == -(nums3[k] + nums4[l])
+    // We record all combination of nums1[i] + nums2[j] in hash map, and then try all combination of
+    // nums3[k] + nums4[l] to find if -(nums3[k] + nums4[l]) is in the hash map.
+
+    // Time: O(n^2)
+    // Space: O(n^2)
+
     // int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
     //     unordered_map<int,int> twoSum;
     //     for (int v1: nums1) {
@@ -29,26 +29,30 @@ public:
     //     return ans;
     // }
 
+    // Generalized Method
+
+    unordered_map<int,int> countSum(int start, int end, vector<vector<int>>& lists) {
+        unordered_map<int,int> count({ { 0, 1 } });
+        for (int i = start; i < end; i++) {
+            unordered_map<int,int> temp;
+            for (auto c: count) {
+                for (int val: lists[i]) {
+                    temp[c.first + val] += c.second;
+                }
+            }
+            count = temp;
+        }
+        return count;
+    };
+
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         vector<vector<int>> lists = { nums1, nums2, nums3, nums4 };
         
-        auto countSum = [&](int start, int end) {
-            unordered_map<int,int> count({ { 0, 1 } });
-            for (int i = start; i < end; i++) {
-                unordered_map<int,int> temp;
-                for (auto c: count) {
-                    for (int val: lists[i]) {
-                        temp[c.first + val] += c.second;
-                    }
-                }
-                count = temp;
-            }
-            return count;
-        };
+
 
         int k = lists.size();
-        unordered_map<int,int> count1 = countSum(0, k/2);
-        unordered_map<int,int> count2 = countSum(k/2, k);
+        unordered_map<int,int> count1 = countSum(0, k/2, lists);
+        unordered_map<int,int> count2 = countSum(k/2, k, lists);
 
         int ans = 0;
         for (auto [c1, v1]: count1) {
