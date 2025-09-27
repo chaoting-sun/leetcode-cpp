083b973f524c9391c73f9398bad3eb0d64e31efd
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-27 16:07:59 +0800

    Time: 1 ms (75.99%), Space: 18 MB (55.94%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,43 +1,5 @@
 class Solution {
 public:
-    // Approach: DFS
-    // Time: O(V + E)
-    // Space: O(V + E)
-
-    // void dfs(vector<vector<int>>& adj, vector<bool>& visited, int curr_node) {
-    //     visited[curr_node] = true;
-
-    //     for (int next_node: adj[curr_node]) {
-    //         if (!visited[next_node]) {
-    //             dfs(adj, visited, next_node);
-    //         }
-    //     }
-    // }
-
-    // int countComponents(int n, vector<vector<int>>& edges) {
-    //     // Time: O(V + E)
-    //     // Space: O(V + E)
-    //     vector<vector<int>> adj(n);
-    //     for (vector<int> edge: edges) {
-    //         adj[edge[0]].push_back(edge[1]);
-    //         adj[edge[1]].push_back(edge[0]);
-    //     }
-        
-    //     // Time: O(V + E). we only traverse those nodes that are not visited
-    //     // Space: O(V)
-    //     vector<bool> visited(n);
-    //     int count = 0;
-    //     for (int i = 0; i < n; i++) {
-    //         if (!visited[i]) {
-    //             dfs(adj, visited, i);
-    //             count++;
-    //         }
-    //     }
-        
-    //     return count;
-    // }
-
-
     void dfs(vector<vector<int>>& nodes, vector<bool>& visited, int currentNode) {
         visited[currentNode] = true;
         for (int nextNode: nodes[currentNode]) {
@@ -49,6 +11,9 @@ public:
 
     int countComponents(int n, vector<vector<int>>& edges) {
         // change to adjacency list
+
+        // Time: O(V + E). initialize the entries + push back all edges
+        // Space: O(V + E)
         vector<vector<int>> nodes(n);
         for (auto& edge: edges) {
             int u = edge[0], v = edge[1];
@@ -57,8 +22,11 @@ public:
         }
 
         // do dfs and count the entries
+
         int numComponents = 0;
         vector<bool> visited(n, false);
+        // Time: O(V + E). loop through V vertices + explore all edges
+        // Space: O(V)
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 dfs(nodes, visited, i);
