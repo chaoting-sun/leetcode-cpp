f3e7ab7be8c452e48f0118de86272d4d11b449d8
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-27 18:16:17 +0800

    Time: 0 ms (100%), Space: 21.6 MB (5.45%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -9,15 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Approach: DFS
+// Time: O(n). we visited each node exactly once
+// Space: O(n). in worst case the tree is unbalanced.
+
 class Solution {
 public:
+    // bool hasPathSum(TreeNode* root, int targetSum) {
+    //     if (!root) return false;
+
+    //     targetSum -= root->val;
+    //     if (!root->left && !root->right) {
+    //         return targetSum == 0;
+    //     }
+    //     return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+    // }
+
     bool hasPathSum(TreeNode* root, int targetSum) {
         if (!root) return false;
+        
+        stack<pair<TreeNode*, int>> nodes;
+        nodes.push({ root, targetSum });
 
-        targetSum -= root->val;
-        if (!root->left && !root->right) {
-            return targetSum == 0;
+        while (!nodes.empty()) {
+            auto [currNode, currSum] = nodes.top();
+            nodes.pop();
+
+            currSum -= currNode->val;
+
+            if (!currNode->left && !currNode->right && currSum == 0) {
+                return true;
+            }
+            if (currNode->left) {
+                nodes.push({ currNode->left, currSum });
+            }
+            if (currNode->right) {
+                nodes.push({ currNode->right, currSum });
+            }
         }
-        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+
+        return false;
     }
 };
