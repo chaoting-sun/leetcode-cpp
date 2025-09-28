a5f09aea93c8192b275eaab94228c86d9067bada
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-09-28 17:33:33 +0800

    Time: 0 ms (100%), Space: 20.9 MB (67.47%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,7 +11,8 @@
  */
 class Solution {
 public:
-    void dfs(vector<vector<int>>& ans, vector<int> path, TreeNode* root, int targetSum) {
+    void dfs(vector<vector<int>>& ans, vector<int>& path, TreeNode* root, int targetSum) {
+        // select
         targetSum -= root->val;
         path.push_back(root->val);
 
@@ -20,11 +21,14 @@ public:
             if (targetSum == 0) {
                 ans.push_back(path);
             }
-            return;
         }
 
+        // recursively processing
         if (root->left) dfs(ans, path, root->left, targetSum);
         if (root->right) dfs(ans, path, root->right, targetSum);
+        
+        // unselect
+        path.pop_back();
     }
     
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
