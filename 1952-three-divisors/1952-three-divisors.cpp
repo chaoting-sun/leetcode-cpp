30f15abaa7e3fa67b0fc78a92dc6b194d5979cab
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-04 22:35:01 +0800

    Time: 0 ms (100%), Space: 7.9 MB (26.23%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -4,6 +4,7 @@ public:
         int divisors = 0;
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) divisors++;
+            if (divisors > 3) return false;
         }
         return divisors == 3;
     }
