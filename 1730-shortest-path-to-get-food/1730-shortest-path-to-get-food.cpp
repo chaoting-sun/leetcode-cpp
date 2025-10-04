367dbcfb460de390f01cf8b9129525d7e2693e08
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-04 19:12:26 +0800

    Time: 12 ms (45.38%), Space: 22.5 MB (85.38%) - LeetHub

--- 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.cpp
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    // Approach: BFS
+    // The problem asks the shortest path from the original cell to any food cell, so we can come up with the
+    // BFS method, in which we are able to explore the cell layer by layer. We use queue to store the position
+    // to be handled, and maintain a variable step to track the current layer. Once a cell is visited, we mark
+    // it as 'X' to avoid visiting again.
+
+    // Time: O(mn)
+    // Space: O(mn)
+
     vector<int> dx = { -1, 1, 0, 0 };
     vector<int> dy = { 0, 0, -1, 1 };
 
     int getFood(vector<vector<char>>& grid) {
+        if (grid.empty() || grid[0].empty()) return -1;
         int m = grid.size(), n = grid[0].size();
-        if (m == 0 || n == 0) return -1;
 
         queue<pair<int,int>> q;
         int step = 0;
@@ -14,6 +23,7 @@ public:
             for (int y = 0; y < n; y++) {
                 if (grid[x][y] == '*') {
                     q.push({ x, y });
+                    grid[x][y] = 'X';
                 }
             }
         }
@@ -26,9 +36,9 @@ public:
                 auto [x, y] = q.front();
                 q.pop();
 
-                for (int i = 0; i < 4; i++) {
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
+                for (int dir = 0; dir < 4; dir++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
                     if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] != 'X') {
                         if (grid[nx][ny] == '#') {
                             return step;
