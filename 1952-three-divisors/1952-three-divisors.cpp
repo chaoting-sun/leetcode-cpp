99bed544a37a2f3f0508a5b338687de2e1ba8305
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-04 22:46:58 +0800

    Time: 0 ms (100%), Space: 7.8 MB (58.5%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
+    // bool isThree(int n) {
+    //     int divisors = 0;
+    //     for (int i = 1; i <= n; i++) {
+    //         if (n % i == 0) divisors++;
+    //         if (divisors > 3) return false;
+    //     }
+    //     return divisors == 3;
+    // }
+
     bool isThree(int n) {
         int divisors = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) divisors++;
+        for (int d = 1; d <= (int)sqrt(n); d++) {
+            if (n % d == 0) {
+                divisors += 2;
+                if (d * d == n) divisors--;
+            }
             if (divisors > 3) return false;
         }
         return divisors == 3;
