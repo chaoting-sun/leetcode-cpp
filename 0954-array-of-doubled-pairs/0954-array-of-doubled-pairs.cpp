7b282750aba611bed30946047a927a86a147d75a
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-04 23:28:06 +0800

    Time: 11 ms (82.63%), Space: 61.9 MB (39.19%) - LeetHub

--- 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.cpp
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
+    // Approach: Greedy
+    // Time: O(n*logn)
+    // Space: O(n)
+
+    // bool canReorderDoubled(vector<int>& arr) {
+    //     sort(arr.begin(), arr.end(), [](int a, int b) {
+    //         return abs(a) < abs(b);
+    //     });
+    //     unordered_map<int, int> counts;
+    //     for (int val: arr) counts[val]++;
+
+    //     for (int i = 0; i < arr.size(); i++) {
+    //         int a = arr[i], b = 2 * arr[i];
+    //         if (counts[a] == 0) continue;
+    //         if (!counts.count(b) || counts[b] == 0) {
+    //             return false;
+    //         }
+    //         counts[a]--;
+    //         counts[b]--;
+    //     }
+
+    //     return true;
+    // }
+
     bool canReorderDoubled(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), [](int a, int b) {
-            return abs(a) < abs(b);
-        });
-        unordered_map<int, int> counts;
+        map<int,int> counts;
         for (int val: arr) counts[val]++;
-
-        for (int i = 0; i < arr.size(); i++) {
-            int a = arr[i], b = 2 * arr[i];
-            if (counts[a] == 0) continue;
-            if (!counts.count(b) || counts[b] == 0) {
-                return false;
+        for (auto& [val, currentCount]: counts) {
+            if (currentCount == 0) continue;
+            if (val >= 0) {
+                int want = 2 * val;
+                if (!counts.count(want) || currentCount > counts[want]) return false;
+                counts[want] -= currentCount;
+            } else {
+                if (val % 2 != 0) return false;
+                int want = val / 2;
+                if (!counts.count(want) || currentCount > counts[want]) return false;
+                counts[want] -= currentCount;
             }
-            counts[a]--;
-            counts[b]--;
         }
-
         return true;
     }
 };
