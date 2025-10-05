b35934263bcb2246c32dc87f18569ab141e80a44
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-05 12:14:54 +0800

    Time: 0 ms (100%), Space: 32.5 MB (37.5%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // Approach: Trivial
+    // Time: O(n)
+    // Space: O(1)
+
+    int alternatingSum(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2 == 0) {
+                ans += nums[i];
+            } else {
+                ans -= nums[i];
+            }
+        }
+        return ans;
+    }
+};
