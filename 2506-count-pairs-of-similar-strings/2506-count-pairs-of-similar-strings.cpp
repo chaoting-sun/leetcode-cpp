827056d00b4619e05a5169eafd5a554ebeac74df
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-06 00:29:13 +0800

    Time: 3 ms (79.04%), Space: 16.3 MB (91.16%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
+    // Approach: hash map
+    // Time: O(n*k*logk)
+    // Space: O(nk)
+
+    // int similarPairs(vector<string>& words) {
+    //     unordered_map<string, int> freq;
+    //     for (auto& word: words) {
+    //         set<char> chars;
+    //         for (char w: word) chars.insert(w); // O(k*logk)
+    //         string s;
+    //         for (auto ch: chars) s += ch;
+    //         freq[s]++;
+    //     }
+    //     int ans = 0;
+    //     for (auto& [word, c]: freq) {
+    //         ans += c * (c - 1) / 2;
+    //     }
+    //     return ans;
+    // }
+
     int similarPairs(vector<string>& words) {
-        unordered_map<string,int> uniqueCharWords;
+        unordered_map<int, int> freq;
         for (auto& word: words) {
-            set<char> chars;
-            for (char w: word) chars.insert(w);
-            string s;
-            for (auto ch: chars) s += ch;
-            uniqueCharWords[s]++;
+            int mask = 0;
+            for (char c: word) mask |= 1 << c - 'a';
+            freq[mask]++;
         }
         int ans = 0;
-        for (auto& [word, c]: uniqueCharWords) {
+        for (auto& [_, c]: freq) {
             ans += c * (c - 1) / 2;
         }
         return ans;
