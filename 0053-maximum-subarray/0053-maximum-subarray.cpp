95a5d2fe22bbcadccb5c56cdbbb40375a11049a8
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-08 07:52:29 +0800

    Time: 4 ms (11.13%), Space: 71.8 MB (52.33%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -14,19 +14,36 @@ public:
     // Space: O(n) in this implementation (can be reduced to O(1) by keeping only the current and global max).
 
 
+    // int maxSubArray(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if (n == 0) return 0;
+
+    //     vector<int> dp(n + 1, 0);
+    //     dp[1] = nums[0];
+    //     int max_sum = dp[1];
+
+    //     for (int i = 2; i <= n; i++) {
+    //         dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1]);
+    //         max_sum = max(max_sum, dp[i]);
+    //     }
+
+    //     return max_sum;
+    // }
+
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         if (n == 0) return 0;
 
-        vector<int> dp(n + 1, 0);
-        dp[1] = nums[0];
-        int max_sum = dp[1];
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1]);
-            max_sum = max(max_sum, dp[i]);
+        int maxSum = nums[0];
+        int currSum = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (currSum > 0) {
+                currSum += nums[i];
+            } else {
+                currSum = nums[i];
+            }
+            maxSum = max(maxSum, currSum);
         }
-
-        return max_sum;
+        return maxSum;
     }
 };
