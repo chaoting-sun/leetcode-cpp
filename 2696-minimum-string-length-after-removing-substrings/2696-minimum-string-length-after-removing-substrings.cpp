351d8bbad8ed779acea2f33c91f648d7dbd26f21
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-07 23:36:03 +0800

    Time: 0 ms (100%), Space: 9.9 MB (98.14%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
+    // int minLength(string s) {
+    //     string ans;
+    //     for (char c: s) {
+    //         if (!ans.empty() &&
+    //             (ans.back() == 'A' && c == 'B' ||
+    //              ans.back() == 'C' && c == 'D')) {
+    //             ans.pop_back();
+    //         } else {
+    //             ans.push_back(c);
+    //         }
+    //     }
+    //     return ans.size();
+    // }
+
     int minLength(string s) {
-        string ans;
-        for (char c: s) {
-            if (!ans.empty() &&
-                (ans.back() == 'A' && c == 'B' ||
-                 ans.back() == 'C' && c == 'D')) {
-                ans.pop_back();
+        int end = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (end > 0 &&
+                (s[end - 1] == 'A' && s[i] == 'B' ||
+                 s[end - 1] == 'C' && s[i] == 'D')) {
+                end--;
             } else {
-                ans.push_back(c);
+                s[end] = s[i];
+                end++;
             }
         }
-        return ans.size();
+        return end;
     }
 };
