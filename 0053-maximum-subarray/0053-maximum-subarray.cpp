07c4541aeca85322c609229dfa3fedb44aced01c
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-08 21:03:02 +0800

    Time: 4 ms (11.13%), Space: 71.6 MB (97.64%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -13,70 +13,69 @@ public:
     // Time:  O(n)
     // Space: O(n) in this implementation (can be reduced to O(1) by keeping only the current and global max).
 
-
     // int maxSubArray(vector<int>& nums) {
     //     int n = nums.size();
     //     if (n == 0) return 0;
 
     //     vector<int> dp(n + 1, 0);
     //     dp[1] = nums[0];
-    //     int max_sum = dp[1];
+    //     int maxSum = dp[1];
 
     //     for (int i = 2; i <= n; i++) {
     //         dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1]);
-    //         max_sum = max(max_sum, dp[i]);
+    //         maxSum = max(maxSum, dp[i]);
     //     }
 
-    //     return max_sum;
+    //     return maxSum;
     // }
 
     // Approach: Optimized DP
     // Time: O(n)
     // Space: O(1)
 
-    // int maxSubArray(vector<int>& nums) {
-    //     int n = nums.size();
-    //     if (n == 0) return 0;
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
 
-    //     int maxSum = nums[0];
-    //     int currSum = nums[0];
-    //     for (int i = 1; i < n; i++) {
-    //         if (currSum > 0) {
-    //             currSum += nums[i];
-    //         } else {
-    //             currSum = nums[i];
-    //         }
-    //         maxSum = max(maxSum, currSum);
-    //     }
-    //     return maxSum;
+        int maxSum = nums[0];
+        int currSum = nums[0];
+        for (int i = 1; i < n; i++) {
+            currSum = max(nums[i], currSum + nums[i]);
+            maxSum = max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+
+    // Approach: Divide and Conquer
+    // Time: O(nlogn)
+    // Space: O(n)
+
+    // int maxSubArray(vector<int>& nums) {
+    //     return findBestSubarray(nums, 0, nums.size() - 1);
     // }
 
-    int maxSubArray(vector<int>& nums) {
-        return findBestSubarray(nums, 0, nums.size() - 1);
-    }
+    // int findBestSubarray(vector<int>& nums, int left, int right) {
+    //     if (left > right) return INT_MIN;
 
-    int findBestSubarray(vector<int>& nums, int left, int right) {
-        if (left > right) return -1;
-
-        int currSum = 0;
-        int bestLeftSum = 0, bestRightSum = 0;
-        int mid = left + (right - left) / 2;
+    //     int currSum = 0;
+    //     int bestLeftSum = 0, bestRightSum = 0;
+    //     int mid = left + (right - left) / 2;
         
-        for (int i = mid - 1; i >= left; i--) {
-            currSum += nums[i];
-            bestLeftSum = max(bestLeftSum, currSum);
-        }
+    //     for (int i = mid - 1; i >= left; i--) {
+    //         currSum += nums[i];
+    //         bestLeftSum = max(bestLeftSum, currSum);
+    //     }
 
-        currSum = 0;
-        for (int i = mid + 1; i <= right; i++) {
-            currSum += nums[i];
-            bestRightSum = max(bestRightSum, currSum);
-        }
+    //     currSum = 0;
+    //     for (int i = mid + 1; i <= right; i++) {
+    //         currSum += nums[i];
+    //         bestRightSum = max(bestRightSum, currSum);
+    //     }
 
-        int midBest = nums[mid] + bestLeftSum + bestRightSum;
-        int leftBest = findBestSubarray(nums, left, mid - 1);
-        int rightBest = findBestSubarray(nums, mid + 1, right);
+    //     int midBest = nums[mid] + bestLeftSum + bestRightSum;
+    //     int leftBest = findBestSubarray(nums, left, mid - 1);
+    //     int rightBest = findBestSubarray(nums, mid + 1, right);
 
-        return max(max(midBest, leftBest), rightBest);
-    }
+    //     return max(max(midBest, leftBest), rightBest);
+    // }
 };
