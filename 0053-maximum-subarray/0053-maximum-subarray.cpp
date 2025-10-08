d77718ae59fd958d56f18f4c79d17268466cd5a2
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-08 20:10:29 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -30,20 +30,53 @@ public:
     //     return max_sum;
     // }
 
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
+    // Approach: Optimized DP
+    // Time: O(n)
+    // Space: O(1)
 
-        int maxSum = nums[0];
-        int currSum = nums[0];
-        for (int i = 1; i < n; i++) {
-            if (currSum > 0) {
-                currSum += nums[i];
-            } else {
-                currSum = nums[i];
-            }
-            maxSum = max(maxSum, currSum);
+    // int maxSubArray(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if (n == 0) return 0;
+
+    //     int maxSum = nums[0];
+    //     int currSum = nums[0];
+    //     for (int i = 1; i < n; i++) {
+    //         if (currSum > 0) {
+    //             currSum += nums[i];
+    //         } else {
+    //             currSum = nums[i];
+    //         }
+    //         maxSum = max(maxSum, currSum);
+    //     }
+    //     return maxSum;
+    // }
+
+    int maxSubArray(vector<int>& nums) {
+        return findBestSubarray(nums, 0, nums.size() - 1);
+    }
+
+    int findBestSubarray(vector<int>& nums, int left, int right) {
+        if (left > right) return -1;
+
+        int currSum = 0;
+        int bestLeftSum = 0, bestRightSum = 0;
+        int mid = left + (right - left) / 2;
+        
+        for (int i = mid - 1; i >= left; i--) {
+            currSum += nums[i];
+            bestLeftSum = max(bestLeftSum, currSum);
         }
-        return maxSum;
+
+        currSum = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            currSum += nums[i];
+            bestRightSum = max(bestRightSum, currSum);
+        }
+
+        int midBest = nums[mid] + bestLeftSum + bestRightSum;
+        int leftBest = findBestSubarray(nums, left, mid - 1);
+        int rightBest = findBestSubarray(nums, mid + 1, right);
+
+        return max(max(midBest, leftBest), rightBest);
     }
 };
