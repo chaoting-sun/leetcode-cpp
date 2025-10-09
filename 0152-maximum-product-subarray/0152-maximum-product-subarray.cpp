6f4e54ee7370d954b8e10c241302de3d15b748a1
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-09 08:10:03 +0800

    Time: 0 ms (100%), Space: 18.4 MB (5.79%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,23 +4,39 @@ public:
     // Since multiplying by a negative number can flip a maximum into a minimum (and vice versa),
     // we keep track of both the maximum and minimum product at each step
 
+    // int maxProduct(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if (n == 0) return 0;
+
+    //     int min_so_far = nums[0];
+    //     int max_so_far = nums[0];
+    //     int max_product = max_so_far;
+
+    //     for (int i = 1; i < n; i++) {
+    //         int tmp_max_so_far = max(nums[i], max(max_so_far*nums[i], min_so_far*nums[i]));
+    //         min_so_far = min(nums[i], min(max_so_far*nums[i], min_so_far*nums[i]));
+            
+    //         max_so_far = tmp_max_so_far;
+
+    //         max_product = max(max_product, max_so_far);
+    //     }
+
+    //     return max_product;
+    // }
+
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
         if (n == 0) return 0;
-
-        int min_so_far = nums[0];
-        int max_so_far = nums[0];
-        int max_product = max_so_far;
+        
+        vector<int> minProduct(n), maxProduct(n); 
+        minProduct[0] = maxProduct[0] = nums[0];
+        int ans = nums[0];
 
         for (int i = 1; i < n; i++) {
-            int tmp_max_so_far = max(nums[i], max(max_so_far*nums[i], min_so_far*nums[i]));
-            min_so_far = min(nums[i], min(max_so_far*nums[i], min_so_far*nums[i]));
-            
-            max_so_far = tmp_max_so_far;
-
-            max_product = max(max_product, max_so_far);
+            maxProduct[i] = max(nums[i], max(minProduct[i - 1] * nums[i], maxProduct[i - 1] * nums[i]));
+            minProduct[i] = min(nums[i], min(minProduct[i - 1] * nums[i], maxProduct[i - 1] * nums[i]));
+            ans = max(ans, maxProduct[i]);
         }
-
-        return max_product;
+        return ans;
     }
 };
