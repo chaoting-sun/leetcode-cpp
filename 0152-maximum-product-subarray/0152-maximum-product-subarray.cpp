f5ccda0d0aee80cc0523cfeaf58fa32d345326bf
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-09 23:22:27 +0800

    Time: 0 ms (100%), Space: 17.8 MB (58.6%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -24,18 +24,35 @@ public:
     //     return max_product;
     // }
 
-    int maxProduct(vector<int>& nums) {
+    // int maxProduct(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if (n == 0) return 0;
+        
+    //     vector<int> minProduct(n), maxProduct(n); 
+    //     minProduct[0] = maxProduct[0] = nums[0];
+    //     int ans = nums[0];
+
+    //     for (int i = 1; i < n; i++) {
+    //         maxProduct[i] = max(nums[i], max(minProduct[i - 1] * nums[i], maxProduct[i - 1] * nums[i]));
+    //         minProduct[i] = min(nums[i], min(minProduct[i - 1] * nums[i], maxProduct[i - 1] * nums[i]));
+    //         ans = max(ans, maxProduct[i]);
+    //     }
+    //     return ans;
+    // }
+
+      int maxProduct(vector<int>& nums) {
         int n = nums.size();
         if (n == 0) return 0;
         
-        vector<int> minProduct(n), maxProduct(n); 
-        minProduct[0] = maxProduct[0] = nums[0];
         int ans = nums[0];
+        int minProduct = nums[0], maxProduct = nums[0];
 
         for (int i = 1; i < n; i++) {
-            maxProduct[i] = max(nums[i], max(minProduct[i - 1] * nums[i], maxProduct[i - 1] * nums[i]));
-            minProduct[i] = min(nums[i], min(minProduct[i - 1] * nums[i], maxProduct[i - 1] * nums[i]));
-            ans = max(ans, maxProduct[i]);
+            int lastMinProduct = minProduct;
+            int lastMaxProduct = maxProduct;
+            maxProduct = max(nums[i], max(lastMinProduct * nums[i], lastMaxProduct * nums[i]));
+            minProduct = min(nums[i], min(lastMinProduct * nums[i], lastMaxProduct * nums[i]));
+            ans = max(ans, maxProduct);
         }
         return ans;
     }
