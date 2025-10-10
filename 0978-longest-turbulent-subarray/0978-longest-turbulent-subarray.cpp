e1eb42591df316f6d539a7433bc510302a40b005
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-10 11:21:45 +0800

    Time: 3 ms (43.87%), Space: 44.1 MB (63.07%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
@@ -1,25 +1,55 @@
 class Solution {
 public:
+    // int maxTurbulenceSize(vector<int>& arr) {
+    //     int n = arr.size();
+    //     if (n == 0) return 0;
+        
+    //     vector<int> largerOddLength(n, 1), largerEvenLength(n, 1);
+    //     int maxLength = 1;
+        
+    //     for (int i = 1; i < n; i++) {
+    //         if ((i % 2 == 1 && arr[i - 1] < arr[i]) ||
+    //             (i % 2 == 0) && arr[i - 1] > arr[i]) {
+    //             largerOddLength[i] = largerOddLength[i - 1] + 1;
+    //             maxLength = max(maxLength, largerOddLength[i]);
+    //         }
+    //     }
+
+    //     for (int i = 1; i < n; i++) {
+    //         if ((i % 2 == 1 && arr[i - 1] > arr[i]) ||
+    //             (i % 2 == 0) && arr[i - 1] < arr[i]) {
+    //             largerEvenLength[i] = largerEvenLength[i - 1] + 1;
+    //             maxLength = max(maxLength, largerEvenLength[i]);
+    //         }
+    //     }
+
+    //     return maxLength;
+    // }
+
     int maxTurbulenceSize(vector<int>& arr) {
         int n = arr.size();
         if (n == 0) return 0;
         
-        vector<int> largerOddLength(n, 1), largerEvenLength(n, 1);
+        int largerOddLength = 1, largerEvenLength = 1;
         int maxLength = 1;
         
         for (int i = 1; i < n; i++) {
             if ((i % 2 == 1 && arr[i - 1] < arr[i]) ||
                 (i % 2 == 0) && arr[i - 1] > arr[i]) {
-                largerOddLength[i] = largerOddLength[i - 1] + 1;
-                maxLength = max(maxLength, largerOddLength[i]);
+                largerOddLength++;
+                maxLength = max(maxLength, largerOddLength);
+            } else {
+                largerOddLength = 1;
             }
         }
 
         for (int i = 1; i < n; i++) {
             if ((i % 2 == 1 && arr[i - 1] > arr[i]) ||
                 (i % 2 == 0) && arr[i - 1] < arr[i]) {
-                largerEvenLength[i] = largerEvenLength[i - 1] + 1;
-                maxLength = max(maxLength, largerEvenLength[i]);
+                largerEvenLength++;
+                maxLength = max(maxLength, largerEvenLength);
+            } else {
+                largerEvenLength = 1;
             }
         }
 
