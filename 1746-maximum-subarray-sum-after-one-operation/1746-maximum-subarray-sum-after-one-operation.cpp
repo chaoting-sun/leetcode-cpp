a5755b0ddf7747f832bd1ac85bce10f398d27f9f
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-10 15:47:04 +0800

    Time: 86 ms (45%), Space: 78.3 MB (40%) - LeetHub

---
+++ 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxSumAfterOperation(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        vector<int> maxPrefixSum(n), maxSuffixSum(n);
+        int ans = INT_MIN;
+        
+        maxPrefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            maxPrefixSum[i] = max(maxPrefixSum[i - 1] + nums[i], nums[i]);
+        }
+
+        maxSuffixSum[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            maxSuffixSum[i] = max(maxSuffixSum[i + 1] + nums[i], nums[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int curr = nums[i] * nums[i];
+            if (i > 0) curr = max(curr, curr + maxPrefixSum[i - 1]);
+            if (i < n - 1) curr = max(curr, curr + maxSuffixSum[i + 1]);
+            ans = max(ans, curr);
+        }
+
+        return ans;
+    }
+};
