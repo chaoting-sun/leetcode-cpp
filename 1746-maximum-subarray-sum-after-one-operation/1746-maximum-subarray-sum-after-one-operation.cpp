a21ebd0ade3e408f9712cbf28bccfff444fc0a65
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-10 17:34:54 +0800

    Time: 89 ms (32.5%), Space: 78.5 MB (12.5%) - LeetHub

--- 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
+++ 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
+    // Approach: Prefix/Suffix Sum
+    // Time: O(n)
+    // Space: O(n)
+
     // int maxSumAfterOperation(vector<int>& nums) {
-        // int n = nums.size();
-        // if (n == 0) return 0;
+    //     int n = nums.size();
+    //     if (n == 0) return 0;
 
     //     vector<int> maxPrefixSum(n), maxSuffixSum(n);
     //     int ans = INT_MIN;
@@ -27,19 +31,41 @@ public:
     //     return ans;
     // }
 
+    // Approach: Optimized
+
     int maxSumAfterOperation(vector<int>& nums) {
         int n = nums.size();
         if (n == 0) return 0;
 
-        int opUsed = 0, opNotUsed = 0, ans = INT_MIN;
+        vector<int> opUsed(n), opNotUsed(n);
+        opNotUsed[0] = nums[0];
+        opUsed[0] = nums[0] * nums[0];
+        int ans = opUsed[0];
 
-        for (int x: nums) {
+        for (int i = 1; i < n; i++) {
+            int x = nums[i];
             int xx = x * x;
-            opUsed = max(xx, max(opNotUsed + xx, opUsed + x));
-            opNotUsed = max(opNotUsed + x, x);
-            ans = max(ans, opUsed);
+            opUsed[i] = max(xx, max(opNotUsed[i - 1] + xx, opUsed[i - 1] + x));
+            opNotUsed[i] = max(opNotUsed[i - 1] + x, x);
+            ans = max(ans, opUsed[i]);
         }
 
         return ans;
     }
+
+    // int maxSumAfterOperation(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if (n == 0) return 0;
+
+    //     int opUsed = 0, opNotUsed = 0, ans = INT_MIN;
+
+    //     for (int x: nums) {
+    //         int xx = x * x;
+    //         opUsed = max(xx, max(opNotUsed + xx, opUsed + x));
+    //         opNotUsed = max(opNotUsed + x, x);
+    //         ans = max(ans, opUsed);
+    //     }
+
+    //     return ans;
+    // }
 };
