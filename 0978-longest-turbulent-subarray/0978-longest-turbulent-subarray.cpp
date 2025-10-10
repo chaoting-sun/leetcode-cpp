d51668210db783d0d2cd6e0bcd8645c7fcf11139
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-10 11:42:48 +0800

    Time: 0 ms (100%), Space: 44.2 MB (63.07%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
@@ -26,33 +26,55 @@ public:
     //     return maxLength;
     // }
 
+    // int maxTurbulenceSize(vector<int>& arr) {
+    //     int n = arr.size();
+    //     if (n == 0) return 0;
+        
+    //     int largerOddLength = 1, largerEvenLength = 1;
+    //     int maxLength = 1;
+        
+    //     for (int i = 1; i < n; i++) {
+    //         if ((i % 2 == 1 && arr[i - 1] < arr[i]) ||
+    //             (i % 2 == 0) && arr[i - 1] > arr[i]) {
+    //             largerOddLength++;
+    //             maxLength = max(maxLength, largerOddLength);
+    //         } else {
+    //             largerOddLength = 1;
+    //         }
+    //     }
+
+    //     for (int i = 1; i < n; i++) {
+    //         if ((i % 2 == 1 && arr[i - 1] > arr[i]) ||
+    //             (i % 2 == 0) && arr[i - 1] < arr[i]) {
+    //             largerEvenLength++;
+    //             maxLength = max(maxLength, largerEvenLength);
+    //         } else {
+    //             largerEvenLength = 1;
+    //         }
+    //     }
+
+    //     return maxLength;
+    // }
+
     int maxTurbulenceSize(vector<int>& arr) {
         int n = arr.size();
         if (n == 0) return 0;
         
-        int largerOddLength = 1, largerEvenLength = 1;
-        int maxLength = 1;
-        
+        int incTail = 1, decTail = 1, ans = 1;
         for (int i = 1; i < n; i++) {
-            if ((i % 2 == 1 && arr[i - 1] < arr[i]) ||
-                (i % 2 == 0) && arr[i - 1] > arr[i]) {
-                largerOddLength++;
-                maxLength = max(maxLength, largerOddLength);
+            if (arr[i - 1] < arr[i]) {
+                incTail = decTail + 1;
+                decTail = 1;
+            } else if (arr[i - 1] > arr[i]) {
+                decTail = incTail + 1;
+                incTail = 1;
             } else {
-                largerOddLength = 1;
+                incTail = 1;
+                decTail = 1;
             }
+            ans = max(ans, max(incTail, decTail));
         }
 
-        for (int i = 1; i < n; i++) {
-            if ((i % 2 == 1 && arr[i - 1] > arr[i]) ||
-                (i % 2 == 0) && arr[i - 1] < arr[i]) {
-                largerEvenLength++;
-                maxLength = max(maxLength, largerEvenLength);
-            } else {
-                largerEvenLength = 1;
-            }
-        }
-
-        return maxLength;
+        return ans;
     }
 };
