625a4d83d1ed92883a93c498d8ea08d4fa20f5c4
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-11 22:19:52 +0800

    Time: 0 ms (100%), Space: 69.6 MB (26.13%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,24 +4,45 @@ public:
     // Time: O(n)
     // Space: O(1)
 
+    // int longestOnes(vector<int>& nums, int k) {
+    //     int n = nums.size();
+    //     if (n == 0) return 0;
+        
+    //     int start = 0, end = 0;
+    //     int maxLength = 0;
+    //     int numZeros = 0;
+        
+    //     while (end < n) {
+    //         if (nums[end] == 0) numZeros++;
+    //         // shrink the window from start if it is invalid
+    //         while (numZeros > k) {
+    //             if (nums[start] == 0) numZeros--;
+    //             start++;
+    //         }
+    //         maxLength = max(maxLength, end - start + 1);
+    //         end++;
+    //     }
+    //     return maxLength;
+    // }
+
     int longestOnes(vector<int>& nums, int k) {
         int n = nums.size();
         if (n == 0) return 0;
-        
-        int start = 0, end = 0;
-        int maxLength = 0;
-        int numZeros = 0;
-        
-        while (end < n) {
-            if (nums[end] == 0) numZeros++;
-            // shrink the window from start if it is invalid
-            while (numZeros > k) {
-                if (nums[start] == 0) numZeros--;
-                start++;
+
+        int zeroCnt = 0;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (nums[right] == 0) zeroCnt++;
+
+            while (zeroCnt > k && left <= right) {
+                if (nums[left] == 0) zeroCnt--;
+                left++;
             }
-            maxLength = max(maxLength, end - start + 1);
-            end++;
+
+            ans = max(ans, right - left + 1);
         }
-        return maxLength;
+        return ans;
     }
 };
