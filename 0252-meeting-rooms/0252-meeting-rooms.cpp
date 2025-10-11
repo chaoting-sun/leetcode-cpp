3df495b928319d68038dc9c509fab112a94d7afb
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-11 21:42:42 +0800

    Time: 0 ms (100%), Space: 15.7 MB (99.68%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -7,11 +7,27 @@ public:
     // Time: O(nlogn) for sorting
     // Space: O(1)
 
+    // bool canAttendMeetings(vector<vector<int>>& intervals) {
+    //     int n = intervals.size();
+    //     if (n <= 1) return true;
+
+    //     sort(intervals.begin(), intervals.end(), [&](vector<int> &a, vector<int> &b) {
+    //         return a[0] < b[0];
+    //     });
+
+    //     for (int i = 0; i < n - 1; i++) {
+    //         if (intervals[i][1] > intervals[i + 1][0]) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
     bool canAttendMeetings(vector<vector<int>>& intervals) {
         int n = intervals.size();
         if (n <= 1) return true;
-
-        sort(intervals.begin(), intervals.end(), [&](vector<int> &a, vector<int> &b) {
+        
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
             return a[0] < b[0];
         });
 
