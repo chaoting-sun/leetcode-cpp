d448f28b64bc5464e2b2a1f74ff644cb3b6621ad
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-14 22:06:01 +0800

    Time: 4 ms (38.2%), Space: 16.1 MB (83.63%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -42,35 +42,55 @@ public:
     // Time: O(nlogn)
     // Space: O(n)
 
+    // int minMeetingRooms(vector<vector<int>>& intervals) {
+    //     int n = intervals.size();
+    //     if (n <= 1) return n;
+
+    //     vector<int> start_times(n), end_times(n);
+    //     for (int i = 0; i < n; i++) {
+    //         start_times[i] = intervals[i][0];
+    //         end_times[i] = intervals[i][1];
+    //     }
+
+    //     sort(start_times.begin(), start_times.end());
+    //     sort(end_times.begin(), end_times.end());
+
+    //     int rooms = 0;
+    //     int i = 0, j = 0;
+
+    //     while (i < n) {
+    //         // the meeting of earliest end time has not finished, so we need to increase a room
+    //         if (start_times[i] < end_times[j]) {
+    //             rooms++;
+    //             i++;
+    //         } 
+    //         // there is a meeting ended. a previously used room is empty now, so we can use it for the current meeting
+    //         else {
+    //             i++;
+    //             j++;
+    //         }
+    //     }
+
+    //     return rooms;
+    // }
+
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        if (n <= 1) return n;
-
-        vector<int> start_times(n), end_times(n);
-        for (int i = 0; i < n; i++) {
-            start_times[i] = intervals[i][0];
-            end_times[i] = intervals[i][1];
-        }
-
-        sort(start_times.begin(), start_times.end());
-        sort(end_times.begin(), end_times.end());
-
-        int rooms = 0;
-        int i = 0, j = 0;
-
-        while (i < n) {
-            // the meeting of earliest end time has not finished, so we need to increase a room
-            if (start_times[i] < end_times[j]) {
-                rooms++;
-                i++;
-            } 
-            // there is a meeting ended. a previously used room is empty now, so we can use it for the current meeting
-            else {
-                i++;
-                j++;
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
+            return a[0] < b[0];
+        });
+        priority_queue<int, vector<int>, greater<>> pq; // minheap
+        int maxRooms = 0;
+        for (auto& interval: intervals) {
+            while (!pq.empty() && pq.top() <= interval[0]) {
+                pq.pop();
             }
+            pq.push(interval[1]);
+            maxRooms = max(maxRooms, (int)pq.size());
         }
-
-        return rooms;
+        return maxRooms;
     }
 };
+
+// [(0,1)] -> 1
+// [(0,2),(1,3)] -> 2
+// [(0,2),(2,4)] -> 1
