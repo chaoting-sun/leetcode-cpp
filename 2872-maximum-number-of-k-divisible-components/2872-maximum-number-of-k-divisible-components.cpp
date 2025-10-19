c0deefa0ee5fd09a9368657438c22a28e18ffc35
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-19 18:09:59 +0800

    Time: 109 ms (71.18%), Space: 175.1 MB (72.05%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     int ans = 0;
+    vector<vector<int>> g;
+    vector<int> remainder;
 
-    int dfs(vector<vector<int>>& nodes, vector<int>& values, int root, int f, int k, vector<int>& subtreeSumModK) {
-        for (int v: nodes[root]) {
+    void dfs(vector<int>& values, int root, int f, int k, vector<int>& remainder) {
+        for (int v: g[root]) {
             if (f == v) continue;
-            subtreeSumModK[root] += dfs(nodes, values, v, root, k, subtreeSumModK);
+            dfs(values, v, root, k, remainder);
+            remainder[root] += remainder[v];
         }
-        subtreeSumModK[root] += values[root];
-        subtreeSumModK[root] %= k;
-        if (subtreeSumModK[root] == 0) ans++;
-        return subtreeSumModK[root];
+        remainder[root] += values[root];
+        remainder[root] %= k;
+        if (remainder[root] == 0) {
+            ans++;
+        }
     }
 
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {         
-        vector<vector<int>> nodes(n);
+        g.resize(n);
+        remainder.resize(n);
+
         for (auto& edge: edges) {
             int u = edge[0], v = edge[1];
-            nodes[u].push_back(v);
-            nodes[v].push_back(u);
+            g[u].push_back(v);
+            g[v].push_back(u);
         }
-        vector<int> subtreeSumModK(n);
-        dfs(nodes, values, 0, -1, k, subtreeSumModK);
+        dfs(values, 0, -1, k, remainder);
         return ans;
     }
 };
