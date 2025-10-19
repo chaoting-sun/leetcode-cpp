859aaaeda2c9fc1697f87203e63ea0f8501505b8
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-19 15:27:52 +0800

    Time: 11 ms (95.94%), Space: 95.7 MB (65.68%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,31 +1,43 @@
-class Solution {
-public:
-    int getOperationNumber(int val) {
-        int multiplier = val / 2;
-        for (int i = 0; i <= multiplier; i++) {
-            int v = val - i * 2;
-            if (v % 3 == 0) {
-                return i + v / 3;
-            }
-        }
-        return -1;
-    }
+// Approach
+// 2 * x + 3y = n
+
+// class Solution {
+// public:
+//     int getOperationNumber(int val) {
+//         int multiplier = val / 2;
+//         for (int i = 0; i <= multiplier; i++) {
+//             int v = val - i * 2;
+//             if (v % 3 == 0) {
+//                 return i + v / 3;
+//             }
+//         }
+//         return -1;
+//     }
     
+//     int minOperations(vector<int>& nums) {
+//         unordered_map<int,int> counts;
+//         for (int n: nums) counts[n]++;
+//         int totalOpNumber = 0;
+//         for (auto [_, cnt]: counts) {
+//             int opNumber = getOperationNumber(cnt);
+//             if (opNumber == -1) return -1;
+//             totalOpNumber += opNumber;
+//         }
+//         return totalOpNumber;
+//     }
+// };
+
+class Solution {
+public:    
     int minOperations(vector<int>& nums) {
         unordered_map<int,int> counts;
         for (int n: nums) counts[n]++;
-        int totalOpNumber = 0;
+        int totalOps = 0;
         for (auto [_, cnt]: counts) {
-            int opNumber = getOperationNumber(cnt);
-            if (opNumber == -1) return -1;
-            totalOpNumber += opNumber;
+            if (cnt == 1) return -1;
+            totalOps += cnt / 3;
+            if (cnt % 3 > 0) totalOps++;
         }
-        return totalOpNumber;
+        return totalOps;
     }
 };
-
-// 2 * x + 3y = n
-
-// x = 0, y = n / 3
-// x = n / 2, y = 0
-
