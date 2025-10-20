382be42bc641b870399dc07012d4a93f64c5300f
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-20 21:35:28 +0800

    Time: 0 ms (100%), Space: 72.2 MB (71.47%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
+    // Approach: Brute-force
+    // Time: O(n^2)
+    // Space: O(1)
+
+    // vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+    //     vector<int> ans(n);
+    //     for (auto& booking: bookings) {
+    //         int first = booking[0], last = booking[1], seats = booking[2];
+    //         for (int i = first; i <= last; i++) {
+    //             ans[i - 1] += seats;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    // Approach: Line Sweep
+    // Time: O(n)
+    // Space: O(1)
+
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int> ans(n);
         for (auto& booking: bookings) {
             int first = booking[0], last = booking[1], seats = booking[2];
-            for (int i = first; i <= last; i++) {
-                ans[i - 1] += seats;
-            }
+            ans[first - 1] += seats;
+            if (last < n) ans[last] -= seats;
         }
-        return ans;   
-    }
+        for (int i = 1; i < n; i++) ans[i] += ans[i - 1];
+        return ans;
+    }  
 };
