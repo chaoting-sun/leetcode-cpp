f3d48a75a3c2511d1ff3825fc0e407199ac0438b
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-22 22:50:47 +0800

    Time: 0 ms (100%), Space: 13.9 MB (90.26%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
+    // Approach: Merge
+    // Time: O(nlogn)
+    // Space: O(n)
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> occurrence;
-        unordered_set<int> seen;
-        for (int n: nums1) occurrence.insert(n);
-        for (int n: nums2) {
-            if (occurrence.count(n)) seen.insert(n);
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0, j = 0;
+        vector<int> ans;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                if (ans.empty() || ans.back() != nums1[i]) {
+                    ans.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
         }
-        vector<int> ans(seen.begin(), seen.end());
         return ans;
     }
+
+    // Approach: Hash Table
+    // Time: O(n)
+    // Space: O(n)
+    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    //     unordered_set<int> occurrence;
+    //     unordered_set<int> seen;
+    //     for (int n: nums1) occurrence.insert(n);
+    //     for (int n: nums2) {
+    //         if (occurrence.count(n)) seen.insert(n);
+    //     }
+    //     vector<int> ans(seen.begin(), seen.end());
+    //     return ans;
+    // }
 };
