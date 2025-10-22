efb3663ba591c1ce4c46ec35884f29770163a433
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-22 20:30:15 +0800

    Time: 98 ms (11.55%), Space: 44.9 MB (8.45%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.cpp
+++ 0218-the-skyline-problem/0218-the-skyline-problem.cpp
@@ -9,22 +9,56 @@ public:
         
         vector<int> positions(edgeSet.begin(), edgeSet.end());
         vector<vector<int>> ans;
+        priority_queue<pair<int,int>> maxHeap; // height, right side
+        sort(buildings.begin(), buildings.end(), [](vector<int>& b1, vector<int> b2) {
+            return b1[0] < b2[0];
+        });
 
+        int idx = 0;
         for (int position: positions) {
-            int maxHeight = 0;
-            for (auto& building: buildings) {
-                int l = building[0], r = building[1], h = building[2];
-                if (l <= position && position < r) {
-                    maxHeight = max(maxHeight, h);
-                }
+            while (idx < buildings.size() && buildings[idx][0] <= position) {
+                int right = buildings[idx][1], height = buildings[idx][2];
+                maxHeap.push({ height, right });
+                idx++;
             }
+            while (!maxHeap.empty() && maxHeap.top().second <= position) {
+                maxHeap.pop();
+            }
+            int maxHeight = maxHeap.empty() ? 0 : maxHeap.top().first;
             if (ans.empty() || ans.back()[1] != maxHeight) {
                 ans.push_back({ position, maxHeight });
             }
         }
-
         return ans;
     }
+
+    // Approach: Line Sweep
+
+    // vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+    //     set<int> edgeSet;
+    //     for (auto& building: buildings) {
+    //         edgeSet.insert(building[0]);
+    //         edgeSet.insert(building[1]);
+    //     }
+        
+    //     vector<int> positions(edgeSet.begin(), edgeSet.end());
+    //     vector<vector<int>> ans;
+
+    //     for (int position: positions) {
+    //         int maxHeight = 0;
+    //         for (auto& building: buildings) {
+    //             int l = building[0], r = building[1], h = building[2];
+    //             if (l <= position && position < r) {
+    //                 maxHeight = max(maxHeight, h);
+    //             }
+    //         }
+    //         if (ans.empty() || ans.back()[1] != maxHeight) {
+    //             ans.push_back({ position, maxHeight });
+    //         }
+    //     }
+
+    //     return ans;
+    // }
 
     // vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
     //     set<int> edgeSet;
