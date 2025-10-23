c20271aab08f7bee9bb661433bcbd67e5a696a24
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-23 22:28:07 +0800

    Time: 24 ms (94.21%), Space: 34.4 MB (97.02%) - LeetHub

--- 0391-perfect-rectangle/0391-perfect-rectangle.cpp
+++ 0391-perfect-rectangle/0391-perfect-rectangle.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    long long getKey(int x, int y) {
+        return ((long long)x << 32) ^ ((long long)y - INT_MIN);
+    }
+
     bool isRectangleCover(vector<vector<int>>& rectangles) {
         int minX = INT_MAX, minY = INT_MAX;
         int maxX = INT_MIN, maxY = INT_MIN;
         long long area = 0;
-        set<pair<int,int>> edges;
+        unordered_set<long long> points;
         for (auto& rect: rectangles) {
-            pair<int,int> p1 = { rect[0], rect[1] };
-            pair<int,int> p2 = { rect[0], rect[3] };
-            pair<int,int> p3 = { rect[2], rect[1] };
-            pair<int,int> p4 = { rect[2], rect[3] };
+            long long p1 = getKey(rect[0], rect[1]);
+            long long p2 = getKey(rect[0], rect[3]);
+            long long p3 = getKey(rect[2], rect[1]);
+            long long p4 = getKey(rect[2], rect[3]);
             for (auto& p: { p1, p2, p3, p4 }) {
-                if (edges.count(p)) {
-                    edges.erase(p);
+                if (points.count(p)) {
+                    points.erase(p);
                 } else {
-                    edges.insert(p);
+                    points.insert(p);
                 }
             }
             minX = min(minX, rect[0]);
@@ -23,11 +27,11 @@ public:
             maxY = max(maxY, rect[3]);
             area += (long long)(rect[2] - rect[0]) * (rect[3] - rect[1]);
         }
-        if (edges.size() != 4 ||
-            !edges.count({ minX, minY }) ||
-            !edges.count({ minX, maxY }) ||
-            !edges.count({ maxX, minY }) ||
-            !edges.count({ maxX, maxY })) {
+        if (points.size() != 4) return false;
+        if (!points.count(getKey(minX, minY)) ||
+            !points.count(getKey(minX, maxY)) ||
+            !points.count(getKey(maxX, minY)) ||
+            !points.count(getKey(maxX, maxY))) {
             return false;
         }
         return area == (long long)(maxX - minX) * (maxY - minY);
