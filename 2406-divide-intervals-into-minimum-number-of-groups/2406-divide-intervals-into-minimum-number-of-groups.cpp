65ea79cc78ffbb15568057bd1fcada3b259180e0
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-24 11:40:21 +0800

    Time: 223 ms (13.87%), Space: 122.3 MB (27.15%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -4,17 +4,35 @@ public:
     // Time: O(nlogn)
     // Space: O(n)
 
+    // int minGroups(vector<vector<int>>& intervals) {
+    //     sort(intervals.begin(), intervals.end());
+    //     priority_queue<int, vector<int>, greater<>> pq;
+    //     for (auto& interval: intervals) {
+    //         if (!pq.empty() && pq.top() < interval[0]) {
+    //             pq.pop();
+    //             pq.push(interval[1]);
+    //         } else {
+    //             pq.push(interval[1]);
+    //         }
+    //     }
+    //     return pq.size();
+    // }
+
+    // Approach: Line Sweep -> We find where most intervals intersect with each other
+    // Time: O(nlogn)
+    // Space: O(n)
+
     int minGroups(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        priority_queue<int, vector<int>, greater<>> pq;
+        map<int,int> intervalCounts;
         for (auto& interval: intervals) {
-            if (!pq.empty() && pq.top() < interval[0]) {
-                pq.pop();
-                pq.push(interval[1]);
-            } else {
-                pq.push(interval[1]);
-            }
+            intervalCounts[interval[0]]++;
+            intervalCounts[interval[1] + 1]--;
         }
-        return pq.size();
+        int currentCount = 0, maxCount = 0;
+        for (auto [_, cnt]: intervalCounts) {
+            currentCount += cnt;
+            maxCount = max(maxCount, currentCount);
+        }
+        return maxCount;
     }
 };
