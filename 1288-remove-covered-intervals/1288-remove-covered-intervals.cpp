0691467a6d97c8a2d143fae3236ba1cf8ddbb41d
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-24 08:54:46 +0800

    Time: 1 ms (51.57%), Space: 15.1 MB (84.71%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -9,17 +9,11 @@ public:
         int n = intervals.size();
         int maxRight = intervals[0][1];
         int remainingIntervals = n;
-        vector<bool> isRemoved(n, false);
 
         for (int i = 0; i < n - 1; i++) {
             maxRight = max(maxRight, intervals[i][1]);
             if (maxRight >= intervals[i + 1][1]) {
                 remainingIntervals--;
-                isRemoved[i + 1] = true;
-                if (intervals[i][0] == intervals[i + 1][0] && intervals[i][1] == intervals[i + 1][1] && !isRemoved[i]) {
-                    remainingIntervals--;
-                    isRemoved[i] = true;
-                }
             }
         }
         return remainingIntervals;
