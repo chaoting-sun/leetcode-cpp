c958c51c4d83fcd6332c8dfca81771160606b98a
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-25 21:24:09 +0800

    Time: 0 ms (100%), Space: 44.2 MB (27.91%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,23 +1,57 @@
+// Approach: Line Sweep
+// Time: O(nlogn)
+// Space: O(n)
+
+// class MyCalendar {
+// public:
+//     map<int,int> events;
+
+//     MyCalendar() {
+        
+//     }
+    
+//     bool book(int startTime, int endTime) {
+//         events[startTime]++;
+//         events[endTime]--;
+//         int eventCount = 0;
+//         for (auto& [t, e]: events) {
+//             eventCount += e;
+//             if (eventCount > 1) {
+//                 events[startTime]--;
+//                 events[endTime]++;
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+// Approach: Set
+// Time: O(nlogn)
+// Space: O(n)
+
 class MyCalendar {
 public:
-    map<int,int> events;
+    set<pair<int,int>> calendar;
 
     MyCalendar() {
         
     }
     
     bool book(int startTime, int endTime) {
-        events[startTime]++;
-        events[endTime]--;
-        int eventCount = 0;
-        for (auto& [t, e]: events) {
-            eventCount += e;
-            if (eventCount > 1) {
-                events[startTime]--;
-                events[endTime]++;
+        pair<int,int> booking = { startTime, endTime };
+        // find the first existing booking with s1 >= startTime or e1 >= endTime
+        auto nextEvent = calendar.lower_bound(booking);
+        if (nextEvent != calendar.end() && nextEvent->first < endTime) {
+            return false;
+        }
+        if (nextEvent != calendar.begin()) {
+            auto prevEvent = prev(nextEvent);
+            if (startTime < prevEvent->second) {
                 return false;
             }
         }
+        calendar.insert(booking);
         return true;
     }
 };
