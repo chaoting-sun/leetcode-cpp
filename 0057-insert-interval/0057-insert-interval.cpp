befc5b978622d9be3154493f15d647862c35d730
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-27 21:57:38 +0800

    Time: 3 ms (31.47%), Space: 21.8 MB (21.35%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,60 +2,31 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n = intervals.size();
-
-        // find the first interval with interval[1] >= newInterval[0]
-        int left = -1, right = n;
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            if (intervals[mid][1] >= newInterval[0]) {
-                right = mid;
-            } else {
-                left = mid;
-            }
-        }
-        int firstIdx = right;
-
-        // find the last interval with interval[0] <= newInterval[1]
-        left = -1;
-        right = n;
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            if (intervals[mid][0] <= newInterval[1]) {
-                left = mid;
-            } else {
-                right = mid;
-            }
-        }
-        int lastIdx = left;
-
         vector<vector<int>> ans;
-        if (lastIdx == -1) {
-            ans.push_back(newInterval);
-            for (auto& iv: intervals) ans.push_back(iv);
-        } else if (firstIdx == n) {
-            for (auto& iv: intervals) ans.push_back(iv);
-            ans.push_back(newInterval);
-        } else if (lastIdx < firstIdx) {
-            for (int i = 0; i <= lastIdx; i++) ans.push_back(intervals[i]);
-            ans.push_back(newInterval);
-            for (int i = firstIdx; i < n; i++) ans.push_back(intervals[i]);
-        } else {
-            int minStart = min(intervals[firstIdx][0], newInterval[0]);
-            int maxEnd = max(intervals[lastIdx][1], newInterval[1]);
-            for (int i = 0; i < n; i++) {
-                if (i < firstIdx || i > lastIdx) {
-                    ans.push_back(intervals[i]);
-                } else if (i == firstIdx) {
-                    ans.push_back({ minStart, maxEnd });
-                }
-            }
+        int index = 0;
+
+        // those before and not overlapped with newInterval
+        while (index < n && intervals[index][1] < newInterval[0]) {
+            ans.push_back(intervals[index]);
+            index++;
+        }
+        
+        // those overlapped with newInterval
+        int minL = newInterval[0];
+        int maxR = newInterval[1];
+        while (index < n && intervals[index][0] <= newInterval[1]) {
+            minL = min(minL, intervals[index][0]);
+            maxR = max(maxR, intervals[index][1]);
+            index++;
+        }
+        ans.push_back({ minL, maxR });
+
+        while (index < n) {
+            ans.push_back(intervals[index]);
+            index++;
         }
         return ans;
     }
-
-    //             -- ---- ----- ----
-    //  --------
-
 
     // Approach: handle 3 cases
     // - non-overlapping intervals at the left of newInterval
