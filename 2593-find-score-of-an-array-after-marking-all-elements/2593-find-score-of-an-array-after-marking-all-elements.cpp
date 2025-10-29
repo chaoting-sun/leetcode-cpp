05ac3e755cd71bb7c14141c3e2d9d3e0d0ac3d95
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-29 22:50:01 +0800

    Time: 0 ms (100%), Space: 93.4 MB (93.75%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -2,25 +2,49 @@ class Solution {
 public:
     long long findScore(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> marked(n, false);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        stack<int> stk;
         long long score = 0;
         for (int i = 0; i < n; i++) {
-            pq.push({ nums[i], i });
-        }
-        while (!pq.empty()) {
-            while (!pq.empty() && marked[pq.top().second]) {
-                pq.pop();
+            if (!stk.empty() && stk.top() <= nums[i]) {
+                while (!stk.empty()) {
+                    score += stk.top();
+                    stk.pop();
+                    if (!stk.empty()) stk.pop();
+                }
+                continue;
             }
-            if (pq.empty()) break;
-
-            auto [val, idx] = pq.top();
-            pq.pop();
-            score += val;
-            marked[idx] = true;
-            if (idx > 0) marked[idx - 1] = true;
-            if (idx < n - 1) marked[idx + 1] = true;
+            stk.push(nums[i]);
         }
+        while (!stk.empty()) {
+            score += stk.top();
+            stk.pop();
+            if (!stk.empty()) stk.pop();
+        }
+
         return score;
     }
+
+    // long long findScore(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<bool> marked(n, false);
+    //     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    //     long long score = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         pq.push({ nums[i], i });
+    //     }
+    //     while (!pq.empty()) {
+    //         while (!pq.empty() && marked[pq.top().second]) {
+    //             pq.pop();
+    //         }
+    //         if (pq.empty()) break;
+
+    //         auto [val, idx] = pq.top();
+    //         pq.pop();
+    //         score += val;
+    //         marked[idx] = true;
+    //         if (idx > 0) marked[idx - 1] = true;
+    //         if (idx < n - 1) marked[idx + 1] = true;
+    //     }
+    //     return score;
+    // }
 };
