32fa29ebb4c21ca48b6cc3d03cf6e8b911c1e885
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-29 21:42:19 +0800

    Time: 17 ms (32.75%), Space: 80.8 MB (75.44%) - LeetHub

--- 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     int maximizeGreatness(vector<int>& nums) {
         int ans = 0;
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (int n: nums) pq.push(n);
         sort(nums.begin(), nums.end());
+        int i = 0;
         for (int n: nums) {
-            while (!pq.empty() && pq.top() <= n) {
-                pq.pop();
-            }
-            if (!pq.empty()) {
-                pq.pop();
+            if (n > nums[i]) {
                 ans++;
+                i++;
             }
         }
         return ans;
