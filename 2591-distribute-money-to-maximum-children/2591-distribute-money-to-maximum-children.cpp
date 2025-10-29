1644bcd24d433a424e47e7059c634ef03e5b6d0d
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-29 21:16:04 +0800

    Time: 0 ms (100%), Space: 10 MB (64.48%) - LeetHub

--- 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cpp
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cpp
@@ -3,22 +3,15 @@ public:
     int distMoney(int money, int children) {
         if (money < children) return -1;
         if (money > children * 8) return children - 1;
-        int leftMoney = money - children;
-        int numEights = leftMoney / 7;
-        int leftChildren = children - numEights;
-        if (leftMoney - numEights * 7 == 3 && leftChildren == 1) {
+        
+        int numEights = (money - children) / 7;
+        int remainder = (money - children) % 7;
+        
+        // 特殊情況：剩餘1人且剩3元（會導致最後一人拿4元）
+        if (numEights == children - 1 && remainder == 3) {
             return numEights - 1;
-        } else {
-            return numEights;
         }
+        
+        return numEights;
     }
 };
-
-// money < children -> -1
-
-// money / 8 = 0 -> 0
-//             k -> money % 8 >= n - k
-
-// 8k + (n-k) * 1 <= money
-// n + 7k <= money
-// k <= (money - n) / 7
