3915fbb1a97061cdbc86c7139ce4072204590f4a
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-30 22:09:15 +0800

    Time: 0 ms (100%), Space: 10.6 MB (59.84%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.cpp
+++ 0362-design-hit-counter/0362-design-hit-counter.cpp
@@ -1,32 +1,21 @@
 class HitCounter {
+private:
+    queue<int> hits;
 public:
-    vector<int> hits;
 
     HitCounter() {
-        
     }
     
     void hit(int timestamp) {
-        hits.push_back(timestamp);
+        this->hits.push(timestamp);
     }
     
     int getHits(int timestamp) {
-        if (hits.empty()) return 0;
-
-        int target = timestamp - 300 + 1;
-        if (target < 0) return hits.size();
-        
-        // find 1st number >= timestamp - 300
-        int left = -1, right = hits.size();
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
-            if (hits[mid] >= target) {
-                right = mid;
-            } else {
-                left = mid;
-            }
+        int minTimeStamp = timestamp - 300 + 1;
+        while (!this->hits.empty() && this->hits.front() < minTimeStamp) {
+            this->hits.pop();
         }
-        return hits.size() - right;
+        return this->hits.size();
     }
 };
 
