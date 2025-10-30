8dcb25ea992687ff386be8a450681261307da15c
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-30 18:24:58 +0800

    Time: 16 ms (37.9%), Space: 80.7 MB (93.01%) - LeetHub

--- 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.cpp
@@ -1,15 +1,33 @@
+// class Solution {
+// public:
+//     int maximizeGreatness(vector<int>& nums) {
+//         int ans = 0;
+//         sort(nums.begin(), nums.end());
+//         int i = 0;
+//         for (int n: nums) {
+//             if (n > nums[i]) {
+//                 ans++;
+//                 i++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int maximizeGreatness(vector<int>& nums) {
         int ans = 0;
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        int i = 0;
-        for (int n: nums) {
-            if (n > nums[i]) {
+        int i = n - 1;
+        for (int j = n - 1; j >= 0; j--) {
+            if (nums[j] < nums[i]) {
                 ans++;
-                i++;
+                i--;
             }
         }
         return ans;
     }
 };
+
