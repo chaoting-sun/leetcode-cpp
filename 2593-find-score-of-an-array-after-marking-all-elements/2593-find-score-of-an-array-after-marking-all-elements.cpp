beb2e82df85cc72724ac06c4a5d4d2f1dd0f4712
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-10-30 20:42:49 +0800

    Time: 0 ms (100%), Space: 92 MB (98.93%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,28 +1,54 @@
 class Solution {
 public:
+    // [3,2,1,10]
+
     long long findScore(vector<int>& nums) {
         int n = nums.size();
-        stack<int> stk;
         long long score = 0;
-        for (int i = 0; i < n; i++) {
-            if (!stk.empty() && stk.top() <= nums[i]) {
-                while (!stk.empty()) {
-                    score += stk.top();
-                    stk.pop();
-                    if (!stk.empty()) stk.pop();
+        int lastEnd = -1;
+        int i = 1;
+        while (i < n) {
+            if (nums[i - 1] <= nums[i]) {
+                int j = i - 1;
+                while (lastEnd < j) {
+                    score += nums[j];
+                    j -= 2;
                 }
-                continue;
+                lastEnd = i;
+                i += 2;
+            } else {
+                i++;
             }
-            stk.push(nums[i]);
         }
-        while (!stk.empty()) {
-            score += stk.top();
-            stk.pop();
-            if (!stk.empty()) stk.pop();
+        for (int i = n - 1; i > lastEnd; i -= 2) {
+            score += nums[i];
         }
-
         return score;
     }
+
+    // long long findScore(vector<int>& nums) {
+    //     int n = nums.size();
+    //     stack<int> stk;
+    //     long long score = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         if (!stk.empty() && stk.top() <= nums[i]) {
+    //             while (!stk.empty()) {
+    //                 score += stk.top();
+    //                 stk.pop();
+    //                 if (!stk.empty()) stk.pop();
+    //             }
+    //             continue;
+    //         }
+    //         stk.push(nums[i]);
+    //     }
+    //     while (!stk.empty()) {
+    //         score += stk.top();
+    //         stk.pop();
+    //         if (!stk.empty()) stk.pop();
+    //     }
+
+    //     return score;
+    // }
 
     // long long findScore(vector<int>& nums) {
     //     int n = nums.size();
