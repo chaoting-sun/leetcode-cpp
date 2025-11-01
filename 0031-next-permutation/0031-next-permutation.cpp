5d57d2d17ff8bf0d056d20620349b4f87411afab
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-01 11:30:56 +0800

    Time: 0 ms (100%), Space: 15.7 MB (32.62%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,44 +2,20 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        bool isReverseOrder = true;
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] < nums[i]) {
-                isReverseOrder = false;
-                break;
-            }
+        int i = n - 1;
+        while (i > 0 && nums[i - 1] >= nums[i]) {
+            i--;
         }
-        if (isReverseOrder) {
+        if (i == 0) {
             sort(nums.begin(), nums.end());
             return;
         }
-        
-        int maxValue = INT_MIN;
-        vector<int> freq(101);
-        for (int i = n - 1; i >= 0; i--) {
-            maxValue = max(maxValue, nums[i]);
-            freq[nums[i]]++;
-            for (int j = nums[i] + 1; j <= maxValue; j++) {
-                if (!freq[j]) continue;
-                nums[i] = j;
-                freq[j]--;
-                int p = i + 1;
-                for (int k = 0; k <= maxValue; k++) {
-                    while (freq[k]--) {
-                        nums[p++] = k;
-                    }
-                }
-                return;
-            }
+        i--;
+        int j = n - 1;
+        while (i < j && nums[i] >= nums[j]) {
+            j--;
         }
+        swap(nums[i], nums[j]);
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
-
-// [1,1,5]
-// [0,0,0,0,0,1]
-
-
-// [1,2,3,4,5]
-
-// [1,2,3,5,4]
-// [1,2,4,3,5]
