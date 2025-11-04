04d3299f0a79a7419ca546cb6cfde4bf39c80aca
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-04 23:17:58 +0800

    Time: 0 ms (100%), Space: 15.4 MB (25%) - LeetHub

--- 1842-next-palindrome-using-same-digits/1842-next-palindrome-using-same-digits.cpp
+++ 1842-next-palindrome-using-same-digits/1842-next-palindrome-using-same-digits.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-    bool nextPermutation(string& s) {
-        int n = s.size();
-        if (n <= 1) return false;
-
-        int pivot = n - 2;
-        while (pivot >= 0 && s[pivot] >= s[pivot + 1]) {
-            pivot--;
-        }
-        if (pivot == -1) return false;
-        int nextGreaterIdx = n - 1;
-        while (pivot < nextGreaterIdx && s[pivot] >= s[nextGreaterIdx]) {
-            nextGreaterIdx--;
-        }
-        swap(s[pivot], s[nextGreaterIdx]);
-        reverse(s.begin() + pivot + 1, s.end());
-        return true;
-    }
-
     string nextPalindrome(string num) {
         int n = num.size();
         int nHalf = n / 2;
@@ -36,5 +18,24 @@ public:
         reverse(sHalfReversed.begin(), sHalfReversed.end());
         ans += sHalfReversed;
         return ans;
+    }
+
+private:
+    bool nextPermutation(string& s) {
+        int n = s.size();
+        if (n <= 1) return false;
+
+        int pivot = n - 2;
+        while (pivot >= 0 && s[pivot] >= s[pivot + 1]) {
+            pivot--;
+        }
+        if (pivot == -1) return false;
+        int nextGreaterIdx = n - 1;
+        while (pivot < nextGreaterIdx && s[pivot] >= s[nextGreaterIdx]) {
+            nextGreaterIdx--;
+        }
+        swap(s[pivot], s[nextGreaterIdx]);
+        reverse(s.begin() + pivot + 1, s.end());
+        return true;
     }
 };
