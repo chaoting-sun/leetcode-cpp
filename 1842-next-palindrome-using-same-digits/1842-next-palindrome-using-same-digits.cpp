9374079c9b49fe0ff6734ff5e0eb0dbc19350c02
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-04 23:17:31 +0800

    Time: 1 ms (45%), Space: 15.6 MB (25%) - LeetHub

--- 1842-next-palindrome-using-same-digits/1842-next-palindrome-using-same-digits.cpp
+++ 1842-next-palindrome-using-same-digits/1842-next-palindrome-using-same-digits.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
+    bool nextPermutation(string& s) {
+        int n = s.size();
+        if (n <= 1) return false;
+
+        int pivot = n - 2;
+        while (pivot >= 0 && s[pivot] >= s[pivot + 1]) {
+            pivot--;
+        }
+        if (pivot == -1) return false;
+        int nextGreaterIdx = n - 1;
+        while (pivot < nextGreaterIdx && s[pivot] >= s[nextGreaterIdx]) {
+            nextGreaterIdx--;
+        }
+        swap(s[pivot], s[nextGreaterIdx]);
+        reverse(s.begin() + pivot + 1, s.end());
+        return true;
+    }
+
     string nextPalindrome(string num) {
-        int n = num.size(), half = n / 2;
-        string ans(num);
-        int pivot = n % 2 == 1 ? half + 2 : half + 1;
-        while (pivot < n && ans[pivot - 1] <= ans[pivot]) {
-            pivot++;
+        int n = num.size();
+        int nHalf = n / 2;
+        string sHalf = num.substr(0, nHalf);
+
+        if (!nextPermutation(sHalf)) {
+            return "";
         }
-        if (pivot >= n) return "";
-        int prevGreaterIdx = n % 2 == 1 ? half + 1 : half;
-        while (prevGreaterIdx < pivot && ans[prevGreaterIdx] <= ans[pivot]) {
-            prevGreaterIdx++;
-        }
-        swap(ans[prevGreaterIdx], ans[pivot]);
+
+        string ans(sHalf);
         if (n % 2 == 1) {
-            reverse(ans.begin() + half + 1, ans.begin() + pivot);
-        } else {
-            reverse(ans.begin() + half, ans.begin() + pivot);
+            ans += num[nHalf];
         }
-        for (int i = 0; i < half; i++) {
-            ans[i] = ans[n - 1 - i];
-        }
+        
+        string sHalfReversed(sHalf);
+        reverse(sHalfReversed.begin(), sHalfReversed.end());
+        ans += sHalfReversed;
         return ans;
     }
 };
-
-// 121
-// 31435 5 53413
-
-// 31455 3 55413
-// 31453 5 35413
-
-// 131131
-
-// 1212
-// half = 2
-// pivot = 3
-// prevGreaterIdx = 2
-// reverse: 2~2
-
-// 0 1 2 3 4  5 6 7 8 9
-// 1 3 2 2 1  3 2 1 1 2
-// n = 10
-// half = 5
-// pivot = 9
-// prevGreaterIdx = 6
-
-// 5 4 4 5 5 4 4 5
