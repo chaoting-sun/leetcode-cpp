27ce2917700f3dbf620f00bd95b83dd5ea6b5739
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-07 23:23:16 +0800

    Time: 0 ms (100%), Space: 15 MB (61.04%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,6 +11,25 @@
  */
 class Solution {
 public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) return {};
+
+        vector<int> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int sz = q.size();
+            ans.push_back(q.front()->val);
+            for (int i = 0; i < sz; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->right) q.push(node->right);
+                if (node->left) q.push(node->left);
+            }
+        }
+        return ans;
+    }
+
     // Approach: Recursion
     // Time: O(n)
     // Space: O(n). skewwed tree -> O(n), balanced tree -> O(logn)
@@ -39,23 +58,23 @@ public:
     // Time: O(n)
     // Space: O(n). skewwed -> O(1), balanced tree -> O(n)
 
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        queue<TreeNode*> q;
+    // vector<int> rightSideView(TreeNode* root) {
+    //     vector<int> ans;
+    //     queue<TreeNode*> q;
 
-        if (!root) return ans;
+    //     if (!root) return ans;
         
-        q.push(root);
-        while (!q.empty()) {
-            int n = q.size();
-            ans.push_back(q.front()->val);
-            for (int i = 0; i < n; i++) {
-                if (q.front()->right) q.push(q.front()->right);
-                if (q.front()->left) q.push(q.front()->left);
-                q.pop();
-            }
-        }
+    //     q.push(root);
+    //     while (!q.empty()) {
+    //         int n = q.size();
+    //         ans.push_back(q.front()->val);
+    //         for (int i = 0; i < n; i++) {
+    //             if (q.front()->right) q.push(q.front()->right);
+    //             if (q.front()->left) q.push(q.front()->left);
+    //             q.pop();
+    //         }
+    //     }
 
-        return ans;
-    }
+    //     return ans;
+    // }
 };
