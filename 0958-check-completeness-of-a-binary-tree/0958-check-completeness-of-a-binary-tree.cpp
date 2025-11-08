4957630517af20381b9ccfdd3786797d2c56e8d7
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-08 12:02:01 +0800

    Time: 0 ms (100%), Space: 13.7 MB (54.61%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -9,37 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+using ull = unsigned long long;
+
 class Solution {
 public:
-    bool isCompleteTree(TreeNode* root) {
-        if (!root) return true;
-
-        queue<TreeNode*> q;
-        q.push(root);
-        bool lackNode = false;
-        while (!q.empty()) {
-            int sz = q.size();
-            for (int i = 0; i < sz; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                if (!node) {
-                    lackNode = true;
-                    continue;
-                }
-
-                if (lackNode) return false;
-                q.push(node->left);
-                q.push(node->right);
-            }
-        }
-        return true;
+    void dfs(TreeNode* root, ull index, ull& maxIndex, ull& count) {
+        if (!root) return;
+        count++;
+        maxIndex = max(maxIndex, index);
+        dfs(root->left, 2 * index, maxIndex, count);
+        dfs(root->right, 2 * index + 1, maxIndex, count);
     }
+
+    bool isCompleteTree(TreeNode* root) {
+        ull maxIndex = 1;
+        ull count = 0;
+        dfs(root, 1, maxIndex, count);
+        return maxIndex == count;
+    }
+
+    // bool isCompleteTree(TreeNode* root) {
+    //     if (!root) return true;
+
+    //     queue<TreeNode*> q;
+    //     q.push(root);
+    //     bool lackNode = false;
+    //     while (!q.empty()) {
+    //         int sz = q.size();
+    //         for (int i = 0; i < sz; i++) {
+    //             TreeNode* node = q.front();
+    //             q.pop();
+    //             if (!node) {
+    //                 lackNode = true;
+    //                 continue;
+    //             }
+
+    //             if (lackNode) return false;
+    //             q.push(node->left);
+    //             q.push(node->right);
+    //         }
+    //     }
+    //     return true;
+    // }
 };
-
-// [1,2,3,4,5,6,7,8,9,10,11,12,13,null,null,15]
-
-//             1
-//            2 3
-//           4 5 6
-//          7 8 9 10
-//  11 12 13 null null 15
