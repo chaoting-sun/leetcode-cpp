ab20192a07cf4445431c2520024d0b1ea60911d6
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-08 22:17:41 +0800

    Time: 24 ms (89.74%), Space: 68.2 MB (67.5%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,19 +1,51 @@
 class Solution {
 public:
+    // Approach: Max Heap
+    // Time: O(n*logn)
+    // Space: O(n)
+
+    // vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    //     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // distance^2, index
+
+    //     for (int i = 0; i < points.size(); i++) {
+    //         int x = points[i][0];
+    //         int y = points[i][1];
+    //         pq.push({ x*x + y*y, i });
+    //     }
+
+    //     vector<vector<int>> ans;
+
+    //     while (k--) {
+    //         int index = pq.top().second;
+    //         pq.pop();
+    //         ans.push_back(points[index]);
+    //     }
+
+    //     return ans;
+    // }
+
+    // Approach: Sort
+    // Time: O(n*logn)
+    // Space: O(n)
+
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int,int>> pq; // max heap (dist^2, index)
+        vector<pair<int, int>> distances;
+
+        for (int i = 0; i < points.size(); i++) {
+            int x = points[i][0];
+            int y = points[i][1];
+            distances.push_back({ x*x + y*y, i });
+        }
+
+        sort(distances.begin(), distances.end());
+
         vector<vector<int>> ans;
-        int n = points.size();
-        for (int i = 0; i < n; i++) {
-            int x = points[i][0], y = points[i][1];
-            int distSquared = x * x + y * y;
-            pq.push({ distSquared, i });
-            if (pq.size() > k) pq.pop();
-        }
+
         for (int i = 0; i < k; i++) {
-            ans.push_back(points[pq.top().second]);
-            pq.pop();
+            int index = distances[i].second;
+            ans.push_back(points[index]);
         }
+
         return ans;
     }
 };
