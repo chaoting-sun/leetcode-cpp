767f92eb3d013fd360bc20b68f8a554ffb46fb2e
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-08 22:17:29 +0800

    Time: 28 ms (82.8%), Space: 66.8 MB (79.82%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    // Approach: Max Heap
-    // Time: O(n*logn)
-    // Space: O(n)
-
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // distance^2, index
-
-        for (int i = 0; i < points.size(); i++) {
-            int x = points[i][0];
-            int y = points[i][1];
-            pq.push({ x*x + y*y, i });
-        }
-
+        priority_queue<pair<int,int>> pq; // max heap (dist^2, index)
         vector<vector<int>> ans;
-
-        while (k--) {
-            int index = pq.top().second;
-            pq.pop();
-            ans.push_back(points[index]);
+        int n = points.size();
+        for (int i = 0; i < n; i++) {
+            int x = points[i][0], y = points[i][1];
+            int distSquared = x * x + y * y;
+            pq.push({ distSquared, i });
+            if (pq.size() > k) pq.pop();
         }
-
+        for (int i = 0; i < k; i++) {
+            ans.push_back(points[pq.top().second]);
+            pq.pop();
+        }
         return ans;
     }
 };
