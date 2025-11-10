b88f8587b430eb494df5408e95ae727779d2b0e5
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-10 19:40:02 +0800

    Time: 328 ms (21.07%), Space: 142.3 MB (11.32%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -13,32 +13,35 @@ public:
         if (deadendsLookup.count(start)) return -1;
 
         q.push(start);
-        int time = 0;
+        int steps = 0;
         while (!q.empty()) {
             int sz = q.size();
             for (int i = 0; i < sz; i++) {
                 string s = q.front();
                 q.pop();
-                if (s == target) return time;
+                if (s == target) return steps;
 
-                for (int j = 0; j < 4; j++) {
-                    string nextS = s;
-                    nextS[j] = (nextS[j] - '0' + 1) % 10 + '0';
-                    if (!deadendsLookup.count(nextS) && !visited.count(nextS)) {
-                        q.push(nextS);
-                        visited.insert(nextS);
+                for (string nei: getNeighbors(s)) {
+                    if (!deadendsLookup.count(nei) && !visited.count(nei)) {
+                        q.push(nei);
+                        visited.insert(nei);
                     }
-
-                    string prevS = s;
-                    prevS[j] = (prevS[j] - '0' + 9) % 10 + '0';
-                    if (!deadendsLookup.count(prevS) && !visited.count(prevS)) {
-                        q.push(prevS);
-                        visited.insert(prevS);
-                    } 
                 }
             }
-            time++;
+            steps++;
         }
         return -1;
+    }
+
+    vector<string> getNeighbors(string& s) {
+        vector<string> result;
+        for (int i = 0; i < 4; i++) {
+            for (int diff = -1; diff <= 1; diff += 2) {
+                string nei = s;
+                nei[i] = (nei[i] - '0' + 10 + diff) % 10 + '0';
+                result.push_back(nei);
+            }
+        }
+        return result;
     }
 };
