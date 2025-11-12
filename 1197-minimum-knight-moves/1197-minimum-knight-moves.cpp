85a18375529cd33687d0670131c861d0b8e35e46
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-12 09:52:27 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    uint64_t pack(int x, int y) {
+        return (uint64_t)x << 32 | (uint32_t)y;
+    }
+
+    int minKnightMoves(int x, int y) {
+        vector<int> dx = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        vector<int> dy = { 2, 1, -1, -2, -2, -1, 1, 2 };
+
+        queue<pair<int,int>> q;
+        unordered_set<uint64_t> visited;
+        q.push({ 0, 0 });
+        int moves = 0;
+        while (!q.empty()) {
+            int sz = q.size();
+            while (sz--) {
+                auto [cx, cy] = q.front();
+                q.pop();
+                if (x == cx && y == cy) return moves;
+
+                for (int i = 0; i < 8; i++) {
+                    int nx = cx + dx[i];
+                    int ny = cy + dy[i];
+                    uint64_t key = pack(nx, ny);
+                    cout << key << endl;
+                    if (visited.count(key)) continue;
+
+                    q.push({ nx, ny });
+                    visited.insert(key);
+                }
+            }
+
+            moves++;
+        }
+        return -1;
+    }
+};
