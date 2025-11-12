096134911554a1c699e62e87906a6f3868c836c0
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-12 10:12:11 +0800

    Time: 591 ms (45.27%), Space: 134.9 MB (29.8%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -5,35 +5,46 @@ public:
     }
 
     int minKnightMoves(int x, int y) {
+        if (x == 0 && y == 0) return 0;
+
         vector<int> dx = { 1, 2, 2, 1, -1, -2, -2, -1 };
         vector<int> dy = { 2, 1, -1, -2, -2, -1, 1, 2 };
+        int directions = 8;
 
-        queue<pair<int,int>> q;
-        unordered_set<uint64_t> visited;
-        q.push({ 0, 0 });
-        visited.insert(pack(0, 0));
+        queue<pair<int,int>> q1, q2;
+        unordered_set<uint64_t> fronts, ends;
+        
+        q1.push({ 0, 0 });
+        q2.push({ x, y });
+        fronts.insert(pack(0, 0));
+        ends.insert(pack(x, y));
+        int moves = 1;
 
-        int moves = 0;
-        while (!q.empty()) {
-            int sz = q.size();
+        while (!q1.empty() && !q2.empty()) {
+            if (q1.size() > q2.size()) {
+                swap(q1, q2);
+                swap(fronts, ends);
+            }
+            
+            int sz = q1.size();
             while (sz--) {
-                auto [cx, cy] = q.front();
-                q.pop();
-                if (x == cx && y == cy) return moves;
-
-                for (int i = 0; i < 8; i++) {
+                auto [cx, cy] = q1.front();
+                q1.pop();
+                for (int i = 0; i < directions; i++) {
                     int nx = cx + dx[i];
                     int ny = cy + dy[i];
                     uint64_t key = pack(nx, ny);
-                    if (visited.count(key)) continue;
+                    if (ends.count(key)) return moves;
 
-                    q.push({ nx, ny });
-                    visited.insert(key);
+                    if (fronts.count(key)) continue;
+                    q1.push({ nx, ny });
+                    fronts.insert(pack(nx, ny));
                 }
             }
 
             moves++;
         }
+
         return -1;
     }
 };
