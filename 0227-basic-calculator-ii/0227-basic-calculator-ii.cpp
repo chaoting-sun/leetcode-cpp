fc3388646152d4410d2e747eb3cccff0cf5fd358
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-16 21:18:11 +0800

    Time: 7 ms (65.8%), Space: 15.4 MB (30.03%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    // digits = [3]
-    // operators = [+]
-
     int calculate(string s) {
-        deque<int> digits;
-        deque<char> operators;
-        int p = 0;
-        int n = s.size();
-        while (p < n) {
-            if (isdigit(s[p])) {
-                int start = p;
-                while (p < n - 1 && isdigit(s[p + 1])) {
-                    p++;
-                }
-                int curr = stoi(s.substr(start, p - start + 1));
-                if (operators.empty() || !(operators.back() == '*' || operators.back() == '/')) {
-                    digits.push_back(curr);
-                } else {
-                    int prev = digits.back();
-                    digits.pop_back();
-                    int res = operators.back() == '*' ? prev * curr : prev / curr;
-                    operators.pop_back();
-                    digits.push_back(res);
-                }
-            } else if (s[p] != ' ') {
-                operators.push_back(s[p]);
+        int num = 0;
+        char sign = '+';
+        stack<int> stk;
+        
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+            if (isdigit(ch)) {
+                num = num * 10 + (ch - '0');
             }
-            p++;
+            if ((!isdigit(ch) && !iswspace(ch)) || i == s.size() - 1) {
+                if (sign == '+') {
+                    stk.push(num);
+                } else if (sign == '-') {
+                    stk.push((-1) * num);
+                } else if (sign == '*') {
+                    int prevNum = stk.top();
+                    stk.pop();
+                    stk.push(prevNum * num);
+                } else {
+                    int prevNum = stk.top();
+                    stk.pop();
+                    stk.push(prevNum / num);
+                }
+                num = 0;
+                sign = ch;
+            }
         }
 
-        while (!operators.empty()) {
-            int curr = digits.front();
-            digits.pop_front();
-            int next = digits.front();
-            digits.pop_front();
-            int res = operators.front() == '+' ? curr + next : curr - next;
-            digits.push_front(res);
-            operators.pop_front();
+        int ans = 0;
+        while (!stk.empty()) {
+            ans += stk.top();
+            stk.pop();
         }
-
-        return digits.front();
+        return ans;
     }
 };
