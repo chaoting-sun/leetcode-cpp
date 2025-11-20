e100c4e407bfc946c1b0dea2f2cb54ab6f5f14fa
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-20 21:51:58 +0800

    Time: 0 ms (100%), Space: 21.9 MB (37.44%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,11 +4,24 @@ public:
     // Time: O(n)
     // Space: O(1)
 
+    // int missingNumber(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int res = 0;
+    //     for (int i = 1; i <= n; i++) res ^= i;
+    //     for (int v: nums) res ^= v;
+    //     return res;
+    // }
+
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int res = 0;
-        for (int i = 1; i <= n; i++) res ^= i;
-        for (int v: nums) res ^= v;
-        return res;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= 0 && nums[i] < n && nums[i] != i) {
+                swap(nums[i], nums[nums[i]]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i) return i;
+        }
+        return n;
     }
 };
