088375484063944b09d82ae53822e9704810c0ca
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-24 07:58:52 +0800

    Time: 373 ms (86.11%), Space: 365.8 MB (35.46%) - LeetHub

--- 3755-find-maximum-balanced-xor-subarray-length/3755-find-maximum-balanced-xor-subarray-length.cpp
+++ 3755-find-maximum-balanced-xor-subarray-length/3755-find-maximum-balanced-xor-subarray-length.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
+    inline long long getKey(int xor_value, int balance_value) {
+        return ((long long)xor_value << 32) | (unsigned int)balance_value;
+    }
+
     int maxBalancedSubarray(vector<int>& nums) {
         int n = nums.size();
-
-        vector<int> xorPrefix(n);
-        xorPrefix[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            xorPrefix[i] = xorPrefix[i - 1] ^ nums[i];
-        }
-        vector<int> oddToEvenPrefix(n);
-        oddToEvenPrefix[0] = nums[0] % 2 == 1 ? 1 : -1;
-        for (int i = 1; i < n; i++) {
-            oddToEvenPrefix[i] = oddToEvenPrefix[i - 1] + (nums[i] % 2 == 1 ? 1 : -1);
-        }
-
-        int ans = 0;
-        map<pair<int,int>, int> mp;
-        mp[{ 0, 0 }] = -1;
-        for (int i = 0; i < n; i++) {
-            pair<int, int> p(xorPrefix[i], oddToEvenPrefix[i]);
-            if (mp.count(p)) {
-                ans = max(ans, i - mp[p]);
+        
+        // 記錄狀態對應到的最早索引位置
+        // Key: {current_xor, current_balance}
+        // Value: index
+        unordered_map<long long, int> first_occurrence;
+        
+        // Base case: 在索引 -1 的位置，XOR 為 0，奇偶平衡為 0
+        first_occurrence[getKey(0, 0)] = -1;
+        
+        int curr_xor = 0;
+        int curr_balance = 0;
+        int max_len = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            // 更新 XOR 狀態
+            curr_xor ^= nums[i];
+            
+            // 更新奇偶平衡狀態 (奇數 +1, 偶數 -1)
+            curr_balance += (nums[i] % 2 != 0 ? 1 : -1);
+            
+            long long key = getKey(curr_xor, curr_balance);
+            
+            // 如果這個狀態曾經出現過，計算長度並更新最大值
+            if (first_occurrence.count(key)) {
+                max_len = max(max_len, i - first_occurrence[key]);
             } else {
-                mp[p] = i;
+                // 否則，記錄這個狀態第一次出現的位置
+                first_occurrence[key] = i;
             }
         }
-        return ans;
+        
+        return max_len;
     }
 };
-
-// prefix[i] = [nums[0] ^ nums[1] ... nums[i]]
-// [j..i]
-// prefix[i] ^ prefix[j - 1] -> XOR of [j..i]
-// prefix[i] == prefix[j - 1] -> prefix[i] ^ prefix[j - 1] == 0
-
-//  0 1 2 3 4 5
-// [0 1 2 3 0 2 .... 2]
-// map -> value -> index at first time
-
-// prefixOddToEven -> prefixOddToEven[i] -> [0 .. i] #odd - #even
-// map -> value -> index at firt time
-
-// => map<value1, value2> -> the first time
