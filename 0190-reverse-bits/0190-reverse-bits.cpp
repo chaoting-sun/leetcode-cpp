5b64f857078b43602f0e753b3a412440c5bd8aa3
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-23 15:45:45 +0800

    Time: 0 ms (100%), Space: 8.2 MB (40.94%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,14 +1,29 @@
+// class Solution {
+// public:
+//     // Bit Manipulation
+
+//     int reverseBits(int n) {
+//         int ans = 0;
+//         int bits = 31;
+//         while (n > 0) {
+//             ans += (n & 1) << bits;
+//             n >>= 1;
+//             bits--;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     // Bit Manipulation
 
     int reverseBits(int n) {
         int ans = 0;
-        int bits = 31;
-        while (n > 0) {
-            ans += (n & 1) << bits;
+
+        for (int i = 0; i < 32; i++) {
+            ans = (ans << 1) | (n & 1);
             n >>= 1;
-            bits--;
         }
         return ans;
     }
