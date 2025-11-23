008345ccfbe1a21d963e8e28e2a183057adeb1ee
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-23 20:31:32 +0800

    Time: 0 ms (100%), Space: 9.7 MB (99.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,56 +1,22 @@
 class Solution {
 public:
-    // Approach: Two pointers. Maintain two pointers from left-most and right-most
-    // index of s, comparing them and moving them toward the center
-    // Time: O(n), where n is the length of s
-    // Space: O(1)
-
-    // Technical Details
-    // - isalnum and tolower are both from <cctype> library
-    // - as s consists only of printable ASCII characters, so it is safe to write
-    //   isalnum(ch) or tolower(ch). If the constraint is not given, a safer method
-    //   would be isalnum(static_cast<unsigned char>(ch), and ...(ignored)
-
-    // bool isPalindrome(string s) {
-    //     int n = s.size();
-    //     int left = 0, right = n - 1;
-
-    //     while (left < right) {
-    //         while (left < n && !isalnum(s[left]))
-    //             left++;
-    //         while (right >= 0 && !isalnum(s[right]))
-    //             right--;
-
-    //         if (left < right && tolower(s[left]) != tolower(s[right])) {
-    //             return false;
-    //         }
-    //         left++;
-    //         right--;
-    //     }
-    //     return true;
-    // }
-
-    // simpler code
-
     bool isPalindrome(string s) {
         int n = s.size();
-        int left = 0, right = n - 1;
-
-        while (left < right) {
-            if (!isalnum(s[left])) {
-                left++;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            if (!isalnum(s[l])) {
+                l++;
                 continue;
             }
-            if (!isalnum(s[right])) {
-                right--;
+            if (!isalnum(s[r])) {
+                r--;
                 continue;
             }
-
-            if (tolower(s[left]) != tolower(s[right])) {
-                return false;
-            }
-            left++;
-            right--;
+            int lChar = tolower(s[l]);
+            int rChar = tolower(s[r]);
+            if (lChar != rChar) return false;
+            l++;
+            r--;
         }
         return true;
     }
