86eb7ce6dc73bfa8816a6fce055c05614ed40466
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-26 21:50:17 +0800

    Time: 0 ms (100%), Space: 10.1 MB (62.83%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -2,22 +2,45 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        vector<int> down(n, 1), up(n, 1);
+        int up = 1, down = 1;
         for (int i = 1; i < n; i++) {
+            int prev_up = up, prev_down = down;
             if (nums[i - 1] < nums[i]) {
-                up[i] = down[i - 1] + 1;
-                down[i] = down[i - 1];
+                up = prev_down + 1;
+                down = prev_down;
             } else if (nums[i - 1] > nums[i]) {
-                down[i] = up[i - 1] + 1;
-                up[i] = up[i - 1];
+                down = prev_up + 1;
+                up = prev_up;
             } else {
-                up[i] = up[i - 1];
-                down[i] = down[i - 1];
+                up = prev_up;
+                down = prev_down;
             }
         }
-        return max(up[n - 1], down[n - 1]);
+        return max(up, down);
     }
 };
+
+
+// class Solution {
+// public:
+//     int wiggleMaxLength(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> down(n, 1), up(n, 1);
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i - 1] < nums[i]) {
+//                 up[i] = down[i - 1] + 1;
+//                 down[i] = down[i - 1];
+//             } else if (nums[i - 1] > nums[i]) {
+//                 down[i] = up[i - 1] + 1;
+//                 up[i] = up[i - 1];
+//             } else {
+//                 up[i] = up[i - 1];
+//                 down[i] = down[i - 1];
+//             }
+//         }
+//         return max(up[n - 1], down[n - 1]);
+//     }
+// };
 
 
 // class Solution {
