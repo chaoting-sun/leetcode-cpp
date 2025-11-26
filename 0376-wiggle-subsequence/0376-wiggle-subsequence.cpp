5f685fb025a747e8a4f0d11b96b486b12bb6685f
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-26 21:48:14 +0800

    Time: 0 ms (100%), Space: 10.6 MB (24.54%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -2,46 +2,55 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        vector<int> downwardMaxLength(n, 1), upwardMaxLength(n, 1);
-        int maxLength = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i - 1; j++) {
-                if (nums[j] > nums[i]) {
-                    downwardMaxLength[i] = max(downwardMaxLength[i], upwardMaxLength[j] + 1);
-                }
+        vector<int> down(n, 1), up(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (nums[i - 1] < nums[i]) {
+                up[i] = down[i - 1] + 1;
+                down[i] = down[i - 1];
+            } else if (nums[i - 1] > nums[i]) {
+                down[i] = up[i - 1] + 1;
+                up[i] = up[i - 1];
+            } else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
             }
-            for (int j = 0; j <= i - 1; j++) {
-                if (nums[j] < nums[i]) {
-                    upwardMaxLength[i] = max(upwardMaxLength[i], downwardMaxLength[j] + 1);
-                }
-            }
-            maxLength = max(maxLength, max(downwardMaxLength[i], upwardMaxLength[i]));
         }
-        return maxLength;
+        return max(up[n - 1], down[n - 1]);
     }
 };
 
-// [1] -> 1
-// [1, 2] -> 2
-// [1, 2, 1]
-// downward = [1, 1, 3]
-// upward   = [1, 2, 1]
 
-// 每個元素可能都有離他最近比他大或小的元素
+// class Solution {
+// public:
+//     int wiggleMaxLength(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> downwardMaxLength(n, 1), upwardMaxLength(n, 1);
+//         int maxLength = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j <= i - 1; j++) {
+//                 if (nums[j] > nums[i]) {
+//                     downwardMaxLength[i] = max(downwardMaxLength[i], upwardMaxLength[j] + 1);
+//                 }
+//             }
+//             for (int j = 0; j <= i - 1; j++) {
+//                 if (nums[j] < nums[i]) {
+//                     upwardMaxLength[i] = max(upwardMaxLength[i], downwardMaxLength[j] + 1);
+//                 }
+//             }
+//             maxLength = max(maxLength, max(downwardMaxLength[i], upwardMaxLength[i]));
+//         }
+//         return maxLength;
+//     }
+// };
 
-// [3, 5, 3, 2]
-// down
-// [1  ]
-// up
-// [1. ]
+// [2 4 3 2]
 
-// downward(n)
-// upward(n)
+// downwardMaxLength[]
 
-// downwardMaxLength[0] = 1
-
-// downwardMaxLength[i] = 
-// nums[j] > nums[i] -> upwardMaxLength[j], j = 0 ~ i - 1
-
-// upwardMaxLength[i] = 
-// nums[j] < nums[i] -> downwardMaxLength[j], j = 0 ~ i - 1
+// nums[i -1] < nums[i] -> {
+//     upwardMaxLength[i] 繼承 upwardMaxLength[i - 1]
+    
+// }
+// nums[i - 1] > nums[i] -> {
+//     downwardMaxLength[i] 繼承 downwardMaxLength[i - 1]
+// }
