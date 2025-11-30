300918ce6b1a826500ad303af5626ea123d8f991
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-11-30 13:01:11 +0800

    Time: 0 ms (100%), Space: 8.6 MB (43.03%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    // Approach: recursion, binary exponentiation
-    // if n = even -> x^n = (x^(n/2))^2
-    // if n = odd -> x^n = x * (x^((n-1)/2))
-    // Time: O(logn)
-    // Space: O(logn)
-
-    double binaryExp(double x, long long n) {
-        if (n == 0) return 1;
-        if (n < 0) return 1 / binaryExp(x, -n);
-        
-        // even
-        if (n % 2 == 0) {
-            double halfPow = binaryExp(x, n / 2);
-            return halfPow * halfPow;
-        }
-        // odd
-        else {
-            double halfPow = binaryExp(x, (n - 1) / 2);
-            return x * halfPow * halfPow;
-        }
-    }
-
     double myPow(double x, int n) {
-        return binaryExp(x, (long long)n);
+        double ans = 1;
+        long long m = abs((long long)n);
+        while (m > 0) {
+            if (m % 2 == 1) ans *= x;
+            x *= x;
+            m /= 2;
+        }
+        return n >= 0 ? ans : 1 / ans;
     }
 };
