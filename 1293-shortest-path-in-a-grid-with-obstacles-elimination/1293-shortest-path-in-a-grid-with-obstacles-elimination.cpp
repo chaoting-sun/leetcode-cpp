bc8cb40e08ae6f7a2598e766cb46ada33d18172b
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-02 22:59:10 +0800

    Time: 5 ms (88.4%), Space: 13.3 MB (89.36%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -5,17 +5,17 @@ public:
         vector<int> dx = { 1, -1, 0, 0 };
         vector<int> dy = { 0, 0, 1, -1 };
         
-        vector<vector<vector<int>>> minDistance(m, vector<vector<int>>(n, vector<int>(k + 1, INT_MAX)));
+        vector<vector<int>> maxRemainingObstacles(m, vector<int>(n, -1));
         queue<pair<int,int>> q; // distance, current obstacles
-        q.push({ 0, 0 });
-        minDistance[0][0][0] = 0;
+        q.push({ 0, k });
+        maxRemainingObstacles[0][0] = k;
         int steps = 0;
 
         while (!q.empty()) {
             int size = q.size();
 
             while (size--) {
-                auto [p, obstacles] = q.front();
+                auto [p, remainingObstacles] = q.front();
                 q.pop();
                 int x = p / n;
                 int y = p % n;
@@ -27,13 +27,13 @@ public:
                     int newY = y + dy[i];
                     if (newX < 0 || newX >= m || newY < 0 || newY >= n) continue;
                     
-                    int newObstacles = obstacles + (grid[newX][newY] == 1 ? 1 : 0);
+                    int newRemainingObstacles = remainingObstacles - grid[newX][newY];
                     
-                    if (newObstacles > k) continue; // invalid move
-                    if (minDistance[newX][newY][newObstacles] != INT_MAX) continue; // visited
+                    if (newRemainingObstacles < 0) continue; // invalid move
+                    if (maxRemainingObstacles[newX][newY] >= newRemainingObstacles) continue; // already has a better path
 
-                    minDistance[newX][newY][newObstacles] = minDistance[x][y][obstacles] + 1;
-                    q.push({ newX * n + newY, newObstacles });
+                    maxRemainingObstacles[newX][newY] = newRemainingObstacles;
+                    q.push({ newX * n + newY, newRemainingObstacles });
                 }
             }
             steps++;
@@ -41,3 +41,47 @@ public:
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int shortestPath(vector<vector<int>>& grid, int k) {
+//         int m = grid.size(), n = grid[0].size();
+//         vector<int> dx = { 1, -1, 0, 0 };
+//         vector<int> dy = { 0, 0, 1, -1 };
+        
+//         vector<vector<vector<int>>> minDistance(m, vector<vector<int>>(n, vector<int>(k + 1, INT_MAX)));
+//         queue<pair<int,int>> q; // distance, current obstacles
+//         q.push({ 0, 0 });
+//         minDistance[0][0][0] = 0;
+//         int steps = 0;
+
+//         while (!q.empty()) {
+//             int size = q.size();
+
+//             while (size--) {
+//                 auto [p, obstacles] = q.front();
+//                 q.pop();
+//                 int x = p / n;
+//                 int y = p % n;
+
+//                 if (x == m - 1 && y == n - 1) return steps;
+
+//                 for (int i = 0; i < 4; i++) {
+//                     int newX = x + dx[i];
+//                     int newY = y + dy[i];
+//                     if (newX < 0 || newX >= m || newY < 0 || newY >= n) continue;
+                    
+//                     int newObstacles = obstacles + (grid[newX][newY] == 1 ? 1 : 0);
+                    
+//                     if (newObstacles > k) continue; // invalid move
+//                     if (minDistance[newX][newY][newObstacles] != INT_MAX) continue; // visited
+
+//                     minDistance[newX][newY][newObstacles] = minDistance[x][y][obstacles] + 1;
+//                     q.push({ newX * n + newY, newObstacles });
+//                 }
+//             }
+//             steps++;
+//         }
+//         return -1;
+//     }
+// };
