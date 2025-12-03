780fac310b0ecf3ca1901ac1abdf1d690fa42283
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-03 23:20:24 +0800

    Time: 864 ms (34.32%), Space: 234.9 MB (31.36%) - LeetHub

--- 3690-split-and-merge-array-transformation/3690-split-and-merge-array-transformation.cpp
+++ 3690-split-and-merge-array-transformation/3690-split-and-merge-array-transformation.cpp
@@ -1,58 +1,58 @@
 class Solution {
 public:
-    // Intuition: the size of nums1 and nums2 are bounded between 2 and 6, which is quite small.
-    // to return the minimum number of split and merge from num1 to num2, owing to the small size,
-    // we can try a brute-force BFS.
-
-    string getKey(vector<int>& nums) {
-        string key = "";
-        for (int i = 0; i < (int)nums.size(); i++) {
-            if (i > 0) key += ",";
-            key += to_string(nums[i]);
-        }
-        return key;
-    }
-
     int minSplitMerge(vector<int>& nums1, vector<int>& nums2) {
+        // change to 0~size-1
         int n = nums1.size();
-        if (n == 0 || nums1 == nums2) return 0;
-        
-        queue<pair<vector<int>,int>> q;
+        unordered_map<int,int> mp;
+        for (int i = 0; i < n; i++) mp[nums1[i]] = i;
+        for (int i = 0; i < n; i++) {
+            nums1[i] = mp[nums1[i]];
+            nums2[i] = mp[nums2[i]];
+        }
+
+        // initialize
         unordered_set<string> visited;
+        visited.reserve(720);
+        string s = "";
+        for (int d: nums1) s += d + '0';
+        visited.insert(s);
 
-        q.push({ nums1, 0 });
-        visited.insert(getKey(nums1));
+        queue<vector<int>> q;
+        q.push(nums1);
+        int steps = 0;
 
+        // bfs
         while (!q.empty()) {
-            auto [curr, dist] = q.front();
-            q.pop();
+            int size = q.size();
+            while (size--) {
+                auto curr = q.front(); // [3,1,2]
+                q.pop();
+                if (curr == nums2) return steps;
 
-            for (int left = 0; left < n; left++) {
-                for (int right = left; right < n; right++) {
-                    vector<int> block(curr.begin() + left, curr.begin() + right + 1);
-                    vector<int> rest;
-                    rest.insert(rest.end(), curr.begin(), curr.begin() + left);
-                    rest.insert(rest.end(), curr.begin() + right + 1, curr.end());
+                for (int i = 0; i < n; i++) {
+                    for (int j = i; j < n; j++) {
+                        vector<int> removed(j - i + 1);
+                        vector<int> left;
+                        for (int k = i; k <= j; k++) removed[k - i] = curr[k];
+                        for (int k = 0; k < i; k++) left.push_back(curr[k]);
+                        for (int k = j + 1; k < n; k++) left.push_back(curr[k]);
 
-                    int m = rest.size();
-                    for (int gap = 0; gap <= m; gap++) {
-                        vector<int> nxt;
-                        nxt.insert(nxt.end(), rest.begin(), rest.end());
-                        nxt.insert(nxt.begin() + gap, block.begin(), block.end());
-
-                        if (nxt == nums2) return dist + 1;
-                        
-                        string key = getKey(nxt);
-                        if (visited.count(key)) continue;
-                        visited.insert(key);
-
-                        q.push({ nxt, dist + 1 });
+                        for (int k = 0; k <= left.size(); k++) {
+                            vector<int> next = left;
+                            next.insert(next.begin() + k, removed.begin(), removed.end());
+                            string s = "";
+                            for (int d: next) s += d + '0';
+                            if (visited.count(s)) continue;
+                            visited.insert(s);
+                            q.push(next);
+                        }
                     }
                 }
             }
-        }
 
-        return 0;
+            steps++;
+        }
+        
+        return -1;
     }
 };
-
