898ff542230ee4117f8bee72c8ef6057f6835d34
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-04 21:11:10 +0800

    Time: 5 ms (29.4%), Space: 20.1 MB (95.24%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
@@ -2,30 +2,56 @@ class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
         int n = stations.size();
-        priority_queue<pair<int,int>> pq; // max heap // (position + fuel, index)
+        priority_queue<int> pq; // max heap // (fuel)
         
-        int currentPosition = 0;
-        int currentFuel = startFuel;
+        int maxReach = startFuel;
         int stationIdx = 0;
         int stops = 0;
 
         sort(stations.begin(), stations.end());
-
-        while (currentPosition + currentFuel < target) {
-            while (stationIdx < n && currentPosition + currentFuel >= stations[stationIdx][0]) {
-                pq.push({ stations[stationIdx][1], stationIdx });
+        while (maxReach < target) {
+            while (stationIdx < n && maxReach >= stations[stationIdx][0]) {
+                pq.push(stations[stationIdx][1]);
                 stationIdx++;
             }
-
             if (pq.empty()) break;
-
-            auto [fuelToAdd, idx] = pq.top();
+            maxReach += pq.top();
             pq.pop();
-            currentFuel = currentFuel - (stations[idx][0] - currentPosition) + fuelToAdd;
-            currentPosition = stations[idx][0];
             stops++;
         }
 
-        return currentPosition + currentFuel < target ? -1 : stops;
+        return maxReach < target ? -1 : stops;
     }
 };
+
+// class Solution {
+// public:
+//     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+//         int n = stations.size();
+//         priority_queue<pair<int,int>> pq; // max heap // (position + fuel, index)
+        
+//         int currentPosition = 0;
+//         int currentFuel = startFuel;
+//         int stationIdx = 0;
+//         int stops = 0;
+
+//         sort(stations.begin(), stations.end());
+
+//         while (currentPosition + currentFuel < target) {
+//             while (stationIdx < n && currentPosition + currentFuel >= stations[stationIdx][0]) {
+//                 pq.push({ stations[stationIdx][1], stationIdx });
+//                 stationIdx++;
+//             }
+
+//             if (pq.empty()) break;
+
+//             auto [fuelToAdd, idx] = pq.top();
+//             pq.pop();
+//             currentFuel = currentFuel - (stations[idx][0] - currentPosition) + fuelToAdd;
+//             currentPosition = stations[idx][0];
+//             stops++;
+//         }
+
+//         return currentPosition + currentFuel < target ? -1 : stops;
+//     }
+// };
