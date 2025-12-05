107928a7808c9f69e687d1a1b8967653dde20ae3
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-05 21:03:30 +0800

    Time: 40 ms (21.49%), Space: 37.2 MB (18.32%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -2,67 +2,89 @@ class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size();
-        int maxSum = 0;
-        for (int val: nums) maxSum += val;
-        if (maxSum % 3 == 0) return maxSum;
-
-        sort(nums.begin(), nums.end());
+        vector<int> dp { 0, 0, 0 };
         
-        int ans = 0;
-        int check = 0;
-        int leftSum = maxSum;
+        for (int i = 0; i < n; i++) {
+            vector<int> next_dp = dp;
 
-        // 1 -> 刪1個餘1的數 ^ 或2個餘2的數
-
-        if (maxSum % 3 == 1) {
-            for (int i = 0; i < n; i++) {
-                if (nums[i] % 3 == 1) {
-                    leftSum -= nums[i];
-                    ans = leftSum;
-                    break;
-                }
+            for (int j = 0; j < 3; j++) {
+                int currSum = dp[j] + nums[i];
+                int remainder = currSum % 3;
+                next_dp[remainder] = max(currSum, next_dp[remainder]);
             }
 
-            leftSum = maxSum;
-            for (int i = 0; i < n; i++) {
-                if (nums[i] % 3 == 2) {
-                    leftSum -= nums[i];
-                    check++;
-                    if (check == 2) {
-                        ans = max(ans, leftSum);
-                        break;
-                    }
-                }
-            }
-        } else {
-            // 刪1個餘2的數 或 刪2個餘1的數
-
-            leftSum = maxSum;
-            for (int i = 0; i < n; i++) {
-                if (nums[i] % 3 == 2) {
-                    leftSum -= nums[i];
-                    ans = leftSum;
-                    break;
-                }
-            }
-
-            leftSum = maxSum; 
-            check = 0;
-            for (int i = 0; i < n; i++) {
-                if (nums[i] % 3 == 1) {
-                    leftSum -= nums[i];
-                    check++;
-                    if (check == 2) {
-                        ans = max(ans, leftSum);
-                        break;
-                    }
-                }
-            }
+            dp = next_dp;
         }
-
-        return ans;
+        return dp[0];
     }
 };
 
-// [1,3,5,6,8] -> 23 % 3 == 2
+
+// class Solution {
+// public:
+//     int maxSumDivThree(vector<int>& nums) {
+//         int n = nums.size();
+//         int maxSum = 0;
+//         for (int val: nums) maxSum += val;
+//         if (maxSum % 3 == 0) return maxSum;
+
+//         sort(nums.begin(), nums.end());
+        
+//         int ans = 0;
+//         int check = 0;
+//         int leftSum = maxSum;
+
+//         // 1 -> 刪1個餘1的數 ^ 或2個餘2的數
+
+//         if (maxSum % 3 == 1) {
+//             for (int i = 0; i < n; i++) {
+//                 if (nums[i] % 3 == 1) {
+//                     leftSum -= nums[i];
+//                     ans = leftSum;
+//                     break;
+//                 }
+//             }
+
+//             leftSum = maxSum;
+//             for (int i = 0; i < n; i++) {
+//                 if (nums[i] % 3 == 2) {
+//                     leftSum -= nums[i];
+//                     check++;
+//                     if (check == 2) {
+//                         ans = max(ans, leftSum);
+//                         break;
+//                     }
+//                 }
+//             }
+//         } else {
+//             // 刪1個餘2的數 或 刪2個餘1的數
+
+//             leftSum = maxSum;
+//             for (int i = 0; i < n; i++) {
+//                 if (nums[i] % 3 == 2) {
+//                     leftSum -= nums[i];
+//                     ans = leftSum;
+//                     break;
+//                 }
+//             }
+
+//             leftSum = maxSum; 
+//             check = 0;
+//             for (int i = 0; i < n; i++) {
+//                 if (nums[i] % 3 == 1) {
+//                     leftSum -= nums[i];
+//                     check++;
+//                     if (check == 2) {
+//                         ans = max(ans, leftSum);
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+// // [1,3,5,6,8] -> 23 % 3 == 2
 
