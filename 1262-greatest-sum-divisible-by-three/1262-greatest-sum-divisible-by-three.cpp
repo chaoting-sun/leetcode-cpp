f4221652c99e1d6af86ec92c2a1ae16cfba20689
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-05 21:20:26 +0800

    Time: 3 ms (79.92%), Space: 28.8 MB (81.44%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,24 +1,64 @@
+// class Solution {
+// public:
+//     int maxSumDivThree(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp { 0, 0, 0 };
+        
+//         for (int i = 0; i < n; i++) {
+//             vector<int> next_dp = dp;
+
+//             for (int j = 0; j < 3; j++) {
+//                 int currSum = dp[j] + nums[i];
+//                 int remainder = currSum % 3;
+//                 next_dp[remainder] = max(currSum, next_dp[remainder]);
+//             }
+
+//             dp = next_dp;
+//         }
+//         return dp[0];
+//     }
+// };
+
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp { 0, 0, 0 };
+        int max_val = 10001;
+        int min1_rem1 = max_val, min1_rem2 = max_val;
+        int min2_rem1 = max_val, min2_rem2 = max_val;
         
         for (int i = 0; i < n; i++) {
-            vector<int> next_dp = dp;
-
-            for (int j = 0; j < 3; j++) {
-                int currSum = dp[j] + nums[i];
-                int remainder = currSum % 3;
-                next_dp[remainder] = max(currSum, next_dp[remainder]);
+            if (nums[i] % 3 == 1) {
+                if (nums[i] < min1_rem1) {
+                    int tmp = min1_rem1;
+                    min1_rem1 = nums[i];
+                    min2_rem1 = tmp;
+                } else if (nums[i] < min2_rem1) {
+                    min2_rem1 = nums[i];
+                }
+            } else if (nums[i] % 3 == 2) {
+                if (nums[i] < min1_rem2) {
+                    int tmp = min1_rem2;
+                    min1_rem2 = nums[i];
+                    min2_rem2 = tmp;
+                } else if (nums[i] < min2_rem2) {
+                    min2_rem2 = nums[i];
+                }
             }
-
-            dp = next_dp;
         }
-        return dp[0];
+
+        int totalSum = 0;
+        for (int v: nums) totalSum += v;
+        
+        if (totalSum % 3 == 0) {
+            return totalSum;
+        } else if (totalSum % 3 == 1) {
+            return totalSum - min(min1_rem1, min1_rem2 + min2_rem2);
+        } else {
+            return totalSum - min(min1_rem2, min1_rem1 + min2_rem1);
+        }
     }
 };
-
 
 // class Solution {
 // public:
