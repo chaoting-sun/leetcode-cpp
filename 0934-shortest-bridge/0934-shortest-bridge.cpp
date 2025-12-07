2e5f9f836dc75d66fd1c8d7eb16822848aa0e759
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-07 09:46:13 +0800

    Time: 205 ms (6.56%), Space: 27.8 MB (24.02%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    void bfsIsland(vector<vector<int>>& grid, set<vector<int>>& visited, vector<int> start) {
+    void bfsIsland(vector<vector<int>>& grid, unordered_set<int>& visited, int start) {
         vector<int> dx = { 1, -1, 0, 0 };
         vector<int> dy = { 0, 0, 1, -1 };
 
         int m = grid.size();
         int n = grid[0].size();
 
-        queue<vector<int>> cells;
+        queue<int> cells;
 
         cells.push(start);
         visited.insert(start);
 
         while (!cells.empty()) {
-            vector<int> position = cells.front();
+            int position = cells.front();
             cells.pop();
-            int x = position[0], y = position[1];
+            int x = position / n, y = position % n;
             for (int k = 0; k < 4; k++) {
                 int newX = x + dx[k];
                 int newY = y + dy[k];
                 if (newX < 0 || newX >= m || newY < 0 || newY >= n) continue;
                 if (grid[newX][newY] == 0) continue;
-                if (visited.count({ newX, newY })) continue;
 
-                vector<int> newPosition = { newX, newY };
+                int newPosition = newX * n + newY;
+                if (visited.count(newPosition)) continue;
+
                 cells.push(newPosition);
                 visited.insert(newPosition);
             }
@@ -48,13 +49,13 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        set<vector<int>> firstVisited;
+        unordered_set<int> firstVisited;
         bool found = false;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    bfsIsland(grid, firstVisited, { i, j });
+                    bfsIsland(grid, firstVisited, i * n + j);
                     found = true;
                     break;
                 }
@@ -62,12 +63,12 @@ public:
             if (found) break;
         }
 
-        set<vector<int>> secondVisited;
+        unordered_set<int> secondVisited;
         found = false;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                vector<int> start { i, j };
+                int start = i * n + j;
                 if (firstVisited.count(start)) continue;
                 if (grid[i][j] == 1) {
                     bfsIsland(grid, secondVisited, start);
@@ -79,10 +80,10 @@ public:
         }
 
         int minDistance = INT_MAX;
-        for (const vector<int>& firstCell: firstVisited) {
-            for (const vector<int>& secondCell: secondVisited) {
-                int xDirDistance = abs(firstCell[0] - secondCell[0]);
-                int yDirDistance = abs(firstCell[1] - secondCell[1]);
+        for (const int& firstCell: firstVisited) {
+            for (const int& secondCell: secondVisited) {
+                int xDirDistance = abs(firstCell / n - secondCell / n);
+                int yDirDistance = abs(firstCell % n - secondCell % n);
                 minDistance = min(minDistance, xDirDistance + yDirDistance - 1);
             }
         }
