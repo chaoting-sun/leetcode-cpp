ef6e80562d818451ad16f355d616b1b3d90310e6
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-13 20:51:34 +0800

    Time: 4 ms (15.08%), Space: 20.9 MB (5.13%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,107 +1,49 @@
+// board (m, n) range? 1 <= m,n <= 200
+
+// dfs problem
+// iterate board, do dfs on all unvisited cells.
+// for each dfs, storing all visited positions in a vector. if when no edges have not visited, update all visited positions 
+
 class Solution {
+private:
+    bool captureSurroundedRegion(vector<vector<char>>& board, vector<vector<bool>>& visited, int x, int y, vector<pair<int,int>>& current_visited) {
+        int m = board.size();
+        int n = board[0].size(); 
+        
+        if (x == 0 || x == m - 1 || y == 0 || y == n - 1) return false;
+        if (visited[x][y]) return true;
+
+        visited[x][y] = true;
+        current_visited.push_back({ x, y });
+
+        int is_surrounded = true;
+        if (x > 0 && board[x - 1][y] == 'O') is_surrounded &= captureSurroundedRegion(board, visited, x - 1, y, current_visited);
+        if (x < m - 1 && board[x + 1][y] == 'O') is_surrounded &= captureSurroundedRegion(board, visited, x + 1, y, current_visited);
+        if (y > 0 && board[x][y - 1] == 'O') is_surrounded &= captureSurroundedRegion(board, visited, x, y - 1, current_visited);
+        if (y < n - 1 && board[x][y + 1] == 'O') is_surrounded &= captureSurroundedRegion(board, visited, x, y + 1, current_visited);
+        return is_surrounded;
+    }
+
 public:
-    // Intuition: One method is starting from the cell (i,j) with 0<i<m-1, 0<j<n-1, board[i][j]='O' and dfs
-    // but it is hard to implement as we have to track the paths before we check this is the surrounded region.
-    // we can do it in an opposite way. we start from the border with board[i][j] = 'O' and dfs those connected
-    // 'O' and mark them '#'. any cell that is marked as '#' is not surrounded by 'X'.
-    
-    // Approach: DFS
-    // Time: O(m*n)
-    // Space: O(m*n). the depth of the function call stack, in worst case, is the number of cells
-
-    // void dfs(vector<vector<char>>& board, int i, int j) {
-    //     int m = board.size();
-    //     int n = board[0].size();
-
-    //     // termination condition
-    //     if (i < 0 || i >= m || j < 0 || j >= n) {
-    //         return;
-    //     }
-    //     if (board[i][j] == 'X' || board[i][j] == '#') return;
-
-    //     // visited
-    //     board[i][j] = '#';
-
-    //     dfs(board, i - 1, j);
-    //     dfs(board, i + 1, j);
-    //     dfs(board, i, j - 1);
-    //     dfs(board, i, j + 1);
-    // }
-
-    // void solve(vector<vector<char>>& board) {
-    //     int m = board.size();
-    //     int n = board[0].size();
-
-    //     for (int i = 0; i < m; i++) {
-    //         for (int j = 0; j < n; j++) {
-    //             if ((i == 0 || i == m - 1 || j == 0 || j == n - 1) && board[i][j]) {
-    //                 dfs(board, i, j);
-    //             }
-    //         }
-    //     }
-
-    //     for (int i = 0; i < m; i++) {
-    //         for (int j = 0; j < n; j++) {
-    //             if (board[i][j] == '#') {
-    //                 board[i][j] = 'O';
-    //             } else if (board[i][j] == 'O') {
-    //                 board[i][j] = 'X';
-    //             }
-    //         }
-    //     }
-    // }
-
-    // Approach: BFS
-    // Time: O(m*n)
-    // Space: O(m+n)
-
     void solve(vector<vector<char>>& board) {
         int m = board.size();
         int n = board[0].size();
-
-        queue<pair<int,int>> cells;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i == 0 || i == m - 1 || j == 0 || j == n - 1) && board[i][j] == 'O') {
-                    cells.push({ i, j });
-                    board[i][j] = '#';
-                }
-            }
-        }
-
-        vector<int> dx { 0, 0, 1, -1 };
-        vector<int> dy { 1, -1, 0, 0 };
-
-        while (!cells.empty()) {
-            int size = cells.size();
-
-            for (int i = 0; i < size; i++) {
-                int x = cells.front().first;
-                int y = cells.front().second;
-                cells.pop();
-
-                for (int j = 0; j < 4; j++) {
-                    int newx = x + dx[j];
-                    int newy = y + dy[j];
-                    if (newx >= 0 && newx < m && newy >= 0 && newy < n && board[newx][newy] == 'O') {
-                        cells.push({ newx, newy });
-                        board[newx][newy] = '#';
+        
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for (int x = 1; x < m - 1; x++) {
+            for (int y = 1; y < n - 1; y++) {
+                if (visited[x][y] || board[x][y] != 'O') continue;
+                vector<pair<int,int>> current_visited;
+                bool is_surrounded_region = captureSurroundedRegion(board, visited, x, y, current_visited);
+                if (is_surrounded_region) {
+                    for (const auto& [visited_x, visited_y]: current_visited) {
+                        board[visited_x][visited_y] = 'X';
                     }
-                    
-                }
-                
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == '#') {
-                    board[i][j] = 'O';
-                } else if (board[i][j] == 'O') {
-                    board[i][j] = 'X';
                 }
             }
         }
     }
 };
+
+// test case: board = [[O]]
+// board will not be changed
