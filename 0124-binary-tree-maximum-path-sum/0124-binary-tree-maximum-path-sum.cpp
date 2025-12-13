f2f33baf49f99b6f43e47c9af1ca0df64c8e0169
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-13 18:07:25 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,3 +1,17 @@
+// N range: [1, 3*10^4]
+// Value range: -1000 <= val <= 1000
+
+// for each node, optimal path includes
+// 1. node
+// 2. node + max(0, max sum that ends with node->left) + max(0, max sum that ends with node->right)
+
+// bottom up approach -> post-order traversal (dfs)
+// function
+//  - max_left_sum = getMaxSum(node->left) // ends with node->left
+//  - max_right_sum = getMaxSum(node->right) // ends with node->right
+//  - max_sum = max(max_sum, node->val + max(max_left_sum, 0), max(max_right_sum, 0))
+//  > return node->val + max(max(max_left_sum, 0) max(max_right_sum, 0))
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,65 +23,18 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+
 class Solution {
-public:
-    // Approach: Recursion. DFS and constantly compute the max path. we only compute the path
-    // under the current node, which includes:
-    // - c: itself
-    // - lc: left + itself
-    // - rc: right + itself
-    // - lrc: left + itself + right
-    // the recursion function will return the maximum sum with path ends with the current node
-    // to avoid split
-    
-    // Time: O(n)
-    // Space: O(n)
-
-    // int max_sum;
-
-    // int gainFromSubtree(TreeNode* root) {
-    //     if (!root) return 0;
-
-    //     int _l_sum = recursion(root->left);
-    //     int _r_sum = recursion(root->right);
-
-    //     int l_sum = _l_sum + root->val;
-    //     int c_sum = root->val;
-    //     int r_sum = _r_sum + root->val;
-    //     int lcr_sum = _l_sum + root->val + _r_sum;
-
-    //     int root_max_sum = max(lcr_sum, max(c_sum, max(l_sum, r_sum)));
-
-    //     max_sum = max(max_sum, root_max_sum);
-    //     return max(c_sum, max(l_sum, r_sum));
-    // }
-
-    // int maxPathSum(TreeNode* root) {
-    //     max_sum = INT_MIN;
-    //     gainFromSubtree(root);
-    //     return max_sum;
-    // }
-
-    // simplify
-
-    int max_sum;
-
-    int gainFromSubtree(TreeNode* root) {
-        if (!root) return 0;
-
-        int l_sum = gainFromSubtree(root->left);
-        int r_sum = gainFromSubtree(root->right);
-
-        l_sum = max(l_sum, 0);
-        r_sum = max(r_sum, 0);
-        max_sum = max(max_sum, l_sum + root->val + r_sum);
-
-        return max(l_sum, r_sum) + root->val;
+private:
+    int getMaxPathSum(TreeNode* root, int& maxSum) {
+        int max_left_sum = getMaxPathSum(root->left, maxSum);
     }
 
+public:
     int maxPathSum(TreeNode* root) {
-        max_sum = INT_MIN;
-        gainFromSubtree(root);
-        return max_sum;
+        int maxSum = INT_MIN;
+        getMaxPathSum(root, maxSum);
     }
 };
