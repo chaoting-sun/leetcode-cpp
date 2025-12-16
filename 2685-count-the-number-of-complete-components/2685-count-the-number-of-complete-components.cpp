8ab9f68ff7fa353ba13012e45d4e69a248a104ed
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-16 20:59:45 +0800

    Time: 0 ms (100%), Space: 116.9 MB (98.8%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -27,49 +27,112 @@
 
 // Implementation
 
+
 class Solution {
-private:
-    // return: total edges & total nodes
-    pair<int,int> exploreConnectedComponent(const vector<vector<int>>& adj, vector<bool>& visited, int current_node) {
-        visited[current_node] = true;
-        int total_nodes = 1;
-        int total_edges = adj[current_node].size();
-        for (int neighbor: adj[current_node]) {
-            if (!visited[neighbor]) {
-                auto [neighbor_total_nodes, neighbor_total_edges] = exploreConnectedComponent(adj, visited, neighbor);
-                total_nodes += neighbor_total_nodes;
-                total_edges += neighbor_total_edges;
+    struct DSU {
+        vector<int> size;
+        vector<int> parent;
+        vector<int> edge_count;
+
+        DSU(int n) {
+            size.resize(n);
+            parent.resize(n);
+            edge_count.resize(n);
+
+            for (int i = 0; i < n; i++) size[i] = 1;
+            for (int i = 0; i < n; i++) parent[i] = i;
+        }
+
+        int find(int u) {
+            if (parent[u] == u) return u;
+            return parent[u] = find(parent[u]);
+        }
+
+        void unite(int u, int v) {
+            u = find(u);
+            v = find(v);
+            if (u != v) {
+                if (size[u] > size[v]) {
+                    swap(size[u], size[v]);
+                }
+                // size of v is larger than u
+                parent[u] = v;
+                size[v] += size[u];
+                edge_count[v] += edge_count[u] + 1;
+            } else {
+                edge_count[v]++;
             }
         }
-        return { total_nodes, total_edges };
-    }
+    };
 
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
+        DSU dsu = DSU(n);
+        
         for (const auto& edge: edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+            dsu.unite(edge[0], edge[1]);
         }
 
-        // DFS for the unvisited node
-        int complete_connected_components = 0;
-        vector<bool> visited(n, false);
-        
+        int complete_component_count = 0;
+
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                auto [total_nodes, total_edges] = exploreConnectedComponent(adj, visited, i);
-                // we will count the edges twice for the undirected graph
-                total_edges /= 2;
-                if (total_edges == total_nodes * (total_nodes - 1) / 2) {
-                    complete_connected_components++;
+            if (dsu.parent[i] == i) {
+                int nodes = dsu.size[i];
+                int edges = dsu.edge_count[i];
+                bool is_complete = edges == nodes * (nodes - 1) / 2;
+                if (is_complete) {
+                    complete_component_count++;
                 }
             }
         }
 
-        return complete_connected_components;
+        return complete_component_count;
     }
 };
+
+// class Solution {
+// private:
+//     // return: total edges & total nodes
+//     pair<int,int> exploreConnectedComponent(const vector<vector<int>>& adj, vector<bool>& visited, int current_node) {
+//         visited[current_node] = true;
+//         int total_nodes = 1;
+//         int total_edges = adj[current_node].size();
+//         for (int neighbor: adj[current_node]) {
+//             if (!visited[neighbor]) {
+//                 auto [neighbor_total_nodes, neighbor_total_edges] = exploreConnectedComponent(adj, visited, neighbor);
+//                 total_nodes += neighbor_total_nodes;
+//                 total_edges += neighbor_total_edges;
+//             }
+//         }
+//         return { total_nodes, total_edges };
+//     }
+
+// public:
+//     int countCompleteComponents(int n, vector<vector<int>>& edges) {
+//         vector<vector<int>> adj(n);
+//         for (const auto& edge: edges) {
+//             adj[edge[0]].push_back(edge[1]);
+//             adj[edge[1]].push_back(edge[0]);
+//         }
+
+//         // DFS for the unvisited node
+//         int complete_connected_components = 0;
+//         vector<bool> visited(n, false);
+        
+//         for (int i = 0; i < n; i++) {
+//             if (!visited[i]) {
+//                 auto [total_nodes, total_edges] = exploreConnectedComponent(adj, visited, i);
+//                 // we will count the edges twice for the undirected graph
+//                 total_edges /= 2;
+//                 if (total_edges == total_nodes * (total_nodes - 1) / 2) {
+//                     complete_connected_components++;
+//                 }
+//             }
+//         }
+
+//         return complete_connected_components;
+//     }
+// };
 
 
 // Dry Run
@@ -122,6 +185,17 @@ public:
 // < return 2
 
 
+// Evaluation
+// V = n, E = # edges
+// Time: O(V + E)
+// - it takes O(V + E) to build adj
+// - for DFS, we will visit every node once and every edge twice. So it will take O(V + 2E)
+// Space: O(V + E)
+// - it takes O(V + E) for adj
+// - for DFS, the recursion stack will be O(V) in worst case
+
+
 // Submit Errors
 
 // Wrong Answer
+// mistake for counting the edges
