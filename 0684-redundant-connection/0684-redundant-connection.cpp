358e2665e736501b054cc95e6a7d1704ecc9d381
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-16 21:32:15 +0800

    Time: 0 ms (100%), Space: 13 MB (62.49%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,90 +1,46 @@
 class Solution {
+private:
+	struct DSU {
+	vector<int> size;
+	vector<int> parent;
+	
+	DSU(int n) {
+	size.assign(n, 1);
+	parent.resize(n);
+	iota(parent.begin(), parent.end(), 0);
+}
+
+int find(int u) {
+	if (parent[u] == u) return u;
+	return parent[u] = find(parent[u]);
+}
+
+void unite(int u, int v) {
+	int root_u = find(u);
+	int root_v = find(v);
+	if (root_u != root_v) {
+		if (size[root_u] > size[root_v]) {
+	swap(root_u, root_v);
+}
+parent[root_u] = root_v;
+size[root_v] += size[root_u];
+}
+}
+};
 public:
-    // Approach: build the graph and every time check if cycle exists every time
-    // Time: O(V^2)
-    // Intialize and construct adjacency list: O(E + V) = O(V)
-    // DFS: O(E*(E + V)) = O(V^2)
-    // Space: O(V)
-    // visited: O(V)
-    // adjacency list: O(E + V) = O(V)
+	vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+	int n = edges.size();
 
-    // bool hasCycle(vector<vector<int>>& graph, vector<int>& visited, int prev, int curr) {
-    //     if (visited[curr] == 1) return true;
-    //     if (visited[curr] == 2) return false;
-
-    //     visited[curr] = 1;
-    //     for (int neighbor: graph[curr]) {
-    //         if (prev == neighbor) continue;
-    //         if (hasCycle(graph, visited, curr, neighbor)) {
-    //             return true;
-    //         }
-    //     }
-    //     visited[curr] = 2;
-    //     return false;
-    // }
-
-    // vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-    //     int n = edges.size();
-    //     vector<vector<int>> graph(n + 1);
-
-    //     for (auto edge: edges) {
-    //         graph[edge[0]].push_back(edge[1]);
-    //         graph[edge[1]].push_back(edge[0]);
-    //         vector<int> visited(n + 1, 0);
-    //         if (hasCycle(graph, visited, 0, edge[0])) {
-    //             return edge;
-    //         }
-    //     }
-    //     return {};
-    // }
-
-    // Disjoint Set Union (DSU)
-    // Time: O(n * alpha(n)). n = # nodes
-    // Space: O(n)
-
-    vector<int> parent;
-    vector<int> size;
-
-    void make_set(int v) {
-        parent[v] = v;
-        size[v] = 1;
-    }
-
-    int find_set(int v) {
-        if (v == parent[v])
-            return v;
-        return parent[v] = find_set(parent[v]);
-    }
-
-    bool union_sets(int u, int v) {
-        u = find_set(u);
-        v = find_set(v);
-
-        if (u == v) return true;
-
-        if (size[u] < size[v]) {
-            swap(u, v);
-        }
-        parent[v] = u;
-        size[u] += size[v];
-        return false;
-    }
-
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size(); // # vertices = # edges
-        parent.resize(n + 1, -1);
-        size.resize(n + 1, -1);
-
-        for (int i = 1; i <= n; i++) {
-            make_set(i);
-        }
-
-        for (auto edge: edges) {
-            int u = edge[0];
-            int v = edge[1];
-            bool yes = union_sets(u, v);
-            if (yes) return edge;
-        }
-        return {};
-    }
+	DSU dsu(n + 1);
+	vector<int> last_redundant_edge { 0, 0 };
+	for (const auto& edge: edges) {
+	int u = edge[0], v = edge[1];
+	if (dsu.find(u) != dsu.find(v)) {
+	dsu.unite(u, v);
+} else {
+	last_redundant_edge = edge;
+}
+}
+	return last_redundant_edge;
+}
 };
