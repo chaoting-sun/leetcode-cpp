b3b6f7797ed4f9b260113031e8fe4b35c4782b4f
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-27 12:14:22 +0800

    Time: 141 ms (75.11%), Space: 175.9 MB (59.49%) - LeetHub

--- 1600-throne-inheritance/1600-throne-inheritance.cpp
+++ 1600-throne-inheritance/1600-throne-inheritance.cpp
@@ -1,5 +1,9 @@
 // ## Understand
 
+// the max length of name? -> 15
+// the number calls for each method? -> birth+dead: 10^5, getInheritanceOrder: 10
+
+
 // ## Match
 
 // I will map the family inheritance problem to a preorder traversal on an N-ary tree, in which each node
@@ -12,6 +16,12 @@
 // the person is alive.
 // Also, as the birth and death call can be up to 10^5 times, I will use a hashmap to record the address of
 // node from the person's name, so that each birth and death call can be in constant time.
+
+// In worst case, the king has about 10^5 children. The memory of all nodes will take:
+// name: 15 * 10^5 bytes
+// children: 8 * 10^5 bytes
+// is_alive: 1 * 10^5 bytes
+// totally: 24 * 10^5 bytes ~ 10^6 bytes = 1MB. it is acceptable.
 
 
 class ThroneInheritance {
@@ -47,18 +57,25 @@ public:
     }
     
     void birth(string parentName, string childName) {
+        // check the existence of the parent
+        auto it = family_tree.find(parentName);
+        if (it == family_tree.end()) {
+            return;
+        }
+
         // I create child, storing it in the hash map and the children of its parent
         unique_ptr<Person> child = make_unique<Person>(move(childName));
         Person* child_ptr = child.get();
         family_tree[child_ptr->name] = move(child);
         
-        family_tree[parentName]->children.push_back(child_ptr);
+        it->second->children.push_back(child_ptr);
     }
     
     void death(string name) {
         // If the person exists, mark it as dead
-        if (family_tree.count(name)) {
-            family_tree[move(name)]->is_alive = false;
+        auto it = family_tree.find(name);
+        if (it != family_tree.end()) {
+            it->second->is_alive = false;
         }
     }
     
@@ -77,3 +94,58 @@ public:
  * obj->death(name);
  * vector<string> param_3 = obj->getInheritanceOrder();
  */
+
+// ## Review
+// happy case:
+// ThroneInheritance('a') -> birth('a', 'b') -> death('b') -> getInheritanceOrder()
+// trace:
+// > ThroneInheritance('a')
+//  create node, person[a] = node(a, is alive), save in king
+// > birth('a', 'b')
+//  create node, person['b'] = node(b, is alive), node(a, is alive)->children = [node(b, is alive)]
+// > death('b')
+//  b exists -> person['b'] = node(b, dead)
+// > getInheritanceOrder()
+//  dfs(node(a, is alive)) -> inheritance_order = [a]
+//  children: node(b, dead)
+//  dfs(node(b, dead)) -> dead
+//  return [a]
+
+// base case:
+// ThroneInheritance('a') -> getInheritanceOrder()
+// trace:
+// > ThroneInheritance('a')
+//  create node, person[a] = node(a, is alive), save in king
+// > getInheritanceOrder()
+//  dfs(node(a, is alive)) -> inheritance_order = [a]
+//  children: none
+// < return [a]
+
+// trap path:
+// ThroneInheritance('a') -> birth('a', 'b') -> birth('a', 'c') -> death('a') -> getInheritanceOrder()
+// trace:
+// > ThroneInheritance('a')
+//  create node, person[a] = node(a, is alive), save in king
+// > birth('a', 'b')
+//  create node, person['b'] = node(b, is alive), node(a, is alive)->children = [node(b, is alive)]
+// > birth('a', 'c')
+//  create node, person['c'] = node(c, is alive), node(a, is alive)->children = [node(b, is alive), node(c, is alive)]
+// > death('a')
+//  a exists -> node(a, dead)
+// > getInheritanceOrder()
+//  dfs(node(a, dead)) -> dead
+//  children: node(b, is alive), node(c, is alive)
+//      dfs(node(b, is alive)) -> inheritance_order = [b]
+//          children: none
+//      dfs(node(c, is alive)) -> inheritance_order = [b, c]
+///         children: none
+//  < return [b, c]
+
+
+// Evaluation:
+
+// Time:
+//  birth() and death() take O(name.length) -> O(name.length * 10^5) = O(15 * 10^5) = O(10^6)
+//  getInheritanceOrder() -> O((# birth call) * 10 * name.length) = O(10^5 * 10 * 15) = O(10^7)
+// Space:
+//  O(name.length * (# birth call)) = O(15 * 10^5) = O(10^6)
