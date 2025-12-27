cc673c3dc8df72cd702167d04a176b7b0d9b574c
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-27 22:59:41 +0800

    Time: 7 ms (63.57%), Space: 8.4 MB (99.19%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,62 +1,25 @@
 class Solution {
 public:
-    // Approach: DP
-    // Time: O(n^2)
-    // Space: O(n^2)
-
     int countSubstrings(string s) {
-        int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int count = 0;
+        int s_length = s.size();
+        if (s_length == 0) return 0;
 
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-            count++;
-        }
+        int substring_count = 0;
 
-        for (int l = n - 1; l >= 0; l--) {
-            for (int r = l + 1; r < n; r++) {
-                if (l + 1 == r) {
-                    dp[l][r] = s[l] == s[r];
-                } else {
-                    dp[l][r] = dp[l + 1][r - 1] && s[l] == s[r];
-                }
-                if (dp[l][r]) {
-                    count++;
-                }
+        auto expand = [&](int left, int right) {
+            while (left >= 0 && right < s_length && s[left] == s[right]) {
+                substring_count++;
+                left--;
+                right++;
             }
+        };
+
+        for (int i = 0; i < s_length; i++) {
+            expand(i, i);
         }
-
-        return count;
+        for (int i = 1; i < s_length; i++) {
+            expand(i - 1, i);
+        }
+        return substring_count;
     }
-
-    // Two Pointers
-    // Time: O(n^2)
-    // Space: O(1)
-
-    // int countInSubstring(string& s, int l, int r) {
-    //     if (s[l] != s[r]) return 0;
-    //     int n = s.size();
-    //     int count = 1;
-
-    //     while (l > 0 && r < n - 1 && s[l - 1] == s[r + 1]) {
-    //         l--;
-    //         r++;
-    //         count++;
-    //     }
-    //     return count;
-    // }
-
-    // int countSubstrings(string s) {
-    //     int n = s.size();
-    //     int ans = 0;
-
-    //     for (int i = 0; i < n; i++) {
-    //         ans += countInSubstring(s, i, i);
-    //     }
-    //     for (int i = 0; i < n - 1; i++) {
-    //         ans += countInSubstring(s, i, i + 1);
-    //     }
-    //     return ans;
-    // }
 };
