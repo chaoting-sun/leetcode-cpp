de73d2b88544b65f653af1bc0cffb0140f1cdcac
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-27 22:54:26 +0800

    Time: 12 ms (57.41%), Space: 9.3 MB (78.5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,42 +1,28 @@
 class Solution {
-private:
-    int max_left_idx = 0;
-    int max_length = 1;
-
-    void expand(string& s, int& left, int& right) {
-        int s_length = s.size();
-        while (left > 0 && right < s_length - 1 && s[left - 1] == s[right + 1]) {
-            left--;
-            right++;    
-        }
-    }
-
-    void updateMax(int left, int right) {
-        if (right - left + 1 > max_length) {
-            max_length = right - left + 1;
-            max_left_idx = left;
-        }   
-    }
-
 public:
     string longestPalindrome(string s) {
         int s_length = s.size();
         if (s_length == 0) return "";
         
+        int max_left_idx = 0;
+        int max_length = 1;
 
-        // odd
-        for (int i = 0; i < s_length; i++) {
-            int left = i, right = i;
-            expand(s, left, right);
-            updateMax(left, right);
-        }
-        // even
-        for (int i = 1; i < s_length; i++) {
-            if (s[i - 1] != s[i]) continue;
-            int left = i - 1, right = i;
-            expand(s, left, right);
-            updateMax(left, right);
-        }
+        auto expand = [&](int left, int right) {
+            while (left >= 0 && right < s_length && s[left] == s[right]) {
+                left--;
+                right++;    
+            }
+            int current_length = right - left - 1;
+            if (current_length > max_length) {
+                max_length = current_length;
+                max_left_idx = left + 1;
+            }
+        };
+
+        // odd and even
+        for (int i = 0; i < s_length; i++) expand(i, i);
+        for (int i = 1; i < s_length; i++) expand(i - 1, i);
+        
         return s.substr(max_left_idx, max_length);
     }
 };
