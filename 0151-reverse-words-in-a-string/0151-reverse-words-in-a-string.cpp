94dcf0378ded70201ab6f221b86f8e0602009e08
Sun, Chao-Ting <chaoting0108@gmail.com>
2025-12-28 22:50:20 +0800

    Time: 0 ms (100%), Space: 9.8 MB (94.64%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -4,38 +4,89 @@ public:
         int s_length = s.size();
         if (s_length == 0) return "";
 
+        // " a bc " -> " cb a "
+        // reverse the string
         reverse(s.begin(), s.end());
-        
-        string reversed = "";
 
-        int left = 0, right = 0;
-        while (right < s_length && s[right] == ' ') {
-            left++;
-            right++;
+        // "cb aa "
+        //      l
+        //        r
+
+        // remove the redundant empty space
+        int read_ptr = 0, write_ptr = 0;
+        while (read_ptr < s_length) {
+            if (s[read_ptr] == ' ') {
+                read_ptr++;
+                continue;
+            }
+
+            // not the first word -> add empty space
+            if (write_ptr != 0) {
+                s[write_ptr++] = ' ';
+            }
+
+            while (read_ptr < s_length && s[read_ptr] != ' ') {
+                s[write_ptr++] = s[read_ptr++];
+            }
         }
+        s.resize(write_ptr);
 
-        while (right < s_length) {
-            while (right < s_length && s[right] != ' ') {
-                right++;
+        // reverse each word
+        int start = 0;
+        int new_s_length = s.size();
+        for (int end = 0; end <= new_s_length; end++) {
+            if (end == new_s_length || s[end] == ' ') {
+                reverse(s.begin() + start, s.begin() + end);
+                start = end + 1;
             }
-
-            for (int i = right - 1; i >= left; i--) {
-                reversed += s[i];
-            }
-
-            while (right < s_length && s[right] == ' ') {
-                right++;
-            }
-
-            if (right < s_length) {
-                reversed += ' ';
-            }
-
-            left = right;
         }
-        return reversed;
+        return s;
     }
 };
+
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         int s_length = s.size();
+//         if (s_length == 0) return "";
+
+//         reverse(s.begin(), s.end());
+        
+//         string reversed = "";
+
+//         int left = 0, right = 0;
+//         while (right < s_length && s[right] == ' ') {
+//             left++;
+//             right++;
+//         }
+
+//         while (right < s_length) {
+//             // increment right to the space after the reversed current word
+//             while (right < s_length && s[right] != ' ') {
+//                 right++;
+//             }
+
+//             // paste the reversed current word reversely to make the characters in order
+//             for (int i = right - 1; i >= left; i--) {
+//                 reversed += s[i];
+//             }
+
+//             // increment right to the start of the next word
+//             while (right < s_length && s[right] == ' ') {
+//                 right++;
+//             }
+
+//             // if right is less than the length of s, it means the next word exists
+//             if (right < s_length) {
+//                 reversed += ' ';
+//             }
+
+//             // assign right to left and start processing the next word
+//             left = right;
+//         }
+//         return reversed;
+//     }
+// };
 
 // happy path: s = "ab bc" -> output = "bc ab"
 // trace:
@@ -61,4 +112,4 @@ public:
 // 3    3   5  "c ab"   5   5 -> break
 // < returns "c ab"
 
-// WA: s = "  hello world  "
+// WA: s = "  hello world  " -> output " world hello "
