02b2c1f7d6dcdc0a3be9b4627254de376bfb9d9b
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-04 16:54:30 +0800

    Time: 4 ms (54.79%), Space: 18.2 MB (70.99%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,88 +1,43 @@
 class Solution {
 public:
-    // Approach1: Topological Sort by Kahn's Algorithm
-    // Time: O(E + V)
-    // Space: O(E + V)
-
-    // vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-    //     vector<int> in_degree(numCourses);
-    //     vector<vector<int>> adj(numCourses);
-
-    //     // Time: O(E + V)
-    //     // Space: O(E + V)
-    //     for (auto pre: prerequisites) {
-    //         in_degree[pre[0]]++;
-    //         adj[pre[1]].push_back(pre[0]);
-    //     }
-
-    //     // Time: O(V)
-    //     vector<int> ans;
-    //     queue<int> q;
-    //     for (int i = 0; i < numCourses; i++) {
-    //         if (in_degree[i] == 0) {
-    //             q.push(i);
-    //         }
-    //     }
-
-    //     // Time: O(E + V)
-    //     while (!q.empty()) {
-    //         int size = q.size();
-    //         for (int i = 0; i < size; i++) {
-    //             int curr_course = q.front();
-    //             q.pop();
-    //             ans.push_back(curr_course);
-
-    //             for (int next_course: adj[curr_course]) {
-    //                 in_degree[next_course]--;
-    //                 if (in_degree[next_course] == 0) {
-    //                     q.push(next_course);
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     return ans.size() == numCourses ? ans : vector<int>();
-    // }
-
-    // Approach2: Topological Sort by DFS
-    // Time: O(E + V)
-    // Space: O(E + V)
-
-    bool findCycle(vector<vector<int>>& adj, stack<int>& stk, vector<int>& visited, int curr_course) {
-        if (visited[curr_course] == 1) return true; // visited a course being visited
-        if (visited[curr_course] == 2) return false; // visit a course visited before
-
-        visited[curr_course] = 1;
-        for (int next_course: adj[curr_course]) {
-            if (findCycle(adj, stk, visited, next_course)) {
-                return true;
-            }
-        }
-        visited[curr_course] = 2; // finish visit
-        stk.push(curr_course);
-        return false;
-    }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
+        vector<vector<int>> course_order(numCourses);
+        vector<int> in_degree(numCourses);
 
-        for (auto pre: prerequisites) {
-            adj[pre[1]].push_back(pre[0]);
+        for (auto& courses: prerequisites) {
+            // b is a's prerequisite
+            course_order[courses[1]].push_back(courses[0]);
+            in_degree[courses[0]]++;
         }
 
-        stack<int> stk;
-        vector<int> visited(numCourses, 0);
+        queue<int> q;
         for (int i = 0; i < numCourses; i++) {
-            if (findCycle(adj, stk, visited, i)) {
-                return {};
+            if (in_degree[i] == 0) q.push(i);
+        }
+        
+        vector<int> results;
+
+        while (!q.empty()) {
+            int current_course = q.front();
+            q.pop();
+            results.push_back(current_course);
+            
+            if (results.size() == numCourses) break;
+
+            for (int next_course: course_order[current_course]) {
+                // has been processed or pushed to q
+                if (in_degree[next_course] == 0) continue;
+                in_degree[next_course]--;
+                if (in_degree[next_course] == 0) {
+                    q.push(next_course);
+                }
             }
         }
 
-        vector<int> ans;
-        while (!stk.empty()) {
-            ans.push_back(stk.top());
-            stk.pop();
+        if (results.size() == numCourses) {
+            return results;
         }
-        return ans;
+        return {};
+        // return results.size() == numCourses ? results : {};
     }
 };
