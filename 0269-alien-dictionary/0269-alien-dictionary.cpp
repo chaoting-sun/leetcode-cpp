b2e6e8ded8b06df26e42a955c9dcdf5113913e85
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-04 18:33:25 +0800

    Time: 0 ms (100%), Space: 12.1 MB (75.94%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.cpp
+++ 0269-alien-dictionary/0269-alien-dictionary.cpp
@@ -1,68 +1,122 @@
 class Solution {
 public:
-    // Topological Sort (Kahn's Algorithm)
-    // Time: O(|word_{0}|+...+|word_{n-1}|)
-    // Space: O(V + E). V is the number of different characters.
-
     string alienOrder(vector<string>& words) {
-        unordered_map<char, unordered_set<char>> order;
-        // indegree for every seen char
-        unordered_map<char, int> indegree;
-
-        // Time: O(|word_{0}|+...+|word_{n-1}|)
-        int n = words.size();
-        for (int i = 0; i < n - 1; i++) {
-            string& a = words[i];
-            string& b = words[i + 1];
-            int min_size = min(a.size(), b.size());
-
-            // find first different char
-            int j = 0;
-            while (j < min_size && a[j] == b[j]) j++;
-
-            // all equal up to min length. if a is longer, it's invalid
-            if (j == min_size) {
-                if (a.size() > b.size()) return "";
-            } else {
-                order[a[j]].insert(b[j]);
-            }
-        }
-
+        unordered_set<int> available;
         for (const string& word: words) {
-            for (char c: word) {
-                if (!indegree.count(c)) indegree[c] = 0;
+            for (char ch: word) {
+                available.insert(ch - 'a');
             }
         }
 
-        for (auto& [c1, c2set]: order) {
-            for (char c2: c2set) {
-                indegree[c2]++;   
-            }
-        }
+        vector<vector<int>> adj(26);
+        vector<int> in_degree(26);
+        vector<bool> required(26);
 
-        queue<char> q;
-        for (auto& [c, deg]: indegree) {
-            if (deg == 0) q.push(c);
-        }
+        for (int i = 0; i < words.size() - 1; i++) {
+            string prev = words[i];
+            string curr = words[i + 1];
+            int min_length = min(prev.size(), curr.size());
+            for (int j = 0; j < min_length; j++) {
+                if (prev[j] != curr[j]) {
+                    adj[prev[j] - 'a'].push_back(curr[j] - 'a');
+                    in_degree[curr[j] - 'a']++;
 
-        // Time: O(V + E). for every character it may be recorded to be in front of all the other characters
-        // V = 26 at most, and E = 26 * 25 at most, which is small
-        string ans = "";
-        while (!q.empty()) {
-            char u = q.front();
-            q.pop();
-            ans += u;
-            for (char v: order[u]) {
-                indegree[v]--;
-                if (indegree[v] == 0) {
-                    q.push(v);
+                    required[prev[j] - 'a'] = true;
+                    required[curr[j] - 'a'] = true;
+                    break;
+                } else {
+                    // ab should be before abc
+                    if (j == min_length - 1 && prev.size() > curr.size()) {
+                        return "";
+                    }
                 }
             }
         }
 
-        // if there is any cycle, it's invalid
-        if (ans.size() < indegree.size()) return "";
-        return ans;
+        int required_count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (required[i]) required_count++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < 26; i++) {
+            if (in_degree[i] == 0 && required[i]) q.push(i);
+        }
+
+        string result = "";
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            result.push_back(curr + 'a');
+            available.erase(curr);
+
+            for (int nxt: adj[curr]) {
+                in_degree[nxt]--;
+                if (in_degree[nxt] == 0) {
+                    q.push(nxt);
+                }
+            }
+        }
+
+        if (result.size() != required_count) {
+            return "";
+        }
+
+        for (const char& ch: available) {
+            result.push_back(ch + 'a');
+        }
+        return result;
     }
 };
 
+// Submit Error
+// q.push_back(nxt); -> q.push(nxt);
+
+
+// test case: ['ab', 'bc', 'bd', 'bde'] -> (a b) (c d) (e)
+// trace:
+// available = { 0, 1, 2, 3, 4 }
+// adj[0] = [1]
+// in_degree = [0 1 0 0 0]
+// adj[2] = [3]
+// in_degree = [0 1 0 1 0]
+// required = [t t t t f]
+// required_count = 5
+// q = [0 2]
+// curr = 0
+// q = [2]
+// result = a
+// available = { 1, 2, 3, 4 }
+// in_degree = [0 0 0 1 0]
+// nei -> q = [2 1]
+// curr = 2
+// q = [1]
+// result = ac
+// available = { 1, 3, 4 }
+// nei -> q = [1 3]
+// in_degree = [0 0 0 0 0] -> push: q = [1 3]
+// curr = 1
+// q = [3]
+// result = acb
+// available = { 3, 4 }
+// in_degree = [0 0 0 0 0]
+// curr = 3
+// q = []
+// result = acbd
+// available = { 4 }
+// result = acbde
+// < returnc acbde
+
+// test case: ['ab', 'a'] -> ""
+// trace:
+// a == a, j == 0, 2 > 1 -> returns ""
+
+// test case: ['ba', 'ab'] -> ""
+// trace:
+// j = 0, 1
+// adj[1] = [0], adj[0] = [1]
+// in_degree = [1, 1]
+// required = 2
+// 0 != 2 -> returns ""
