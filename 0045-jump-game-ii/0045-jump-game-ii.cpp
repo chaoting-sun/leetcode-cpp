0f33fa10ace5dd4238873bb586679c32e2b443df
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-05 23:02:50 +0800

    Time: 2 ms (30.66%), Space: 21.6 MB (20.81%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,61 +1,31 @@
 class Solution {
 public:
-    // Approach: Backtracking + Memoization
-    // Time: O(n^2)
-    // Space: O(n)
-
-    // int backtrack(vector<int>& nums, int start, vector<int> &memo) {
-    //     int n = nums.size();
-
-    //     // base case
-    //     if (start == n - 1) return 0;
-    //     if (nums[start] == 0) return INT_MAX; // unreachable
-    
-    //     if (memo[start] != -1) return memo[start];
-
-    //     int min_length = INT_MAX; // unreachable
-    //     int furthest_jump = min(n - 1, start + nums[start]);
-    //     for (int end = start + 1; end <= furthest_jump; end++) {
-    //         int curr_length = backtrack(nums, end, memo);
-    //         if (curr_length != INT_MAX) {
-    //             min_length = min(min_length, 1 + curr_length);
-    //         }
-    //     }
-    //     return memo[start] = min_length;
-    // }
-    
-    // int jump(vector<int>& nums) {
-    //     vector<int> memo(nums.size(), -1); // -1 -> not computed, INT_MAX -> unreachable
-    //     int min_length = backtrack(nums, 0, memo);
-    //     return min_length == INT_MAX ? 0 : min_length;
-    // }
-
-    // Approach: Greedy
-    // think it as a simplified BFS. we start from 0, and use l and r to record the next window
-    // that can be reached from 0 in one step. then iterate the range to find the next range
-    // that can be reached in current range in one step.
-    // Time: O(n)
-    // Space: O(1)
-
     int jump(vector<int>& nums) {
         int n = nums.size();
-        int l = 0, r = 0;
-        int ans = 0;
-
-        while (r < n - 1) {
-            int tmpr = INT_MIN;
-            for (int i = l; i <= r; i++) {
-                int furthest_jump = min(i + nums[i], n - 1);
-                tmpr = max(tmpr, furthest_jump);
-            }
-            
-            if (tmpr == r) return 0;
-            
-            l = r + 1;
-            r = tmpr;
-            ans++;
+        if (n == 0) return 0;
+        
+        vector<int> max_reach(n);
+        for (int i = 0; i < n; i++) {
+            max_reach[i] = min(n - 1, i + nums[i]);
         }
-        return ans;
-    }
 
+        int jumps = 0;
+        int current_reach = 0;
+        int next_reach = 0;
+        
+        for (int i = 0; i < n - 1; i++) {
+            next_reach = max(next_reach, max_reach[i]);
+            if (i == current_reach) {
+                if (next_reach <= i) return -1;
+                current_reach = next_reach;
+                jumps++;
+            }
+        }
+
+        return current_reach == n - 1 ? jumps : -1;
+    }
 };
+
+// nums = [2,3,1,1,4]
+//         0 1 2 3 4
+// max_reach = [2,4,3,4,5]
