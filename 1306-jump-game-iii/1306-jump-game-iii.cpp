cd35d9cb3063bb51ea47db4d62ee96d9f8b5d698
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-07 20:38:37 +0800

    Time: 0 ms (100%), Space: 35.2 MB (89.71%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,14 +1,45 @@
+// class Solution {
+// public:
+//     bool canReach(vector<int>& arr, int start) {
+//         int n = arr.size();
+//         if (n == 0) return false;
+
+//         vector<bool> jumped(n, false);
+        
+//         queue<int> q;
+//         q.push(start);
+//         jumped[start] = true;
+        
+//         while (!q.empty()) {
+//             int curr = q.front();
+//             q.pop();
+//             if (arr[curr] == 0) return true;
+            
+//             int prev = curr - arr[curr];
+//             int next = curr + arr[curr];
+//             if (prev >= 0 && !jumped[prev]) {
+//                 q.push(prev);
+//                 jumped[prev] = true;
+//             }
+//             if (next < n && !jumped[next]) {
+//                 q.push(next);
+//                 jumped[next] = true;
+//             }
+//         }
+
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
         if (n == 0) return false;
-
-        vector<bool> jumped(n, false);
         
         queue<int> q;
         q.push(start);
-        jumped[start] = true;
+        arr[start] = -arr[start];
         
         while (!q.empty()) {
             int curr = q.front();
@@ -16,19 +47,18 @@ public:
             if (arr[curr] == 0) return true;
             
             int prev = curr - arr[curr];
-            int next = curr + arr[curr];
-            if (prev >= 0 && !jumped[prev]) {
+            if (prev < n && prev >= 0 && arr[prev] >= 0) {
                 q.push(prev);
-                jumped[prev] = true;
+                arr[prev] = -arr[prev];
             }
-            if (next < n && !jumped[next]) {
+
+            int next = curr + arr[curr];
+            if (next < n && next >= 0 && arr[next] >= 0) {
                 q.push(next);
-                jumped[next] = true;
+                arr[next] = -arr[next];
             }
         }
 
         return false;
     }
 };
-
-// WA
