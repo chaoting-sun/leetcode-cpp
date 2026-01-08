202b56a80b35f4ee800106222609924fe3f7220c
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-09 07:34:12 +0800

    Time: 3 ms (27.96%), Space: 8.5 MB (53.68%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    // [2^31, 2^31 - 1] = [-2147483648, 2147483647]
-
     int reverse(int x) {
-        int rev = 0;
+        int sign = 1;
+        if (x < 0) sign = -1;
+
+        int result = 0;
         while (x != 0) {
-            int pop = x % 10; // negative if x < 0
+            int digit = abs(x % 10);
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit == INT_MAX % 10)) {
+                return 0;
+            }
+
+            result = result * 10 + digit;
             x /= 10;
-
-            // check upper bound
-            // rev should be not larger than 214748364 to avoid overflow
-            // if it is equal to 214748364, then the pop should be less than or equal to 7
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
-            // check lower bound
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
-
-            rev = rev * 10 + pop;
         }
-        return rev;
+        return sign * result;
     }
 };
