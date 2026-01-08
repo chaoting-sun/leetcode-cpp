8fe99b9ec537d3b6f963af38c9c6a187896a7d16
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-08 22:52:53 +0800

    Time: 0 ms (100%), Space: 23.9 MB (19.3%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int length = nums.size();
-        if (length == 0) return;
-        
-        int left = 0;
-        int right = 0;
-        while (right < length) {
-            if (nums[right] != 0) {
-                swap(nums[left], nums[right]);
-                left++;
-                right++;
-            } else {
-                right++;
+        int n = nums.size();
+        if (n == 0) return;
+
+        int write = 0;
+        for (int read = 0; read < n; read++) {
+            if (nums[read] != 0) {
+                nums[write] = nums[read];
+                write++;
             }
+        }
+        for (int i = write; i < n; i++) {
+            nums[i] = 0;
         }
     }
 };
-
-// CE: return 0 -> return
