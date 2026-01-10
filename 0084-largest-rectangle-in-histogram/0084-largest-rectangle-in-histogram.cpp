3467912e9ff434de850572b84c766ccf2f8b6acd
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-10 14:56:35 +0800

    Time: 11 ms (93.63%), Space: 79.4 MB (96.19%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,35 +1,67 @@
+// O(n)
+
+// class Solution {
+// public:
+//     int largestRectangleArea(vector<int>& heights) {
+//         int n = heights.size();
+//         if (n == 0) return 0;
+        
+//         vector<int> left_smaller_idx(n, -1);
+//         vector<int> right_smaller_idx(n, n);
+//         stack<int> stk;
+
+//         for (int i = 0; i < n; i++) {
+//             while (!stk.empty() && heights[stk.top()] >= heights[i]) {
+//                 stk.pop();
+//             }
+//             if (!stk.empty()) left_smaller_idx[i] = stk.top();
+//             stk.push(i);
+//         }
+
+//         while (!stk.empty()) stk.pop();
+
+//         for (int i = n - 1; i >= 0; i--) {
+//             while (!stk.empty() && heights[i] <= heights[stk.top()]) {
+//                 stk.pop();
+//             }
+//             if (!stk.empty()) right_smaller_idx[i] = stk.top();
+//             stk.push(i);
+//         }
+
+//         int max_area = 0;
+//         for (int i = 0; i < n; i++) {
+//             int current_area = heights[i] * (right_smaller_idx[i] - left_smaller_idx[i] - 1);
+//             max_area = max(max_area, current_area);
+//         }
+
+//         return max_area;
+//     }
+// };
+
+// O(1)
+
+// strictly increasing monotonic stack
+
+// [1(2) 3(5) 4(10)] heights[i] = 3
+// stk.top() -> h = 10, w = i - 上一個 index - 1
+
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        if (n == 0) return 0;
-        
-        vector<int> left_smaller_idx(n, -1);
-        vector<int> right_smaller_idx(n, n);
-        stack<int> stk;
-
-        for (int i = 0; i < n; i++) {
-            while (!stk.empty() && heights[stk.top()] >= heights[i]) {
-                stk.pop();
-            }
-            if (!stk.empty()) left_smaller_idx[i] = stk.top();
-            stk.push(i);
-        }
-
-        while (!stk.empty()) stk.pop();
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stk.empty() && heights[i] <= heights[stk.top()]) {
-                stk.pop();
-            }
-            if (!stk.empty()) right_smaller_idx[i] = stk.top();
-            stk.push(i);
-        }
+        heights.push_back(0);
 
         int max_area = 0;
-        for (int i = 0; i < n; i++) {
-            int current_area = heights[i] * (right_smaller_idx[i] - left_smaller_idx[i] - 1);
-            max_area = max(max_area, current_area);
+        stack<int> stk;
+
+        for (int i = 0; i < heights.size(); i++) {
+            while (!stk.empty() && heights[stk.top()] >= heights[i]) {
+                int current_height = heights[stk.top()];
+                stk.pop();
+                int left_min = stk.empty() ? -1 : stk.top();
+                int current_width = i - left_min - 1;
+                max_area = max(max_area, current_height * current_width);
+            }
+            stk.push(i);
         }
 
         return max_area;
