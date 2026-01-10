b9832fbd45bea8e316a37c8e6c708355e4596ca0
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-10 15:40:05 +0800

    Time: 8 ms (47.15%), Space: 17.4 MB (89.93%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-    // Approach: treat each row as floor, and use leetcode 84 method to get the answer
-    // Time: O(mn)
-    // Space: O(n)
-
-    // Time: O(n)
-    // Space: O(n)
-    int maximalRectangleInHistograms(vector<int> heights) {
-        heights.insert(heights.begin(), 0);
-        heights.push_back(0);    
-        stack<int> index_stack;
-        int max_area = 0;
-
-        for (int i = 0; i < (int)heights.size(); i++) {
-            while (!index_stack.empty() && heights[index_stack.top()] > heights[i]) {
-                int height = heights[index_stack.top()];
-                index_stack.pop();
-                int width = i - index_stack.top() - 1;
-                max_area = max(max_area, height * width);
-            }
-            index_stack.push(i);
-        }
-        return max_area;
-    }
-
     int maximalRectangle(vector<vector<char>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        vector<int> dp(n);
+        vector<int> heights(n);
         int max_area = 0;
-        // O(m)
+
         for (int i = 0; i < m; i++) {
-            // O(n)
+            // update heights
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '0') dp[j] = 0;
-                else dp[j] += 1;
+                heights[j] = matrix[i][j] == '1' ? heights[j] + 1 : 0;
             }
-            max_area = max(max_area, maximalRectangleInHistograms(dp));
+
+            stack<int> stk;
+            for (int j = 0; j <= n; j++) {
+                int next_height = (j == n ? 0 : heights[j]); // 0
+                while (!stk.empty() && heights[stk.top()] >= next_height) {
+                    int current_height = heights[stk.top()]; // 1
+                    stk.pop();
+                    int left_bound = stk.empty() ? -1 : stk.top(); // -1
+                    int width = j - left_bound - 1; // 1 - (-1) - 1 = 1
+                    max_area = max(max_area, current_height * width);
+                }
+                stk.push(j); // stk = [1]
+            }
         }
+
         return max_area;
     }
 };
