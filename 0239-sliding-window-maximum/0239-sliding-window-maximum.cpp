73053a45d5cc0bd3e2f2d8691386808f9ca9109b
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-10 14:21:03 +0800

    Time: 22 ms (62.43%), Space: 139 MB (93.97%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,33 +1,73 @@
+// nlogk ~ nlogn
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         if (n < k) return {};
+
+//         priority_queue<pair<int,int>> max_heap;
+//         vector<int> result;
+
+//         for (int i = 0; i < n; i++) {
+//             max_heap.push({ nums[i], i });
+//             if (i < k - 1) continue;
+
+//             // [i - k + 1, i]
+//             while (max_heap.top().second <= i - k) {
+//                 max_heap.pop();
+//             }
+//             max_heap.push({ nums[i], i });
+//             result.push_back(max_heap.top().first);
+//         }
+
+//         return result;
+//     }
+// };
+
+// monotonic queue; strictly decreasing
+// [大 -> 小]
+// 最長：k
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
-        if (n < k) return {};
+        if (n < k || k == 0) return {};
 
-        priority_queue<pair<int,int>> max_heap;
+        deque<int> dq;
         vector<int> result;
 
         for (int i = 0; i < n; i++) {
-            max_heap.push({ nums[i], i });
-            if (i < k - 1) continue;
-
-            // [i - k + 1, i]
-            while (max_heap.top().second <= i - k) {
-                max_heap.pop();
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
             }
-            max_heap.push({ nums[i], i });
-            result.push_back(max_heap.top().first);
+            dq.push_back(i);
+            // [i - k + 1, i]
+            while (i - dq.front() + 1 > k) {
+                dq.pop_front();
+            }
+
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
         }
 
         return result;
     }
 };
 
-// max heap (k) -> (val, idx)
+// nums = [1, 3, 1, 2], k = 2
+// dq = [0]
+// for loop: i = 1 2 3
+// i = 1
+//  dq = [], dq = [1]
+//  result = [3]
+// i = 2
+//  dq = [1, 2]
+//  result = [3, 3]
+// i = 3
+//  dq = [1]
+//  dq = [1, 3]
+//  dq = [3]
+//  result = [3,3,2]
 
-// 1   3   4   5   7
-// [       ]
-
-// 1. if the element is in the window -> pop
-// 2. add new element
-// 3. get the max value on the top
