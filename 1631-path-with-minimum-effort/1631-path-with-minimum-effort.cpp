8ec2832e5e0747f2030f867098b2a468d04a392d
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-10 23:39:21 +0800

    Time: 159 ms (19.87%), Space: 49.6 MB (14.24%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,130 +1,61 @@
-// Approach: Dijkstra
-// Time: O(mn*log(mn))
-// Space: O(mn)
-
-// using PI2 = pair<int,int>;
-
-// class Solution {
-// public:
-//     vector<int> dx = { 1, -1, 0, 0 };
-//     vector<int> dy = { 0, 0, 1, -1 };
-
-//     int minimumEffortPath(vector<vector<int>>& heights) {
-//         int m = heights.size(), n = heights[0].size();
-//         if (m == 0 || n == 0) return -1;
-        
-//         priority_queue<PI2, vector<PI2>, greater<>> pq;
-//         vector<vector<int>> distance(m, vector<int>(n, INT_MAX));
-
-//         pq.push({ 0, 0 });
-//         distance[0][0] = 0;
-
-//         while (!pq.empty()) {
-//             auto [currentDistance, position] = pq.top();
-//             int x = position / n, y = position % n;
-//             pq.pop();
-
-//             if (currentDistance != distance[x][y]) continue;
-//             if (x == m - 1 && y == n - 1) {
-//                 return currentDistance;
-//             }
-
-//             for (int i = 0; i < 4; i++) {
-//                 int nx = x + dx[i];
-//                 int ny = y + dy[i];
-//                 if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-//                 int newDistance = max(abs(heights[x][y] - heights[nx][ny]), distance[x][y]);
-//                 if (newDistance < distance[nx][ny]) {
-//                     distance[nx][ny] = newDistance;
-//                     pq.push({ distance[nx][ny], nx * n + ny });
-//                 }
-//             }
-//         }
-
-//         return -1;
-//     }    
-// };
-
-
-// Kruskal's Algorithm
-// Time: O(mn*log(mn))
-// Space: O(mn)
-
-struct DSU {
-    vector<int> parent;
-    vector<int> size;
-
-    explicit DSU(int n): parent(n), size(n, 1) { iota(parent.begin(), parent.end(), 0); }
-    int find(int u) {
-        if (u == parent[u]) return u;
-        return parent[u] = find(parent[u]);
-    }
-    void unite(int u, int v) {
-        u = find(u);
-        v = find(v);
-        if (u != v) {
-            if (size[u] < size[v]) swap(u, v);
-            parent[v] = u;
-            size[u] += size[v];
-        }
-    }
-};
-
-struct Edge {
-    int u;
-    int v;
-    int w;
-    bool operator<(Edge& other) const {
-        return w < other.w;
-    };
-};
-
 class Solution {
+private:
+    vector<int> x_directions = { 1, -1, 0, 0 };
+    vector<int> y_directions = { 0, 0, 1, -1 };
+
+    bool canReach(const vector<vector<int>>& heights, int limit) {
+        int m = heights.size(), n = heights[0].size();
+
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        queue<pair<int,int>> q;
+        q.push({ 0, 0 });
+        visited[0][0] = true;
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+            if (x == m - 1 && y == n - 1) return true;
+
+            for (int i = 0; i < 4; i++) {
+                int new_x = x + x_directions[i];
+                int new_y = y + y_directions[i];
+                if (new_x >= 0 && new_x < m && new_y >= 0 && new_y < n && !visited[new_x][new_y] && abs(heights[x][y] - heights[new_x][new_y]) <= limit) {
+                    q.push({ new_x, new_y });
+                    visited[new_x][new_y] = true;
+                }
+            }
+        }
+        return false;
+    }
+
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int m = heights.size();
-        if (m == 0) return -1;
-        int n = heights[0].size();
-        if (n == 0) return -1;
-
-        if (m == 1 && n == 1) return 0; // early return for trivial case
-
-        auto idx = [n](int i, int j) { return i * n + j; };
+        int m = heights.size(), n = heights[0].size();
         
-        int total = m * n;
-        vector<Edge> edges;
-        edges.reserve(2 * total - n - m);
-        
+        int global_max_effort = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int currentIndex = idx(i, j);
-                if (i + 1 < m) {
-                    edges.emplace_back(Edge{
-                        idx(i, j),
-                        idx(i + 1, j),
-                        abs(heights[i][j] - heights[i + 1][j])
-                    });
+                if (i < m - 1) {
+                    global_max_effort = max(global_max_effort, abs(heights[i][j] - heights[i + 1][j]));
                 }
-                if (j + 1 < n) {
-                    edges.emplace_back(Edge{
-                        idx(i, j),
-                        idx(i, j + 1),
-                        abs(heights[i][j] - heights[i][j + 1])
-                    });
+                if (j < n - 1) {
+                    global_max_effort = max(global_max_effort, abs(heights[i][j] - heights[i][j + 1]));
                 }
             }
         }
 
-        sort(edges.begin(), edges.end());
-        
-        DSU dsu(total);
-        int lastIndex = total - 1;
-        
-        for (Edge& e: edges) {
-            dsu.unite(e.u, e.v);
-            if (dsu.find(0) == dsu.find(lastIndex)) return e.w;
+        int left = 0;
+        int right = global_max_effort;
+        // F...FT...T
+        //  l   l
+        //      r   r
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canReach(heights, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-
-        return -1; // make compiler happy
+        return right;
     }
 };
