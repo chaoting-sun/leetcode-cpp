9cca65e3071077e9fb38837f4ee8308f1a5a5a09
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-13 21:25:53 +0800

    Time: 67 ms (30.51%), Space: 75.1 MB (33.69%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -3,55 +3,50 @@ public:
     int maxEvents(vector<vector<int>>& events) {
         sort(events.begin(), events.end());
 
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> event_to_attend; // end, idx
+        priority_queue<int, vector<int>, greater<>> event_to_attend; // end
 
-        int min_day = INT_MAX, max_day = INT_MIN;
-        for (int i = 0; i < events.size(); i++) {
-            min_day = min(min_day, events[i][0]);
-            max_day = max(max_day, events[i][1]);
-        }
-
+        int day = events.front()[0], last_day = 1e5;
         int event_idx = 0;
-        int min_events = 0;
-        for (int i = min_day; i <= max_day; i++) {
-            // pop out all expired events
-            while (!event_to_attend.empty() && event_to_attend.top().first < i) {
-                event_to_attend.pop();
-            }
+        int event_count = 0;
+
+        while (day <= last_day) {
             // push in all available events
-            while (event_idx < events.size() && events[event_idx][0] <= i) {
-                event_to_attend.push({ events[event_idx][1], event_idx });
+            while (event_idx < events.size() && events[event_idx][0] <= day) {
+                event_to_attend.push(events[event_idx][1]);
                 event_idx++;
             }
-            if (!event_to_attend.empty()) {
-                min_events++;
+
+            // remove the outdated events
+            while (!event_to_attend.empty() && event_to_attend.top() < day) {
                 event_to_attend.pop();
+            }
+
+            if (!event_to_attend.empty()) {
+                event_to_attend.pop();
+                event_count++;
+            }
+            
+            // day
+            if (event_to_attend.empty() && event_idx < events.size()) {
+                day = events[event_idx][0];
+            } else if (event_to_attend.empty() && event_idx == events.size()) {
+                break;
+            } else {
+                day++;
             }
         }
 
-        return min_events;
+        return event_count;
     }
 };
 
-// test case: [2,2], [2,3], [2,3]
+// test case: events = [(1,2), (100,200)]
 // trace:
-// min_day = 2, max_day = 3
-// for loop: i = 2,3
-// i = 2
-// event_to_attend = [{ 2, 0 }], event_idx = 1
-// event_to_attend = [{ 2, 0 }, { 3, 1 }], event_idx = 2
-// event_to_attend = [{ 2, 0 }, { 3, 1 }, { 3, 2 }], event_idx = 3
-// min_events = 1, event_to_attend = [{ 3, 1 }, { 3, 2 }]
-// i = 3
-// min_events = 2, event_to_attend = [{ 3, 2 }]
-
-// test case: [1,2], [2,3], [3,4]
-// trace:
-// min_day = 1, max_day = 4
-// for loop, i = 1,2,3,4
-// i = 1
-// event_to_attend = [{ 2, 0 }] -> min_events = 1, event_to_attend = []
-// i = 2
-// event_to_attend = [{ 3, 1 }] -> min_events = 2, event_to_attend = []
-// i = 3
-// event_to_attend = [{ 4, 2 }] -> min_events = 3, event_to_attend = []
+// day = 1, last_day = 200
+// day = 1
+// event_to_attend = [2], event_idx = 1
+// event_to_attend = [], event_count = 1
+// day = 100
+// event_to_attend = [200], event_idx = 2
+// event_to_attend = [], event_count = 2
+// 
