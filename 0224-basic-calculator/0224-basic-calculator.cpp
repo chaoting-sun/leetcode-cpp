f24307092f4320b0646679af717d106c608d6bbd
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-14 19:05:34 +0800

    Time: 6 ms (28.76%), Space: 13.7 MB (23.85%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> stk;
+        stack<pair<int,long long>> stk;
 
         int sign = 1;
-        int operand = 0;
-        int ans = 0;
+        long long operand = 0;
+        long long ans = 0;
 
         for (int i = 0; i < s.size(); i++) {
             if (isdigit(s[i])) {
-                operand = operand * 10 + sign * (s[i] - '0');
+                operand = operand * 10 + (s[i] - '0');
             } else if (s[i] == '+') {
-                ans += operand;
+                ans += sign * operand;
                 sign = 1;
                 operand = 0;
             } else if (s[i] == '-') {
-                ans += operand;
+                ans += sign * operand;
                 sign = -1;
                 operand = 0;
             } else if (s[i] == '(') {
                 // save the snapshot
-                stk.push(ans);
-                stk.push(sign);
+                stk.push({ sign, ans });
                 // reset
                 ans = 0;
                 operand = 0;
                 sign = 1;
             } else if (s[i] == ')') {
-                int currentAns = ans + operand;
-                int prevSign = stk.top(); stk.pop();
-                int prevAns = stk.top(); stk.pop();
+                int currentAns = ans + sign * operand;
+                auto [prevSign, prevAns] = stk.top();
+                stk.pop();
                 ans = prevAns + prevSign * currentAns;
                 operand = 0;
             }
         }
 
-        ans += operand;
+        ans += sign * operand;
 
-        return ans;
+        return (int)ans;
     }
 };
