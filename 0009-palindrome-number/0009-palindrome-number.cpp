209dd17b6efbd789f29175fe406eebce4e2dfba5
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-01-20 22:07:16 +0800

    Time: 2 ms (51.61%), Space: 8.6 MB (39.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -17,18 +17,34 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if (x < 0) return false;
+
+//         long long reversed = 0;
+//         int y = x;
+//         while (y > 0) {
+//             reversed = reversed * 10 + y % 10;
+//             y /= 10;
+//         }
+
+//         return reversed == (long long)x;
+//     }
+// };
+
 class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
+        if (x % 10 == 0 && x > 0) return false;
 
-        long long reversed = 0;
-        int y = x;
-        while (y > 0) {
-            reversed = reversed * 10 + y % 10;
-            y /= 10;
+        int reversed = 0;
+        while (reversed < x) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
         }
 
-        return reversed == (long long)x;
+        return (x == reversed) || (x == reversed / 10);
     }
 };
