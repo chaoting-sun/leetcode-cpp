fc1ee844e12ad7b6eb2e06e873a22ac6b757f72e
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-02-22 16:19:49 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,20 +1,35 @@
 class Solution {
+private:
+    int rob_in_range(vector<int>& nums, int start, int end) {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[start] = nums[start];
+        for (int i = start + 1; i <= end; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[end];
+    }
+
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
 
-        vector<int> dp1(n + 1); // rob 1st house
-        dp1[1] = nums[0];
-        for (int i = 2; i < n; i++) {
-            dp1[i] = max(dp1[i - 1], dp1[i - 2] + nums[i - 1]);
-        }
-
-        vector<int> dp2(n + 1); // do not rob 1st house
-        for (int i = 2; i <= n; i++) {
-            dp2[i] = max(dp2[i - 1], dp2[i - 2] + nums[i - 1]);
-        }
-
-        return max(dp1[n - 1], dp2[n]);
+        int max1 = rob_in_range(nums, 0, n - 2);
+        int max2 = rob_in_range(nums, 1, n - 1);
+        return max(max1, max2);
     }
 };
+
+// test case: nums = [2,4,3] -> 4
+// trace:
+// n = 3
+// max1 = rob_in_range(nums, 0, 1)
+//  - dp = [0 2 0 0]
+//  - for loop: 2 - 1
+//  < returns 2
+// max2 = rob_in_range(nums, 1, 2)
+//  - dp = [0 0 4 0]
+//  - for loop: 3 - 2
+//  < returns 4
+// < returns 4
