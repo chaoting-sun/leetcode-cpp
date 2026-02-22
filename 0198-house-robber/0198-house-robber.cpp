070533d6cb70bf847d6bb7b080c4b8f6a8d1233a
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-02-22 11:33:16 +0800

    Time: 0 ms (100%), Space: 10.7 MB (44.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-    // Approach: DP
-    // state transition function: dp[i] = max(dp[i-2] + nums[i], dp[i-1])
-    // - dp[i-2] + nums[i]: rob i
-    // - dp[i-1]: not rob i
-    // Time: O(n)
-    // Space: O(n)
-
-    // int rob(vector<int>& nums) {
-    //     int n = nums.size();
-    //     if (n == 1) return nums[0];
-
-    //     vector<int> dp(n + 1);
-    //     dp[1] = nums[0];
-
-    //     for (int i = 1; i < n; i++) {
-    //         dp[i + 1] = max(dp[i - 1] + nums[i], dp[i]);
-    //     }
-
-    //     return dp[n];
-    // }
-
-    // Approach: Two variables maintaining the prev max and curr max money
-    // Time: O(n)
-    // Space: O(1)
-
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1) return nums[0];
+        
+        vector<int> dp_rob(n + 1, 0); // nums[i - 1] -> dp1[i]
+        vector<int> dp_not_rob(n + 1, 0);
 
-        int prev_max = 0;
-        int curr_max = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            int next_max = max(prev_max + nums[i], curr_max);
-            prev_max = curr_max;
-            curr_max = next_max;
+        for (int i = 1; i <= n; i++) {
+            dp_rob[i] = dp_not_rob[i - 1] + nums[i - 1];
+            dp_not_rob[i] = max(dp_rob[i - 1], dp_not_rob[i - 1]);
         }
-
-        return curr_max;
+        
+        return max(dp_rob[n], dp_not_rob[n]);
     }
 };
+
+// i:
+// i - 1 is stolen -> i: x
+// i - 1 is not stolen -> i: x, o
+
+// dp1: dp[i] max money from 0 to i if i is stolen
+// dp2: dp[i] max money from 0 to i if i is not stolen
+// answer max(dp1[n], dp2[n])
