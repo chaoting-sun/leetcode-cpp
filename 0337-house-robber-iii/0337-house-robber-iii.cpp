ed255427ddac905023bd1e6f1c04c5a3132d99ad
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-02-22 17:46:14 +0800

    Time: 0 ms (100%), Space: 22.3 MB (89.89%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -16,45 +16,51 @@ private:
     pair<int,int> dfs(TreeNode* root) {
         if (!root) return { 0, 0 };
 
-        pair<int,int> left_result = dfs(root->left);
-        pair<int,int> right_result = dfs(root->right);
+        auto [l_rob, l_skip] = dfs(root->left);
+        auto [r_rob, r_skip] = dfs(root->right);
 
-        int max_rob = root->val + left_result.second + right_result.second;
-        int max_not_rob = max(left_result.first, left_result.second) + max(right_result.first, right_result.second);
+        int rob = root->val + l_skip + r_skip;
+        int skip = max(l_rob, l_skip) + max(r_rob, r_skip);
 
-        return { max_rob, max_not_rob };
+        return { rob, skip };
     }
 
 public:
     int rob(TreeNode* root) {
-        pair<int,int> root_result = dfs(root);
-        return max(root_result.first, root_result.second);
+        auto [root_rob, root_skip] = dfs(root);
+        return max(root_rob, root_skip);
     }
 };
 
+// run it in post order
 // test case:
 //      3
 //    2   6
 //     5    1
 //  -> 11
 // trace:
-// dfs(root, false)
-//  max_not_rob = dfs(2, false) + dfs(6, false)
-//      dfs(2, false)
-//          max_not_rob = dfs(null, false) + dfs(5, false)
-//              dfs(null, false) = 0
-//              dfs(5, false) = 5
-//                  max_not_rob = dfs(null, false) + dfs(null, false)
-//                  max_rob = 5 + dfs(null, false) + dfs(null, false) = 5
-//              < 5
-//          max_rob = 2 + dfs(null, true) + dfs(5, true)
-//              dfs(null, false) = 0
-//              < 2
-//          < max(5, 2) = 5
-//      dfs(6, false)
-//          max_not_rob = dfs(null, false) + dfs(1, false)
-//              dfs(null, false) = 0
-//              dfs(1, false)
-//                  max_not_rob = dfs(null, false) + dfs(null, false) = 0
-//                  max_rob = 6 
-//  max_rob = dfs(2, true) + dfs(6, true)
+// - node 5
+//  l_rob, l_skip = 0, 0
+//  r_rob, r_skip = 0, 0
+//  rob = 5
+//  skip = 0
+//  < [5, 0]
+// - node 1
+//  < [1, 0]
+// - node 2
+//  l_rob, l_skip = dfs(null) = 0, 0
+//  r_rob, r_skip = dfs(5) = 5, 0
+//  rob = 2
+//  skip = 0 + 5 = 5
+//  < [2, 5]
+// - node 6
+//  l_rob, l_skip = dfs(null) = 0, 0
+//  r_rob, r_skip = dfs(1) = 1, 0
+//  rob = 6
+//  skip = 1
+//  < [6, 1]
+// - node 3
+//  l_rob, l_skip = dfs(2) = 2, 5
+//  r_rob, r_skip = dfs(6) = 6, 1
+//  rob = 3 + 5 + 1 = 9
+//  skip = 5 + 6 = 11
