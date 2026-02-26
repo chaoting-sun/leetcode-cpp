9541b105f155724071a7d9f264d533309a94e0fc
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-02-26 15:02:36 +0800

    Time: 0 ms (100%), Space: 11.8 MB (78.84%) - LeetHub

---
+++ 0339-nested-list-weight-sum/0339-nested-list-weight-sum.cpp
@@ -0,0 +1,50 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+private:
+    void dfs(vector<NestedInteger>& nestedList, int depth, int& total_sum) {
+        int current_sum = 0;
+        for (NestedInteger& element: nestedList) {
+            if (element.isInteger()) {
+                current_sum += element.getInteger();
+            } else {
+                dfs(element.getList(), depth + 1, total_sum);
+            }
+        }
+        total_sum += current_sum * depth;
+    }
+
+public:
+    int depthSum(vector<NestedInteger>& nestedList) {
+        int total_sum = 0;
+        dfs(nestedList, 1, total_sum);
+        return total_sum;
+    }
+};
