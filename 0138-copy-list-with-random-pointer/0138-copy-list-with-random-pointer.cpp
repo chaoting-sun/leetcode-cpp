f4245196b488ffd133d74676221251f786accd4d
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-03-08 18:05:31 +0800

    Time: 3 ms (91.86%), Space: 15 MB (85.86%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -14,41 +14,41 @@ public:
 };
 */
 
-class Solution {
-public:
-    Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> mp;
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         unordered_map<Node*, Node*> mp;
 
-        Node* curr = head;
+//         Node* curr = head;
 
-        while (curr) {
-            // handle the current node
-            if (!mp.count(curr)) {
-                mp[curr] = new Node(curr->val);
-            }
-            Node* copy = mp[curr];
+//         while (curr) {
+//             // handle the current node
+//             if (!mp.count(curr)) {
+//                 mp[curr] = new Node(curr->val);
+//             }
+//             Node* copy = mp[curr];
 
-            // handle the next node
-            if (curr->next) {
-                if (!mp.count(curr->next)) {
-                    mp[curr->next] = new Node(curr->next->val);
-                }
-                copy->next = mp[curr->next];                
-            }
-            // handle the random node
-            if (curr->random) {
-                if (!mp.count(curr->random)) {
-                    mp[curr->random] = new Node(curr->random->val);
-                }
-                copy->random = mp[curr->random];
-            }
+//             // handle the next node
+//             if (curr->next) {
+//                 if (!mp.count(curr->next)) {
+//                     mp[curr->next] = new Node(curr->next->val);
+//                 }
+//                 copy->next = mp[curr->next];                
+//             }
+//             // handle the random node
+//             if (curr->random) {
+//                 if (!mp.count(curr->random)) {
+//                     mp[curr->random] = new Node(curr->random->val);
+//                 }
+//                 copy->random = mp[curr->random];
+//             }
 
-            curr = curr->next;
-        }
+//             curr = curr->next;
+//         }
         
-        return mp[head];
-    }
-};
+//         return mp[head];
+//     }
+// };
 
 // test case:
 // next: 1 -> 2 -> 3
@@ -67,3 +67,102 @@ public:
 // keep track of the mapping of node -> copy
 // iterate the linked list from the head, construct the copy of the next node and random node, storing
 // them in the hash map. If the hash map has stored the next or random node, then directly use it.
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         unordered_map<Node*, Node*> mp;
+
+//         Node* curr = head;
+
+//         while (curr) {
+//             // handle the current node
+//             if (!mp.count(curr)) {
+//                 mp[curr] = new Node(curr->val);
+//             }
+//             Node* copy = mp[curr];
+
+//             // handle the next node
+//             if (curr->next) {
+//                 if (!mp.count(curr->next)) {
+//                     mp[curr->next] = new Node(curr->next->val);
+//                 }
+//                 copy->next = mp[curr->next];                
+//             }
+//             // handle the random node
+//             if (curr->random) {
+//                 if (!mp.count(curr->random)) {
+//                     mp[curr->random] = new Node(curr->random->val);
+//                 }
+//                 copy->random = mp[curr->random];
+//             }
+
+//             curr = curr->next;
+//         }
+        
+//         return mp[head];
+//     }
+// };
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;        
+
+        // create copy and link copy->next
+        Node* curr = head;
+        while (curr) {
+            Node* copy = new Node(curr->val);
+            copy->next = curr->next;
+            curr->next = copy;
+
+            curr = copy->next;
+        }
+
+        // link copy->random
+        curr = head;
+        while (curr) {
+            Node* copy = curr->next;
+            copy->random = curr->random ? curr->random->next : nullptr;
+
+            curr = curr->next->next;
+        }
+
+        // split the original linked list and the copy
+        Node* copy_head = head->next;
+        curr = head;
+        while (curr) {
+            Node* copy = curr->next;
+            curr->next = copy->next;
+            copy->next = copy->next ? copy->next->next : nullptr;
+
+            curr = curr->next;
+            copy = copy->next;
+        }
+
+        return copy_head;
+    }
+};
+
+// test case:
+// ll: 1 -> 2 -> 3
+// random: 1->1, 2->null, 3->2
+
+// trace:
+// curr = node1
+// copy1, copy1->next = node2, node1->next = copy1
+// ll: node1 -> copy1 -> node2 -> node3
+// curr = node2
+// ll: node1 -> copy1 -> node2 -> copy2 -> node3
+// curr = node3
+// ll: node1 -> copy1 -> node2 -> copy2 -> node3 -> copy3
+
+// curr = node1
+// copy1->random = copy1
+// curr = node2
+// copy2->random = nullptr
+// curr = node3
+// copy3->random = copy2
+
+// ll: node1 -> copy1 -> node2 -> copy2 -> node3 -> copy3
+//                                          curr     copy
