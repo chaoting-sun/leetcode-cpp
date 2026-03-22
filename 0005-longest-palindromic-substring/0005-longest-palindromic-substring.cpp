5526570e471bd3b00546cbbb341eebcf553e3552
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-03-22 23:33:29 +0800

    Time: 11 ms (63.54%), Space: 18.6 MB (41.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,28 +1,44 @@
 class Solution {
+private:
+    string getPalindrome(string& s, int left, int right) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return s.substr(left + 1, right - left - 1);
+    }
+
 public:
     string longestPalindrome(string s) {
         int s_length = s.size();
-        if (s_length == 0) return "";
         
-        int max_left_idx = 0;
-        int max_length = 1;
+        int max_length = 0;
+        string max_palindrome = "";
 
-        auto expand = [&](int left, int right) {
-            while (left >= 0 && right < s_length && s[left] == s[right]) {
-                left--;
-                right++;    
+        for (int i = 0; i < s_length; i++) {
+            string curr = getPalindrome(s, i, i);
+            if (curr.size() > max_length) {
+                max_palindrome = curr;
+                max_length = curr.size();
             }
-            int current_length = right - left - 1;
-            if (current_length > max_length) {
-                max_length = current_length;
-                max_left_idx = left + 1;
-            }
-        };
-
-        // odd and even
-        for (int i = 0; i < s_length; i++) expand(i, i);
-        for (int i = 1; i < s_length; i++) expand(i - 1, i);
+        }
         
-        return s.substr(max_left_idx, max_length);
+        for (int i = 0; i < s_length - 1; i++) {
+            string curr = getPalindrome(s, i, i + 1);
+            if (curr.size() > max_length) {
+                max_palindrome = curr;
+                max_length = curr.size();
+            }
+        }
+
+        return max_palindrome;
     }
 };
+
+// babad
+// i = 1
+// getPalindrome(1, 1)
+//  left = 1, right = 1
+//  left = 0, right = 2
+//  left = -1, right = 3
+//  start at 0; length = 3
