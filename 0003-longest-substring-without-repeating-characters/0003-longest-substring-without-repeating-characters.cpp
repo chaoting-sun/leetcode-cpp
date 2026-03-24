e93b1ec55975baf2b17d93d52eb5036805f72f89
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-03-24 20:22:15 +0800

    Time: 9 ms (53.28%), Space: 14.3 MB (21.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<bool> seen(128);
-        int left = 0;
-        int max_len = 0;
+        int s_length = s.size();
+        if (s_length == 0) return 0;
 
-        for (int right = 0; right < s.size(); right++) {
-            while (seen[s[right]]) {
-                seen[s[left]] = false;
+        int left = 0;
+        unordered_set<char> seen;
+        int max_length = 0;
+
+        for (int right = 0; right < s_length; right++) {
+            while (seen.count(s[right])) {
+                seen.erase(s[left]);
                 left++;
             }
-            seen[s[right]] = true;
-            max_len = max(max_len, right - left + 1);
+
+            seen.insert(s[right]);
+            max_length = max(max_length, right - left + 1);
         }
-        return max_len;
+
+        return max_length;
     }
 };
+
+// trap case: s = aa
+// left = 0
+// right = 0, seen = { a }, max_length = 1
+// right = 1, seen = {}, left = 1, seen = { a }, max_length = 1
+// < returns 1
+
+// happy case: s = abc
+// trace:
+// left = 0
+// right = 0
+// seen = { a }, max_length = 1
+// seen = { a, b }, max_length = 2
+// seen = { a, b, c }, max_length = 3
+// < returns 3
+
+// base case: s = a
+// trace:
+// left = 0
+// right = 0
+// seen = { a }
+// max_length = 1
+// < returns 1
