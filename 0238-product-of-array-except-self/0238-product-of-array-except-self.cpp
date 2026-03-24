12f033f99530c20d6f50a72c93fbd579c9d05daa
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-03-24 23:43:05 +0800

    Time: 3 ms (36.85%), Space: 42.2 MB (25.16%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return {};
+        
+        vector<int> prefix_product(n), suffix_product(n);
+        prefix_product[0] = nums[0];
+        suffix_product[n - 1] = nums[n - 1];
 
-        vector<int> prefix(n), suffix(n);
-        prefix[0] = nums[0];
-        for (int i = 1; i < n; i++) prefix[i] = prefix[i - 1] * nums[i];
-        suffix[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) suffix[i] = suffix[i + 1] * nums[i];
+        for (int i = 1; i < n; i++) prefix_product[i] = prefix_product[i - 1] * nums[i];
+        for (int i = n - 2; i >= 0; i--) suffix_product[i] = suffix_product[i + 1] * nums[i];
 
         vector<int> ans(n);
         for (int i = 0; i < n; i++) {
-            int res = 1;
-            if (i > 0) res *= prefix[i - 1];
-            if (i < n - 1) res *= suffix[i + 1];
-            ans[i] = res;
+            int prefix = i == 0 ? 1 : prefix_product[i - 1];
+            int suffix = i == n - 1 ? 1 : suffix_product[i + 1];
+            ans[i] = prefix * suffix;
         }
+
         return ans;
     }
 };
