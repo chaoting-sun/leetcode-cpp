92407d3ddcad1f71798d56555a85512686d3eaf6
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-04-05 10:48:43 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -3,21 +3,86 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head:
+#             return None
+        
+#         nodes = []
+#         curr = head
+#         while curr:
+#             nodes.append(curr)
+#             curr = curr.next
+        
+#         nodes.sort(key=lambda x: x.val)
+
+#         for i in range(len(nodes) - 1):
+#             nodes[i].next = nodes[i + 1]
+#         nodes[-1].next = None
+
+#         return nodes[0]
+
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
+        if not head or not head.next:
+            return head
+
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next
         
-        nodes = []
-        curr = head
-        while curr:
-            nodes.append(curr)
+        right_head = slow.next
+        slow.next = None
+
+        left_head = self.sortList(head)
+        right_head = self.sortList(right_head)
+
+        return self.merge(left_head, right_head)
+
+
+    def merge(self, left_head, right_head):
+        dummy = ListNode()
+        curr = dummy
+        left_curr = left_head
+        right_curr = right_head
+
+        while left_curr or right_curr:
+            if not left_curr:
+                curr.next = right_curr
+                right_curr = right_curr.next
+            elif not right_curr:
+                curr.next = left_curr
+                left_curr = left_curr.next
+            else:
+                if left_curr.val <= right_curr.val:
+                    curr.next = left_curr
+                    left_curr = left_curr.next
+                else:
+                    curr.next = right_curr
+                    right_curr = right_curr.next
+
             curr = curr.next
-        
-        nodes.sort(key=lambda x: x.val)
 
-        for i in range(len(nodes) - 1):
-            nodes[i].next = nodes[i + 1]
-        nodes[-1].next = None
+        return dummy.next
 
-        return nodes[0]
+'''
+test case: 2 1
+trace:
+2 1
+right_head = 1
+left_head = sortList(2) -> 2
+right_head = sortList(1) -> 1
+
+dummy -> 1 -> 2
+            curr
+
+test case: 1
+trace:
+returns 1
+
+test case:
+head = None
+trace:
+returns None
+'''
