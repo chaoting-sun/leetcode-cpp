3331b3ae3199cc87dca1f19cae5d30e76ed841c5
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-04-05 11:49:17 +0800

    Time: 3 ms (87.53%), Space: 20.3 MB (95.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         # build DAG and indegree
-        courses = [[] for _ in range(numCourses)]
-        indegree = [0 for _ in range(numCourses)]
-        for pre in prerequisites:
-            courses[pre[1]].append(pre[0])
-            indegree[pre[0]] += 1
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            indegree[course] += 1
 
         # gather those with indegree 0
-        q = deque()
-        for i, v in enumerate(indegree):
-            if v == 0:
-                q.append(i)
+        q = deque(i for i, v in enumerate(indegree) if v == 0)
 
         # multi-source bfs
-        taken_count = 0
+        completed = 0
 
         while q:
             curr_course = q.popleft()
-            taken_count += 1
-            for next_course in courses[curr_course]:
+            completed += 1
+            for next_course in graph[curr_course]:
                 indegree[next_course] -= 1
                 if indegree[next_course] == 0:
                     q.append(next_course)
 
-        return taken_count == numCourses
+        return completed == numCourses
