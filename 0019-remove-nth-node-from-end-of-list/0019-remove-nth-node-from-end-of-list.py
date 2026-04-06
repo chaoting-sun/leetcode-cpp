24302cbbea99eb3f83026c06a83d7f8336389af4
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-04-06 11:57:39 +0800

    Time: 0 ms (100%), Space: 19.3 MB (75.25%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -3,23 +3,39 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#         # count nodes
+#         total = 0
+#         curr = head
+#         while curr:
+#             total += 1
+#             curr = curr.next
+        
+#         k = total - n
+
+#         # remove Nth nodes
+#         dummy = ListNode(next=head)
+#         curr = dummy
+#         for _ in range(k):
+#             curr = curr.next
+        
+#         curr.next = curr.next.next
+
+#         return dummy.next
+
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # count nodes
-        total = 0
-        curr = head
-        while curr:
-            total += 1
-            curr = curr.next
-        
-        k = total - n
-
-        # remove Nth nodes
         dummy = ListNode(next=head)
-        curr = dummy
-        for _ in range(k):
-            curr = curr.next
+        slow = fast = dummy
+        for _ in range(n):
+            fast = fast.next
         
-        curr.next = curr.next.next
-
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        
+        slow.next = slow.next.next
         return dummy.next
+          
