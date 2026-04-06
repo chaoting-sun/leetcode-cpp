4829498082db171582d067f068cd3d0778a234a7
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-04-06 14:02:16 +0800

    Time: 17 ms (20.66%), Space: 22.5 MB (95.25%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,21 +3,49 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         # need i as tie-breaker
+#         hq = [(head.val, i, head) for i, head in enumerate(lists) if head]
+#         heapq.heapify(hq)
+
+#         dummy = ListNode()
+#         curr = dummy
+
+#         while hq:
+#             _, i, head = heapq.heappop(hq)
+#             curr.next = head
+#             curr = curr.next
+            
+#             if head.next:
+#                 heapq.heappush(hq, (head.next.val, i, head.next))
+
+#         return dummy.next
+
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # need i as tie-breaker
-        hq = [(head.val, i, head) for i, head in enumerate(lists) if head]
-        heapq.heapify(hq)
+        if not lists:
+            return None
+        elif len(lists) == 1:
+            return lists[0]
 
+        mid = len(lists) // 2
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
+        return self.merge(left, right)
+
+    def merge(self, left, right):
         dummy = ListNode()
         curr = dummy
-
-        while hq:
-            _, i, head = heapq.heappop(hq)
-            curr.next = head
+        while left and right:
+            if left.val <= right.val:
+                curr.next = left
+                left = left.next
+            else:
+                curr.next = right
+                right = right.next
             curr = curr.next
-            
-            if head.next:
-                heapq.heappush(hq, (head.next.val, i, head.next))
 
+        curr.next = left if left else right
         return dummy.next
