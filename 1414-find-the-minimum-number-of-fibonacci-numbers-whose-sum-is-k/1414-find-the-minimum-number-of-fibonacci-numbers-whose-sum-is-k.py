cd7f1e5a1433a34037f71003ac31c34c76a177e1
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-04-11 15:43:20 +0800

    Time: 0 ms (100%), Space: 19.3 MB (73.43%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.py
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.py
@@ -1,31 +1,40 @@
+# class Solution:
+#     def findMinFibonacciNumbers(self, k: int) -> int:
+#         # gather numbers
+#         fib_nums = [1, 1]
+#         while True:
+#             next_num = fib_nums[-2] + fib_nums[-1]
+#             if next_num <= k:
+#                 fib_nums.append(next_num)
+#             else:
+#                 break
+
+#         # subtract from k
+#         idx = len(fib_nums) - 1
+#         count = 0
+#         while k > 0:
+#             if fib_nums[idx] > k:
+#                 idx -= 1
+#                 continue
+
+#             k -= fib_nums[idx]
+#             count += 1
+
+#         return count
+
+
 class Solution:
     def findMinFibonacciNumbers(self, k: int) -> int:
-        # gather numbers
-        fib_nums = [1, 1]
-        while True:
-            next_num = fib_nums[-2] + fib_nums[-1]
-            if next_num <= k:
-                fib_nums.append(next_num)
-            else:
-                break
-
-        # subtract from k
-        idx = len(fib_nums) - 1
+        a, b = 1, 1
+        while b <= k:
+            a, b = b, a + b
+        
         count = 0
         while k > 0:
-            if fib_nums[idx] > k:
-                idx -= 1
-                continue
-
-            k -= fib_nums[idx]
-            count += 1
+            if a <= k:
+                k -= a
+                count += 1
+            else:
+                a, b = b - a, a
 
         return count
-
-# k = 7
-# fib_nums = [1,1,2,3,5]
-#                 i
-# k = 2
-# count = 1
-# k = 0
-# count = 2
