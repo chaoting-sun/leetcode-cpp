eb1767773ac93e5154838fafea473d2f82c2bef4
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-04-25 21:14:16 +0800

    Time: 44 ms (16.52%), Space: 23.2 MB (15.51%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,32 +1,36 @@
-# class Solution:
-#     def subarraySum(self, nums: List[int], k: int) -> int:
-#         prefix = [0]
-#         for n in nums:
-#             prefix.append(prefix[-1] + n)
-
-#         count = 0
-#         for j in range(1, len(prefix)):
-#             for i in range(j):
-#                 if prefix[j] - prefix[i] == k:
-#                     count += 1
-        
-#         return count
-
-
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # prefix[j] - prefix[i] = k -> prefix[i] = prefix[j] - k
-
-        ans = 0
-        prefix = 0
-        # prefix_count[x]: the count of prefix sum = x before current position
-        prefix_count = defaultdict(int)
-        prefix_count[0] = 1
+        if not nums:
+            return 0
         
-        for n in nums:
-            prefix += n
-            key = prefix - k
-            ans += prefix_count[key] # default = 0
-            prefix_count[prefix] += 1
+        seen = defaultdict(int)
+        prefix = 0
+        seen[0] = 1
+        count = 0
 
-        return ans
+        for i in range(len(nums)):
+            prefix += nums[i]
+            count += seen[prefix - k]
+            seen[prefix] += 1
+        
+        return count
+
+
+'''
+nums = [1, 1, 1]
+k = 2
+
+i = 0
+    prefix = 1
+    count = 0
+    seen = { 0: 1, 1: 1 }
+i = 1
+    prefix = 2
+    count = 1
+    seen = { 0: 1, 1: 1, 2: 1 }
+i = 2
+    prefix = 3
+    count = 2
+    seen = { 0: 1, 1: 1, 2: 2 }
+< returns 2
+'''
