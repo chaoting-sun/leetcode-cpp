e2ddfa89b039df9cb48ec81d106558e74d0af1dc
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-13 20:46:47 +0800

    Time: 35 ms (74.69%), Space: 19.8 MB (43.2%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -5,55 +5,76 @@ class Solution:
         if len(s) < len(p):
             return []
 
-        chars = [0] * 26
-        distinct_chars_to_match = 0
+        p_count = [0] * 26
+        window_count = [0] * 26
+        
         for ch in p:
-            char_index = ord(ch) - ord('a')
-            if chars[char_index] == 0:
-                distinct_chars_to_match += 1
-            chars[char_index] += 1
-
+            p_count[ord(ch) - ord('a')] += 1
+        
         ans = []
         left = 0
 
         for right in range(len(s)):
-            ch = s[right]
-            char_index = ord(ch) - ord('a')
-            
-            chars[char_index] -= 1
-            if chars[char_index] == 0:
-                distinct_chars_to_match -= 1
+            window_count[ord(s[right]) - ord('a')] += 1
 
-            while chars[char_index] < 0:
-                left_char_index = ord(s[left]) - ord('a')
-                if chars[left_char_index] == 0:
-                    distinct_chars_to_match += 1
-                chars[left_char_index] += 1
+            if right - left + 1 > len(p):
+                window_count[ord(s[left]) - ord('a')] -= 1
                 left += 1
             
-            if distinct_chars_to_match == 0:
+            if right - left + 1 == len(p) and window_count == p_count:
                 ans.append(left)
-                
+            
         return ans
 
 
 '''
-case: s = 'abacd', p = 'ab'
-trace:
-chars = [1, 1, 0, 0]
-distinct_chars_to_match = 2
-
+s = 'abc', p = 'ab'
+p_count = [1 1 0]
 l = 0
-
 r = 0
-    chars = [0, 1, 0, 0]
-    distinct_chars_to_match = 1
+    window_count = [1 0 0]
 r = 1
-    chars = [0, 0, 0, 0]
-    distinct_chars_to_match = 0
+    window_count = [1 1 0]
     ans = [0]
 r = 2
-    chars = [-1, 0, 0, 0]
-    chars = [0, 0, 0, 0]
+    window_count = [1 1 1]
+    window_count = [0 1 1]
     l = 1
 '''
+
+
+# class Solution:
+#     def findAnagrams(self, s: str, p: str) -> List[int]:
+#         if len(s) < len(p):
+#             return []
+
+#         chars = [0] * 26
+#         distinct_chars_to_match = 0
+#         for ch in p:
+#             char_index = ord(ch) - ord('a')
+#             if chars[char_index] == 0:
+#                 distinct_chars_to_match += 1
+#             chars[char_index] += 1
+
+#         ans = []
+#         left = 0
+
+#         for right in range(len(s)):
+#             ch = s[right]
+#             char_index = ord(ch) - ord('a')
+            
+#             chars[char_index] -= 1
+#             if chars[char_index] == 0:
+#                 distinct_chars_to_match -= 1
+
+#             while chars[char_index] < 0:
+#                 left_char_index = ord(s[left]) - ord('a')
+#                 if chars[left_char_index] == 0:
+#                     distinct_chars_to_match += 1
+#                 chars[left_char_index] += 1
+#                 left += 1
+            
+#             if distinct_chars_to_match == 0:
+#                 ans.append(left)
+                
+#         return ans
