8116ff2455dad77b1e1faa6dd2641c91fe0009e0
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-13 21:14:47 +0800

    Time: 0 ms (100%), Space: 19.2 MB (96.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,27 +1,52 @@
-# bfs
-
-from collections import deque
+# dfs
 
 class Solution:
+    def dfs(self, image: List[List[int]], sr: int, sc: int, color: int, origin_color: int):
+        if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[0]) or image[sr][sc] != origin_color:
+            return
+        image[sr][sc] = color
+        self.dfs(image, sr + 1, sc, color, origin_color)
+        self.dfs(image, sr - 1, sc, color, origin_color)
+        self.dfs(image, sr, sc + 1, color, origin_color)
+        self.dfs(image, sr, sc - 1, color, origin_color)
+    
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        m, n = len(image), len(image[0])
-
+        m, n = len(image), len(image[0]) 
         origin_color = image[sr][sc]
-        visited = [[False for _ in range(n)] for _ in range(m)]
+        if origin_color == color:
+            return image
 
-        q = deque()
-        q.append((sr, sc))
-        visited[sr][sc] = True
-
-        while q:
-            prev_sr, prev_sc = q.popleft()
-            image[prev_sr][prev_sc] = color
-            
-            for d_row, d_col in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-                new_sr = prev_sr + d_row
-                new_sc = prev_sc + d_col
-                if 0 <= new_sr < m and 0 <= new_sc < n and not visited[new_sr][new_sc] and image[new_sr][new_sc] == origin_color:
-                    q.append((new_sr, new_sc))
-                    visited[new_sr][new_sc] = True
-        
+        self.dfs(image, sr, sc, color, origin_color)
         return image
+
+
+# bfs
+
+# from collections import deque
+
+# class Solution:
+#     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+#         m, n = len(image), len(image[0])
+
+#         origin_color = image[sr][sc]
+#         if origin_color == color:
+#             return image
+
+#         visited = [[False for _ in range(n)] for _ in range(m)]
+
+#         q = deque()
+#         q.append((sr, sc))
+#         visited[sr][sc] = True
+
+#         while q:
+#             prev_sr, prev_sc = q.popleft()
+#             image[prev_sr][prev_sc] = color
+            
+#             for d_row, d_col in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+#                 new_sr = prev_sr + d_row
+#                 new_sc = prev_sc + d_col
+#                 if 0 <= new_sr < m and 0 <= new_sc < n and not visited[new_sr][new_sc] and image[new_sr][new_sc] == origin_color:
+#                     q.append((new_sr, new_sc))
+#                     visited[new_sr][new_sc] = True
+        
+#         return image
