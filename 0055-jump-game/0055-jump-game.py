040ecb791ab7bea1ec1486fbae5b5fe22218e46d
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-14 19:28:53 +0800

    Time: 14 ms (82.28%), Space: 20.2 MB (64.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,18 +1,18 @@
 from collections import deque
 
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-        farthest_loc = 0
-        curr_loc = 0
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         n = len(nums)
+#         farthest_loc = 0
+#         curr_loc = 0
 
-        while curr_loc <= farthest_loc:
-            farthest_loc = max(farthest_loc, curr_loc + nums[curr_loc])
-            if farthest_loc >= n - 1:
-                return True
-            curr_loc += 1
+#         while curr_loc <= farthest_loc:
+#             farthest_loc = max(farthest_loc, curr_loc + nums[curr_loc])
+#             if farthest_loc >= n - 1:
+#                 return True
+#             curr_loc += 1
     
-        return False
+#         return False
 
 """
 test: nums = [2,3,1,1,4]
@@ -25,3 +25,18 @@ loop 1 <= 2
     fl = 4
     < returns True
 """
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        farthest_loc = 0
+
+        for curr_loc in range(n):
+            if farthest_loc < curr_loc:
+                break
+            
+            farthest_loc = max(farthest_loc, curr_loc + nums[curr_loc])
+            if farthest_loc >= n - 1:
+                return True
+
+        return False
