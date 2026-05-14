07bb4d12d1b3bc68d6ca0a3965d79dd89762d908
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-14 17:00:01 +0800

    Time: 0 ms (100%), Space: 19.4 MB (34.36%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -3,51 +3,34 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         cells = m * n
         
-        left_most = 0
-        right_most = n - 1
-        top_most = 1
-        bottom_most = m - 1
+        left = 0
+        right = n - 1
+        top = 0
+        bottom = m - 1
 
-        ans = [matrix[0][0]]
-        ci = 0
-        cj = 0
-        count = 1
+        ans = []
 
-        while True:
+        while left <= right and top <= bottom:
             # go right
-            while cj < right_most:
-                cj += 1
-                ans.append(matrix[ci][cj])
-                count += 1
-            right_most -= 1
-            if count == cells:
-                break
+            for j in range(left, right + 1):
+                ans.append(matrix[top][j])
+            top += 1
 
             # go down
-            while ci < bottom_most:
-                ci += 1
-                ans.append(matrix[ci][cj])
-                count += 1
-            bottom_most -= 1
-            if count == cells:
-                break
+            for i in range(top, bottom + 1):
+                ans.append(matrix[i][right])
+            right -= 1
 
             # go left
-            while cj > left_most:
-                cj -= 1
-                ans.append(matrix[ci][cj])
-                count += 1
-            left_most += 1
-            if count == cells:
-                break
+            if top <= bottom:
+                for j in range(right, left - 1, -1):
+                    ans.append(matrix[bottom][j])
+                bottom -= 1
             
             # go up
-            while ci > top_most:
-                ci -= 1
-                ans.append(matrix[ci][cj])
-                count += 1
-            top_most += 1
-            if count == cells:
-                break
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    ans.append(matrix[i][left])
+                left += 1
 
         return ans
