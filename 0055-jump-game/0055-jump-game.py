b13a59979cceec0628c4fb98b215822248e37685
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-14 19:21:59 +0800

    Time: 23 ms (46.13%), Space: 20.1 MB (85.98%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        farthest_loc = 0
+        curr_loc = 0
+
+        while curr_loc <= farthest_loc:
+            farthest_loc = max(farthest_loc, curr_loc + nums[curr_loc])
+            if farthest_loc >= n - 1:
+                return True
+            curr_loc += 1
+    
+        return False
+
+"""
+test: nums = [2,3,1,1,4]
+trace:
+fl = 0, cl = 0
+loop 0 <= 0
+    fl = 2
+    cl = 1
+loop 1 <= 2
+    fl = 4
+    < returns True
+"""
