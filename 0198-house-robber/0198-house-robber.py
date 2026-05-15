f2d2715636b91515eb6ba9beb81969ff67ff88ed
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-15 18:54:02 +0800

    Time: 0 ms (100%), Space: 19.2 MB (61.34%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * (n + 1)
+        
+        for i in range(1, n + 1):
+            dp[i] = max(dp[i - 1], nums[i - 1] + dp[i - 2]) # not rob / rob
+        
+        return dp[n]
