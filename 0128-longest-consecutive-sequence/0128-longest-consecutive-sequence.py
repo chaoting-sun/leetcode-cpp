c53fc7b842a46fe298f0f40a952f888ea3f1c93b
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-17 12:20:00 +0800

    Time: 119 ms (7.74%), Space: 40.1 MB (9.84%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -5,39 +5,19 @@ class Solution:
         if len(nums) == 0:
             return 0
         
-        seen = defaultdict(int)
+        seen = {}
         max_len = 0
         for v in nums:
             if v in seen:
                 continue
 
-            left_len = seen[v - 1] if v - 1 in seen else 0
-            right_len = seen[v + 1] if v + 1 in seen else 0
-            if v - 1 in seen and v + 1 in seen:
-                seen[v] = seen[v - left_len] = seen[v + right_len] = left_len + right_len + 1
-            elif v - 1 in seen:
-                seen[v] = seen[v - left_len] = left_len + 1
-            elif v + 1 in seen:
-                seen[v] = seen[v + right_len] = right_len + 1
-            else:
-                seen[v] = 1
+            left_len = seen.get(v - 1, 0)
+            right_len = seen.get(v + 1, 0)
+
+            new_len = left_len + right_len + 1
+            seen[v] = new_len
+            seen[v - left_len] = new_len
+            seen[v + right_len] = new_len
+            
             max_len = max(max_len, seen[v])
         return max_len
-
-
-'''
-test case: [1,3,2]
-seen = {}
-v = 1
-    seen = {1:1}
-    max_len = 1
-v = 3
-    seen = {1:1, 3:1}
-    max_len = 1
-v = 2
-    left_len = 1
-    right_len = 1
-    seen[2] = seen[1] = seen[3] = 3
-    max_len = 3
-< returns max_len = 3
-'''
