981e17e25bb207ef9c3c30e7c6ea9a21f6b01946
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-17 11:46:01 +0800

    Time: 7 ms (71.97%), Space: 23.3 MB (7.78%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,20 +3,16 @@ class Solution:
         if len(intervals) <= 1:
             return intervals
 
-        intervals.sort()
-        
         ans = []
+        intervals.sort()
         curr_start = intervals[0][0]
         curr_end = intervals[0][1]
-
         for interval in intervals:
-            start, end = interval[0], interval[1]
-            if curr_end >= start:
-                curr_end = max(end, curr_end)
+            if interval[0] <= curr_end:
+                curr_end = max(interval[1], curr_end)
             else:
                 ans.append([curr_start, curr_end])
-                curr_start = start
-                curr_end = end
-        
+                curr_start = interval[0]
+                curr_end = interval[1]
         ans.append([curr_start, curr_end])
         return ans
