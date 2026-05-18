a14f5e092f0b50b778d75087006e37ecb665eb71
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-18 13:30:55 +0800

    Time: 39 ms (19.15%), Space: 25.9 MB (67.52%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -4,17 +4,14 @@ class Solution:
             return -1
         
         n = len(gas)
-        gain = [gas[i] - cost[i] for i in range(n)]
-
-        # ensure if the answer exists before we search for it
-        if sum(gain) < 0:
-            return -1
-
-        curr_gas = 0
         start_idx = 0
-        for i, g in enumerate(gain):
-            curr_gas += g
-            if curr_gas < 0:
-                curr_gas = 0
+        total_gain = 0
+        curr_gain = 0
+
+        for i in range(n):
+            total_gain += gas[i] - cost[i]
+            curr_gain += gas[i] - cost[i]
+            if curr_gain < 0:
+                curr_gain = 0
                 start_idx = i + 1
-        return start_idx
+        return start_idx if total_gain >= 0 else -1
