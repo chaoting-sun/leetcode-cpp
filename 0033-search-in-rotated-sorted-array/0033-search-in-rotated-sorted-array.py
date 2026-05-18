ac0988a799572fac19f2b56356f7a4e9c0bab4a0
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-18 14:40:25 +0800

    Time: 0 ms (100%), Space: 19.4 MB (77.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,39 +1,45 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left <= right:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
             mid = left + (right - left) // 2
-            if nums[mid] == target:
-                return mid            
-            # left subarray is increasing
-            # use <= because when the subarray length is 2, left == mid (subarray of length 1 is in order)
-            elif nums[left] <= nums[mid]:
-                if nums[left] <= target and target <= nums[mid]:
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
                     right = mid
                 else:
                     left = mid + 1
-            # right subarray is increasing
             else:
-                if nums[mid] <= target and target <= nums[right]:
+                if nums[mid] <= target <= nums[right]:
                     left = mid
                 else:
                     right = mid - 1
-        return -1
+            print(left, right)
+        return left if nums[left] == target else -1
 
-# target = 3
-# 4 5 0 1 2 3
-#           r
-#         l
-#         m
-
-# 0 1 2 3 4 5
-# l         r
-#     m
-
-# 2 3 4 5 0 1
-# l         r
-#     m
-
-# 4 5 8 9 0 3
-# l         r
-#     m
+'''
+test case: nums = [3 4 0 1 2], target = 0
+trace:
+    nums = [3 4 0 1 2]
+                l
+                r
+                m
+test case: nums = [1 2 3 4], target = 1
+trace:
+    nums = [1 2 3 4]
+            l
+            r
+            m
+test case: nums = [3 1 2], target = 1
+trace:
+    nums = [3 1 2]
+              l
+              r
+              m
+test case: nums = [1 2], target = 3
+trace:
+    nums = [1 2]
+              l
+              r
+              m
+'''
