7fb6723ce7dc83555690ac66f2374d1da371cf9a
Sun, Chao-Ting <chaoting0108@gmail.com>
2026-05-18 13:28:06 +0800

    Time: 30 ms (41.19%), Space: 26.6 MB (5.71%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if len(gas) != len(cost):
+            return -1
+        
+        n = len(gas)
+        gain = [gas[i] - cost[i] for i in range(n)]
+
+        # ensure if the answer exists before we search for it
+        if sum(gain) < 0:
+            return -1
+
+        curr_gas = 0
+        start_idx = 0
+        for i, g in enumerate(gain):
+            curr_gas += g
+            if curr_gas < 0:
+                curr_gas = 0
+                start_idx = i + 1
+        return start_idx if start_idx < n else -1
